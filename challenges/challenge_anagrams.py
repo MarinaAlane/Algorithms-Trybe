4ed7fae5db5d7e9a5114e796bc71b9676a158b70
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-02-19 18:09:51 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    dict = {}
+    for letter in first_string:
+        if letter in dict:
+            dict[letter] += 1
+        else:
+            dict[letter] = 1
+    for letter in second_string:
+        if letter in dict:
+            dict[letter] -= 1
+        else:
+            return False
+    for letter in dict:
+        if dict[letter] != 0:
+            return False
+    return True
