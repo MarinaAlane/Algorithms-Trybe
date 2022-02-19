e6cc26e7649567ffc1ce1141d10fddce9a615714
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-02-19 17:45:45 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    length = len(word)
+    if(length == 0 or word[low_index] != word[high_index]):
+        return False
+    if(((high_index - 1) - (low_index + 1)) < 1):
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
