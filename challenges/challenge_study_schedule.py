60d6bb7f81f8d7796e0f08e52af9420fd7c95d2b
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-02-19 17:16:46 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for time in permanence_period:
+        if (
+            type(time[0]) != int
+            or type(time[1]) != int
+            or type(target_time) != int
+        ):
+            return None
+        if time[0] <= target_time and target_time <= time[1]:
+            count += 1
+    return count
