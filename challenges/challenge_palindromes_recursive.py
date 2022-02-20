a7d8dc521ab17c54b83b462280f856f3a31265a9
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 12:54:50 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 1:
+        return True
+    elif len(word) == 2 and word[low_index] == word[high_index]:
+        return True
+    elif word == "" or word[low_index] != word[high_index]:
+        return False
+    else:
+        sliced_word = word[1:high_index]
+        return is_palindrome_recursive(sliced_word, 0, len(sliced_word) -1)
+
