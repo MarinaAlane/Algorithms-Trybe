7783ac24448eb783af53851b9031240f28079578
Alberto Candido <albertocandido98@gmail.com>
2022-02-19 21:42:37 -0300

    iniciando o projeto algorithms

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
+from collections import defaultdict
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None:
+        return None
+    period_dict = defaultdict(lambda:0)
+    for initial, final in permanence_period:
+        if not isinstance(initial, int) or not isinstance(final, int):
+            return None
+        for period in range(initial, final + 1):
+            period_dict[period] += 1
+    return period_dict[target_time]
+
+
+# study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 3)
