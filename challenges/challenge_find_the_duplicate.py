683303a26fe5c990cd552de4632f56967c9a55a4
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 15:50:11 -0300

    decrease find_the_duplicate complexity

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,15 +1,27 @@
+# def find_duplicate(nums):
+#     duplicate = False
+#     try:
+#         nums.sort()
+#         index = 0
+#         # if nums[index] <= 0:
+#         #     raise Exception
+#         while not duplicate and index < len(nums) - 1:
+#             if nums[index] == nums[index + 1] and nums[index] > 0:
+#                 duplicate = nums[index]
+#             else:
+#                 index += 1
+#         return duplicate
+#     except Exception:
+#         return duplicate
+
 def find_duplicate(nums):
     duplicate = False
     try:
         nums.sort()
-        index = 0
-        while not duplicate and index < len(nums) - 1:
-            if nums[index] <= 0:
-                break
-            elif nums[index] == nums[index + 1]:
+        for index, item in enumerate(nums):
+            if item == nums[index + 1] and item > 0:
                 duplicate = nums[index]
-            else:
-                index += 1
+                break
         return duplicate
     except Exception:
         return duplicate
