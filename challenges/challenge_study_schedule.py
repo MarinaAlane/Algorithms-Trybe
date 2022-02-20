cc005a7b9d126577197d0c8bca3b35257df8ec86
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 12:11:03 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counter = 0
+        for student_period in permanence_period:
+            student_period[0] * student_period[1]
+            if student_period[0] == target_time or student_period[1] == target_time:
+                counter += 1
+            elif student_period[0] < target_time < student_period[1]:
+                counter += 1
+        return counter
+    except Exception:
+        return None
