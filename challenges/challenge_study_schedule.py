9bb059ef4db247ee0beb3402ab65b4fe551029da
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 15:02:00 -0300

    fix study_schedule complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,11 @@ def study_schedule(permanence_period, target_time):
         counter = 0
         for period in permanence_period:
             period[0] * period[1]
-            if period[0] == target_time or period[1] == target_time:
-                counter += 1
-            elif period[0] < target_time < period[1]:
+            if (
+                period[0] == target_time or
+                period[1] == target_time or
+                period[0] < target_time < period[1]
+            ):
                 counter += 1
         return counter
     except Exception:
