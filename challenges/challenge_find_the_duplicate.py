f66a993903170bb8fc4cad68d3810a195f323e9b
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 14:12:02 -0300

    req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    duplicate = False
+    try:
+        nums.sort()
+        index = 0
+        while not duplicate and index < len(nums) - 1:
+            if nums[index] <= 0:
+                break
+            elif nums[index] == nums[index + 1]:
+                duplicate = nums[index]
+            else:
+                index += 1
+        return duplicate
+    except Exception:
+        return duplicate
+
+
+print(find_duplicate([-1, "a"]))
---
+++ challenges/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+  # while not duplicate or index <= len(nums) -1:
+    #     if nums[index] == nums[index + 1]:
+    #         duplicate = nums[index]
+    #     else:
+    #         index += 1
