ef671a13604b7d826de3e1ea89fab75a88ce203d
Alberto Candido <albertocandido98@gmail.com>
2022-02-19 23:30:14 -0300

    req-03: add is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
+from collections import defaultdict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_dict = defaultdict(lambda: 0)
+    second_string_dict = defaultdict(lambda: 0)
+    for letter in first_string:
+        first_string_dict[letter] += 1
+    for letter in second_string:
+        second_string_dict[letter] += 1
+
+    return first_string_dict == second_string_dict
