c28a6a47af733ba0f6faf5eb375c068bbab75244
Alberto Candido <albertocandido98@gmail.com>
2022-02-19 22:38:35 -0300

    req-01: add study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,10 @@
-from collections import defaultdict
-
-
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
+    if target_time is None:
         return None
-    period_dict = defaultdict(lambda:0)
+    count = 0
     for initial, final in permanence_period:
         if not isinstance(initial, int) or not isinstance(final, int):
             return None
-        for period in range(initial, final + 1):
-            period_dict[period] += 1
-    return period_dict[target_time]
-
-
-# study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 3)
+        if initial <= target_time <= final:
+            count += 1
+    return count
