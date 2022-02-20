2388a1d4456940e2bdc0dc64cedcb367ece45612
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-20 14:33:47 -0300

    req5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    is_palindrome = True
+    half_index = len(word) // 2
+    index = 0
+
+    if not word: 
+        is_palindrome = False
+    while is_palindrome and index <= half_index:
+        if word[index] != word[-index -1]:
+            is_palindrome = False
+        else:
+            index += 1
+    return is_palindrome
