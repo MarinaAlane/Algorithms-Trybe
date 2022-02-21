7900f3db80e1d02742542fba946ff7b22bf7b452
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2022-02-21 18:54:10 -0300

    realiza desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
+def contains_duplicate(numbers, target_time):
+    if not target_time:
+        return None
+
+    counter = 0
+
+    for number in numbers:
+        if type(number[0]) != int or type(number[1]) != int:
+            return None
+
+        if number[0] <= target_time and number[1] >= target_time:
+            counter += 1
+
+    return counter
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    return contains_duplicate(permanence_period, target_time)
