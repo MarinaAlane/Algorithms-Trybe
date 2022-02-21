54a92d1994905a78d3ab94f0cafcef525efa7f56
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-21 15:47:20 -0300

    adiciona requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def get_char_schema(string):
+    schema = dict()
+
+    for char in string:
+        try:
+            schema[char] += 1
+        except KeyError:
+            schema[char] = 1
+
+    return schema
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_char_schema = get_char_schema(first_string)
+    second_char_schema = get_char_schema(second_string)
+
+    return first_char_schema == second_char_schema
