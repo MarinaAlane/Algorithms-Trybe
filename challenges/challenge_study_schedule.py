bdd696ee7150bf888ea9354242cdbb1270e94766
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 18:43:47 -0300

    req 1, melhoria no tempo de execuçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,21 +4,15 @@ def check_study_schedule(permanence_period, target_time):
     for tupla in permanence_period:
         if type(tupla[0]) != int or type(tupla[1]) != int:
             return None
-    return True
+    return True        
 
 
 def better_time_schedule(permanence_period, target_time):
-    count_target_time = 0
-    result = 0
-    while target_time >= count_target_time:
-        count = 0
-        for tupla in permanence_period:
-            if tupla[0] <= target_time and tupla[1] >= target_time:
-                count = count + 1
-        if result < count:
-            result = count
-        count_target_time = count_target_time + 1
-    return result
+    count = 0
+    for tupla in permanence_period:
+        if tupla[0] <= target_time and tupla[1] >= target_time:
+            count = count + 1
+    return count
 
 
 def study_schedule(permanence_period, target_time):
