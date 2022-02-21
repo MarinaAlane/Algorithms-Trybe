7caf7b43b95c08ce8660150e839f2b5b6a6cab9b
Pedro Piquini <piquini.pedro@gmail.com>
2022-02-21 16:07:58 -0300

    build: criado funcao recursiva que verifica se palavra e palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
-def is_palindrome_recursive(word, low_index, high_index):
+def is_palindrome_recursive(word, low_index = 0, high_index = 0):
     """ Faça o código aqui. """
+    if high_index == -1:
+        return False
+    elif len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome_recursive(word[1:-1])
