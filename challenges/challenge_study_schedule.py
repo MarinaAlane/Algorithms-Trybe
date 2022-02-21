ba973a1c39ae3d668520387eed583049475f3498
Hamander <1hamander@gmail.com>
2022-02-21 17:57:10 -0300

    build: study_schedule func created

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        online_students = [
+            True for entry, exit in permanence_period if exit >= target_time >= entry]
+    except TypeError:
+        return None
+    return len(online_students)
