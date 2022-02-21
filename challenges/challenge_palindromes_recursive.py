6a5f33dd8832cf1683b7e713a3426235430c03e5
Leandro Lopes <leandro_carlos@uol.com.br>
2022-02-21 19:12:31 -0300

    req 02 rerun

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,3 +7,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     except TypeError:
         return False
+        
+# rerun
