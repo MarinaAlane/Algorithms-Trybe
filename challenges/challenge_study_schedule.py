d20258b3dd6f20080dd8ed82064345c246b3d465
Jessie <miss_sc_jessi@hotmail.com>
2022-02-21 16:03:48 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        students = 0
+        for enter, exit in permanence_period:
+            if enter <= target_time <= exit:
+                students += 1
+    except TypeError:
+        return None
+    else:
+        return students
