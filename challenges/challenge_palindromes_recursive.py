b9d44fcd69adc203c8fe5d033ddcb440cd90bfb0
Renan <renan.vamo@gmail.com>
2022-02-21 19:04:14 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
+import math
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '' or word[low_index] != word[high_index]:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive('GG', 0, 1))
+# print(is_palindrome_recursive('GGG', 0, 2))
+# print(is_palindrome_recursive('GOOG', 0, 3))
+# print(is_palindrome_recursive('GOGOG', 0, 4))
+print(is_palindrome_recursive('GOGGOG', 0, 5))
