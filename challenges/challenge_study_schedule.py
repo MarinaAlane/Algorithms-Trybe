0850d1a10b27de32a900f6e4c1a7b2a94b22de9b
Matheus <matheusaraujo.dev@gmail.com>
2022-02-21 19:57:39 -0300

    Add: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    ...
-    """ Faça o código aqui. """
+    # Cada tupla é um estudante com (horario de entrada, horario de saida)
+    # counter armazena quando o estudante está online.
+    counter = 0
+
+    if type(target_time) is not int:
+        return None
+
+    # if type(permanence_period) != list:
+    #     return None
+
+    # Ajuda do Issac e colega Thiago Leite no plantão de CS.
+    # Se o target_time estiver value1 e value2, adiciona-se +1 ao counter.
+    for value1, value2 in permanence_period:
+        if type(value1) is not int or type(value2) is not int:
+            return None
+        if value1 <= target_time <= value2:
+            counter += 1
+
+    return counter
