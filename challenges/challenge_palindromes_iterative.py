2ca76fba1cc04d73afae112d93680cca23fc039a
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-21 18:54:19 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) < 1:
+        return False
+
+    left_index = 0
+    right_index = len(word) - 1
+    is_palindrome = True
+
+    while left_index <= right_index:
+        if word[left_index] != word[right_index]:
+            is_palindrome = False
+            break
+
+        left_index += 1
+        right_index -= 1
+
+    return is_palindrome
