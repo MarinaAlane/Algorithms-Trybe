4a76a2a42c487aaa97fb3b2c9cfb3885318a4ecb
Paulo Henrique Assis <pieit@live.com>
2022-02-21 17:09:44 -0300

    Added req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+permanence_periods = [(4, None), ("0", 4)]
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counter = 0
+        for schedule in permanence_period:
+            if schedule[0] == target_time or schedule[1] == target_time:
+                counter += 1
+            if schedule[0] < target_time < schedule[1]:
+                counter += 1
+        return counter if counter > 0 else None
+    except TypeError:
+        return None
+
+
+# print(study_schedule(permanence_periods, 4))
