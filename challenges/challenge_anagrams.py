95091f26abacd522b440265e708bea60244f3ab9
Hamander <1hamander@gmail.com>
2022-02-21 20:57:42 -0300

    refac: indentation problems

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,23 +1,24 @@
 def quick_sort(sequence):
-  if len(sequence) <= 1:
-    return sequence
-  else:
-    pivot = sequence.pop()
-
-  items_greater = []
-  items_lower = []
-
-  for item in sequence:
-    if item > pivot:
-      items_greater.append(item)
+    if len(sequence) <= 1:
+        return sequence
     else:
-      items_lower.append(item)
+        pivot = sequence.pop()
 
-  return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
 
-def split(word): 
-    return [char for char in word]  
+def split(word):
+    return [char for char in word]
+
 
 def is_anagram(first_string, second_string):
     if quick_sort(split(first_string)) == quick_sort(split(second_string)):
