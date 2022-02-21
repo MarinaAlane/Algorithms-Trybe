a84a0cc9d95607b3f89b9949a6a765f3d1876f4e
Arthur Hermann <arthurhermannc@gmail.com>
2022-02-21 15:39:48 -0300

    req3

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,21 @@
+# def is_palindrome_iterative(word):
+#     is_palindrome = True
+#     half_index = len(word) // 2
+#     index = 0
+#     if not word:
+#         is_palindrome = False
+#     while is_palindrome and index <= half_index:
+#         if word[index] != word[-index - 1]:
+#             is_palindrome = False
+#         else:
+#             index += 1
+#     return is_palindrome
+
+# src:
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
+
+
 def is_palindrome_iterative(word):
-    is_palindrome = True
-    half_index = len(word) // 2
-    index = 0
     if not word:
-        is_palindrome = False
-    while is_palindrome and index <= half_index:
-        if word[index] != word[-index - 1]:
-            is_palindrome = False
-        else:
-            index += 1
-    return is_palindrome
+        return False
+    return word == word[::-1]
