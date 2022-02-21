2c0ef2b547fd9dafdf5bf4e1b4a6858aa420b2c4
KaioHPs <kaiohpsalternativo@gmail.com>
2022-02-21 16:20:55 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    elif len(word) == 1 or high_index < low_index:
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+    
+
+if __name__ == '__main__':
+    word = 'AGUA'
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
