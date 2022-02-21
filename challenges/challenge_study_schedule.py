c07509f5aac6849064afb78c212bf709ac6da850
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-21 15:50:57 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        if target_time is None:
+            return
+        return count_study_time(permanence_period, target_time)
+    except TypeError:
+        return
+
+
+def count_study_time(permanence_period, target_time):
+    count = 0
+    for entrance, exit in permanence_period:
+        for item in range(entrance, exit + 1):
+            if target_time == item:
+                count += 1
+    return count
