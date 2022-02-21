6ecb2b7da9fa4da0a04ef30cdf06137c6fce7a8a
eraldoalmeida <eraldo.salmeida@gmail.com>
2022-02-21 16:49:56 -0300

    Req 1 refatorado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    if (
-        target_time == '' or
-        type(target_time) != int
-    ):
+    try:
+        students = 0
+        for period in permanence_period:
+            ta = period[0]
+            tb = period[1]
+            count = ta <= target_time and tb >= target_time
+
+            if count is True:
+                students += 1
+
+        return students
+
+    except TypeError:
         return None
-
-    count = 0
-    for schedule in permanence_period:
-        if type(schedule[0]) != int or type(schedule[1]) != int:
-            return None
-        if schedule[0] <= target_time <= schedule[1]:
-            count += 1
-
-    return count
-
-
-if __name__ == '__main__':
-    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-    print(study_schedule(permanence_period, 1))
+    
