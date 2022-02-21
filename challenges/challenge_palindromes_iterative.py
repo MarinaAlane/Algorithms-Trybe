21264b07f402805e1e9f3138f0e13dd7a65a5e32
Thiagofox <thiagofox00@gmail.com>
2022-02-21 14:28:38 -0300

    Iniciando projeto Req 3 e 5 finalizado Req 2 e 1 incompleto

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,16 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    return word == word[::-1]
+
+
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
+
+# def is_palindrome_iterative(word):
+#     if len(word) == 0:
+#         return False
+#     length = int(len(word))
+#     for index in range(0, length // 2):
+#         if word[index] != str(len(word) - index - 1):
+#             return False
+#         return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,26 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # if word == "":
+    #     return False
+
+    if len(word) <= 1:
+        return True
+    lenght = len(word) - 1
+    if word[0] == word[lenght]:
+
+        return is_palindrome_recursive(word[1:lenght], 0, lenght)
+    else:
+
+        return False
+
+
+# def is_palindrome_recursive(word, low_index, high_index):
+#     if word == "":
+#         return False
+
+#     lenght = len(word) - 1
+#     if word[0] != word[lenght]:
+#         return False
+#     else:
+#         if len(word) >= 1:
+#             return is_palindrome_recursive(word[1:lenght], 0, lenght)
+#         return True
