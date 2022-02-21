2f43e3d87536c364867791d137121be9b1fbec14
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 19:25:21 -0300

    correçao flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    #https://github.com/tryber/sd-011-project-algorithms/pull/14/files
+    # https://github.com/tryber/sd-011-project-algorithms/pull/14/files
     # Celso Oliva
     try:
         count = 0
         for tupla in permanence_period:
-            count = count+1 if tupla[0] <= target_time and tupla[1] >= target_time else count
+            count = count+1 if tupla[0] \
+                <= target_time and tupla[1] >= target_time else count
         return count
     except TypeError:
         return None
