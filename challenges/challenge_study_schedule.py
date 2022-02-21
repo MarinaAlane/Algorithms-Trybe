4ef3776aa6d363d9864add69b83b53e6c7bb71cc
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-21 15:44:01 -0300

    Add req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    quantity = 0
+    for period in permanence_period:
+        start, end = period
+        if None in period or type(start) != int or type(end) != int or target_time == None:
+            return None
+        if(start <= target_time <= end):
+            quantity += 1
+    return quantity
