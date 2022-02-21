59df98c267b7955900a90c00d57e9fafaf53867d
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-21 16:58:52 -0300

    feat: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
+def utils_palindrome_recursive(word, low_index, high_index):
+    if low_index == high_index:
+        return word
+    return (
+        utils_palindrome_recursive(word[1:], low_index, len(word[1:]) - 1)
+        + word[0]
+    )
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) < 1:
+        return False
+    palindrome = utils_palindrome_recursive(word, low_index, high_index)
+    return palindrome == word
