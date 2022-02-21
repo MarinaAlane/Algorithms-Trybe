6b216264cbf44e3d3e30fbf9a9af00d371fcd0fc
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-21 11:16:16 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+
+    counter = 0
+    for start_time, end_time in permanence_period:
+        if (
+            type(start_time) is not int
+            or type(end_time) is not int
+        ):
+            counter = None
+            break
+
+        if start_time <= target_time <= end_time:
+            counter += 1
+
+    return counter
