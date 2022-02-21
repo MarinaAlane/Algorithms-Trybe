304bc9a577305fe0704c3794026aaf29011a0b64
Thiago Gomes <thiagog.macedom@gmail.com>
2022-02-21 19:46:03 -0300

    add - task 3 done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+def get_char_count(string):
+    char_count = dict()
+
+    for char in string:
+        try:
+            char_count[char] += 1
+        except KeyError:
+            char_count[char] = 1
+
+    return char_count
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return get_char_count(first_string) == get_char_count(second_string)
