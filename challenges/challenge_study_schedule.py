8278944e47c2164ef4c1c80e839c2ab91dc4e789
Ghislaine <glenox79@gmail.com>
2022-02-21 19:39:24 -0300

    refactor: third attempt - replacing elif with if - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,19 +6,13 @@ def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
     student = 0
-    # como o requisito avalia a posição [0] e [1] da tupla referente ao periodo
-    # resolvi colocar as duas posições de variavel no 'for'
-    # estava dando problema no tempo ao tentar usar uma variavel e delimitar
-    # o indice => exemplo: period[0] | period[1]
-    # duas variveis em loop for:
-    # https://stackoverflow.com/questions/51933830/using-multiple-variables-in-a-for-loop-in-python
     for period_0, period_1 in permanence_period:
         # vendo o tipo inteiro referente ao segundo teste:
         # permanence_periods = [(4, None), ("0", 4)]
         # permanence_periods = [("A", 9), (None, 5)]
         if not isinstance(period_0, int) or not isinstance(period_1, int):
             return None
-        elif period_0 <= target_time <= period_1:
+        if period_0 <= target_time <= period_1:
             student += 1
     return student
 
@@ -34,6 +28,14 @@ def study_schedule(permanence_period, target_time):
 # Algoritmos de força bruta:
 # https://www.freecodecamp.org/portuguese/news/algoritmos-de-forca-bruta-explicados/
 # https://www.math.tecnico.ulisboa.pt/~ccal/python/nb06.html
+
+# Sobre o loop for:
+# como o requisito avalia a posição [0] e [1] da tupla referente ao periodo
+# resolvi colocar as duas posições de variavel no 'for'
+# estava dando problema no tempo ao tentar usar uma variavel e delimitar
+# o indice => exemplo: period[0] | period[1]
+# duas variveis em loop for:
+# https://stackoverflow.com/questions/51933830/using-multiple-variables-in-a-for-loop-in-python
 
 # Usei o isinstance() pois ao usar o type não deu muito certo no quesito tempo
 # https://stackoverflow.com/questions/21894575/isinstancefoo-bar-vs-typefoo-is-bar
