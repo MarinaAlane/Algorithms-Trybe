cff11cdc04055760d3f44796c6f4a872454af55a
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-21 18:50:33 -0300

    Refactor requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,13 +2,7 @@ from challenges.merge_sort import merge_sort
 
 
 def is_anagram(first_string, second_string):
-    first_array = list(first_string)
-    second_array = list(second_string)
+    first_string_array = merge_sort(list(first_string))
+    second_string_array = merge_sort(list(second_string))
 
-    ordenated_first_array = merge_sort(first_array)
-    ordenated_second_array = merge_sort(second_array)
-
-    ordenated_first_string = "".join(ordenated_first_array)
-    ordenated_second_string = "".join(ordenated_second_array)
-
-    return ordenated_first_string == ordenated_second_string
+    return first_string_array == second_string_array
