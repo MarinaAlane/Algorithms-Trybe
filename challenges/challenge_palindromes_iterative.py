dc988ce0c27b0498392a714eb8570f1ffedbd45c
Pedro Amaral <amaralph1995@gmail.com>
2022-02-21 18:11:29 -0300

    feat: Implementa versão recursiva da função de palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    first_index = 0
+    last_index = -1
+    mid = len(word) // 2
+    while first_index < mid:
+        if word[first_index] != word[last_index]:
+            return False
+        first_index += 1
+        last_index -= 1
+    return True
