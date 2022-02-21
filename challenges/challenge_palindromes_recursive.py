bd7c6e3f2722c5e26854e2917382f585ab26ef76
heyset <c.inaciomatheus@gmail.com>
2022-02-21 19:11:14 -0300

    req-2: adiciona requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    if high_index <= (len(word) - 1) / 2:
+        return True
+
+    return (True
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1))
