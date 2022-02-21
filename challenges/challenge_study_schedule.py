207974943a125cb81ee4510e4a542274749cd501
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-21 15:04:58 -0300

    adiciona requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    counter = 0
+
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+
+        if period[0] <= target_time and period[1] >= target_time:
+            counter += 1
+
+    return counter
