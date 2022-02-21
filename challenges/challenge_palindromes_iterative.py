1706b962fba58e65f75739f34f1000a4ed1ed06f
Paulo Henrique Assis <pieit@live.com>
2022-02-21 19:16:37 -0300

    Added req3

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) < 1:
+        return False
+    for i in range(len(word) // 2):
+        begining_char = word[i]
+        end_char = word[-i - 1]
+        if begining_char != end_char:
+            return False
+    return True
