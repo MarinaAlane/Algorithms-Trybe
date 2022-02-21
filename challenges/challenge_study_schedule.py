3384cb8d6c50b285eed1767427712d7cda92b11e
Thiago Gomes <thiagog.macedom@gmail.com>
2022-02-21 19:45:43 -0300

    add - task 1 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    cont = 0
+    try:
+        for p1, p2 in permanence_period:
+            if p1 <= target_time <= p2:
+                cont += 1
+        return cont
+    except TypeError:
+        return None
+
+if __name__ == '__main__':
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
