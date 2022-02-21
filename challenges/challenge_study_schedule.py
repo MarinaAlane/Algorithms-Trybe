00b4fc80cbb6e9b4e2bd6d37cb989283d632448a
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-21 16:03:54 -0300

    revert: remove list comprehension

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,15 +7,15 @@ def study_schedule(permanence_period, target_time):
         return
 
 
-# def count_study_time(permanence_period, target_time):
-#     count = 0
-#     for entrance, exit in permanence_period:
-#         for item in range(entrance, exit + 1):
-#             if target_time == item:
-#                 count += 1
-#     return count
-
 def count_study_time(permanence_period, target_time):
-    return len([1 for entrance, exit in permanence_period
-                for item in range(entrance, exit + 1)
-                if target_time == item])
+    count = 0
+    for entrance, exit in permanence_period:
+        for item in range(entrance, exit + 1):
+            if target_time == item:
+                count += 1
+    return count
+
+# def count_study_time(permanence_period, target_time):
+#     return len([1 for entrance, exit in permanence_period
+#                 for item in range(entrance, exit + 1)
+#                 if target_time == item])
