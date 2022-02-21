cbc55a2310fc77edddfb44752401e2a5f5dc185b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-21 15:54:47 -0300

    Refactor req01 - alright

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,13 @@
 def study_schedule(permanence_period, target_time):
     quantity = 0
+    if not target_time:
+        return None
     for start, end in permanence_period:
         if (
             not start
             or not end
             or type(start) != int
             or type(end) != int
-            or not target_time
         ):
             return None
         if(start <= target_time <= end):
