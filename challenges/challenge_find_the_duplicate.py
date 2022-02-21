4c2e0de15a6d637acda8a2b3e6d84ae546529111
heyset <c.inaciomatheus@gmail.com>
2022-02-21 20:03:37 -0300

    req-4: adiciona requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,28 @@
+from challenges.challenge_anagrams import merge_sort
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    try:
+        validate_input(nums)
+        sorted = merge_sort(nums)
+        for i in range(1, len(sorted)):
+            current = sorted[i]
+            previous = sorted[i - 1]
+            if (current == previous):
+                return current
+
+        return False
+    except ValueError:
+        return False
+
+
+def validate_input(nums):
+    if not nums:
+        raise ValueError()
+
+    for entry in nums:
+        if type(entry) is not int:
+            raise ValueError()
+
+        if entry < 0:
+            raise ValueError()
