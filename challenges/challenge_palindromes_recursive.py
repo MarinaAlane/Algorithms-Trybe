e5dd47d90b2f52d02c046dbd4f241790d270fec5
Renan <renan.vamo@gmail.com>
2022-02-21 19:05:05 -0300

    fix: retira linhas nao usada

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,3 @@
-import math
-
-
 def is_palindrome_recursive(word, low_index, high_index):
     if word == '' or word[low_index] != word[high_index]:
         return False
@@ -9,10 +6,3 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
-
-# print(is_palindrome_recursive('GG', 0, 1))
-# print(is_palindrome_recursive('GGG', 0, 2))
-# print(is_palindrome_recursive('GOOG', 0, 3))
-# print(is_palindrome_recursive('GOGOG', 0, 4))
-print(is_palindrome_recursive('GOGGOG', 0, 5))
