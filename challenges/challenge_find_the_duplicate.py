1a968cf546f19b37eae6d2ce4947e0d3e1aa714d
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-21 16:31:29 -0300

    adiciona requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,28 @@
+def get_schema(list):
+    schema = dict()
+
+    for item in list:
+        if type(item) is not int or item < 0:
+            return False
+
+        try:
+            schema[item] += 1
+        except KeyError:
+            schema[item] = 1
+
+    return schema
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    schema = get_schema(nums)
+
+    if not schema:
+        return False
+
+    duplicate = False
+
+    for attribute, value in schema.items():
+        if value > 1:
+            duplicate = attribute
+
+    return duplicate
