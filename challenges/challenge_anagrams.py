3ae741a122e9cc75ebecdce7f587dc1fc2eed18e
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-21 19:01:14 -0300

    feat: is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
+def new_sort(string):
+    if len(string) <= 1:
+        return string
+    base_letter = string[len(string) // 2]
+    middle = []
+    left = []
+    right = []
+    for letter in string:
+        if letter > base_letter:
+            right.append(letter)
+        elif letter < base_letter:
+            left.append(letter)
+        else:
+            middle.append(letter)
+    return new_sort(left) + middle + new_sort(right)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) is not len(second_string):
+        return False
+    first_string = "".join(new_sort(first_string))
+    second_string = "".join(new_sort(second_string))
+    return first_string == second_string
