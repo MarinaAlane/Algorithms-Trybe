750a99df2b5526b7cb425f0821ca11d27065ca7c
heyset <c.inaciomatheus@gmail.com>
2022-02-21 20:07:15 -0300

    req-5: adiciona requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,15 @@
+from math import ceil
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    max_iterator = ceil(len(word) / 2)
+    max_index = len(word) - 1
+
+    for i in range(max_iterator):
+        if word[i] != word[max_index - i]:
+            return False
+
+    return True
