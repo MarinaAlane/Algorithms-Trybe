bfa912eb02135591a78bc52c16e34f4fdf0ab438
Thiago Leite <leite.tjs@gmail.com>
2022-02-21 17:22:27 -0300

    feat: test 1 again

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,13 @@
-students_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
 def study_schedule(permanence_period, target_time):
     total = 0
 
-    if not target_time and target_time != 0:
-        return None
-
-    for item in permanence_period:
-        for i in item:
-            if type(i) is not int:
-                return None
-
     for student in permanence_period:
-        for i in range(student[0], student[1]+1):
+        if not target_time and target_time != 0:
+            return None
+        for i in range(student[0], student[1] + 1):
+            if student[0] or student[1] is not int:
+                return None
             if i == target_time:
                 total += 1
                 break
     return total
-
-print(study_schedule(students_period, 0))
