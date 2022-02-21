924d4138a7abb67cb5a3be76de0305e1cc35e68a
Pedro Amaral <amaralph1995@gmail.com>
2022-02-21 15:39:42 -0300

    feat: Cria função para analise de schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    best_time_count = 0
+    for student_time in permanence_period:
+        if type(student_time[0]) != int or type(student_time[1]) != int:
+            return None
+        if student_time[0] <= target_time <= student_time[1]:
+            best_time_count += 1
+
+    return best_time_count
