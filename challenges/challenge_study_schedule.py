5f60470802b4606317f3ac6bce48ef434256cdec
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 18:47:54 -0300

    req 1,nova melhoria no tempo de execuçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,17 +5,13 @@ def check_study_schedule(permanence_period, target_time):
         if type(tupla[0]) != int or type(tupla[1]) != int:
             return None
     return True        
+    
 
-
-def better_time_schedule(permanence_period, target_time):
+def study_schedule(permanence_period, target_time):
+    if not check_study_schedule(permanence_period, target_time):
+        return None
     count = 0
     for tupla in permanence_period:
         if tupla[0] <= target_time and tupla[1] >= target_time:
             count = count + 1
     return count
-
-
-def study_schedule(permanence_period, target_time):
-    if not check_study_schedule(permanence_period, target_time):
-        return None
-    return better_time_schedule(permanence_period, target_time)
