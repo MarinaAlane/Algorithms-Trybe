414aea3e6e85124bd3ae638074ac765a7ccb4872
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 18:18:21 -0300

    req 1, correçao de linhas brancas e reduçao da complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,16 @@
-
-
-
-def study_schedule(permanence_period, target_time):
-    count_target_time = 0
-    result = 0
+def check_study_schedule(permanence_period, target_time):
     if type(target_time) != int:
         return None
     for tupla in permanence_period:
         if type(tupla[0]) != int or type(tupla[1]) != int: 
             return None
+    return True        
+
+def study_schedule(permanence_period, target_time):
+    count_target_time = 0
+    result = 0
+    if not check_study_schedule(permanence_period, target_time):
+        return None    
     while target_time >= count_target_time:
         count = 0
         for tupla in permanence_period:
@@ -18,6 +20,5 @@ def study_schedule(permanence_period, target_time):
             result = count
         count_target_time = count_target_time + 1
     return result
-
 
 print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
