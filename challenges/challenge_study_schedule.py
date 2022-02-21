9f711f3732b7d673a6aa275b3f4c5828c5b3546a
Thiagofox <thiagofox00@gmail.com>
2022-02-21 16:23:15 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None
+    if target_time is None:
         return None
+
+    counter = 0
+
+    for index in permanence_period:
+        if type(index[0]) != int or type(index[1]) != int:
+            # verifica que as entradas das duas posições sao numeros
+            return None
+
+        if index[0] <= target_time and index[1] >= target_time:
+            # Verifica que se a primeiro valor e menos ou igual ao paramentro
+            # e se o segundo valor é menor ou igual ao paramentro
+            counter = counter + 1
+            # Caso seja adiciona mais um ao contador
+    return counter
