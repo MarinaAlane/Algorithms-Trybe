9a59095dcee5421e01156919c7b7b301730d886f
heyset <c.inaciomatheus@gmail.com>
2022-02-21 19:04:20 -0300

    req-1: adiciona requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,26 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        validate_target_time(target_time)
+        count = 0
+
+        for student_time in permanence_period:
+            (entry, leave) = get_student_int_entry(student_time)
+            if entry <= target_time <= leave:
+                count += 1
+
+        return count
+    except TypeError:
+        return None
+
+
+def validate_target_time(target_time):
+    if target_time is None:
+        raise TypeError()
+
+
+def get_student_int_entry(student_entry):
+    try:
+        (entry, leave) = student_entry
+        return (int(entry), int(leave))
+    except (TypeError, ValueError):
+        raise TypeError()
