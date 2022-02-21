3c5259de4ff7de548eaa63a5e906aad4414afd1c
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 18:59:56 -0300

    req 1,uniao de funçoes e reduçoes de ifs

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,10 @@
-def check_study_schedule(target_time):
+def study_schedule(permanence_period, target_time):
     if type(target_time) != int:
         return None
-    return True
-
-
-def better_time_schedule(permanence_period, target_time):
     count = 0
     for tupla in permanence_period:
         if type(tupla[0]) != int or type(tupla[1]) != int:
             return None
-        if tupla[0] <= target_time and tupla[1] >= target_time:
+        elif tupla[0] <= target_time and tupla[1] >= target_time:
             count = count + 1
     return count
-
-
-def study_schedule(permanence_period, target_time):
-    if not check_study_schedule(target_time):
-        return None
-    return better_time_schedule(permanence_period, target_time)
-
