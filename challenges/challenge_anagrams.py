866c2631a1acc20624df66f9c979e35bf9c43b27
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-02-21 18:34:26 -0300

    Add Req03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def set_string(string, character_dict, method=-1):
+    for i in string:
+        if i in character_dict:
+            character_dict[i] += method
+        else:
+            character_dict[i] = 1
+    return character_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    character_dict = dict()
+
+    character_dict = set_string(first_string, character_dict, 1)
+    character_dict = set_string(second_string, character_dict)
+
+    for i in character_dict:
+        if character_dict[i] != 0:
+            return False
+    return True
