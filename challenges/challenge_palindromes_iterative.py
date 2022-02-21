f39f7e8d1ef35019a728ec82f9138a2b8806b73f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-02-21 16:39:01 -0300

    adiciona requisito 05

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    counter = 0
+    result = True
+
+    while counter <= (len(word) // 2) - 1:
+        result = word[counter] == word[-counter - 1]
+        counter += 1
+
+    return result
