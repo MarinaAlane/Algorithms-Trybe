312cad56aec59d99b82a0f6c43d4f82a15711102
marcoswarmling <marcoswarmling15@gmail.com>
2022-02-21 16:55:23 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    counter = 0
+    for first_time, last_time in permanence_period:
+        if type(first_time) is not int or type(last_time) is not int:
+            return None
+        if first_time <= target_time <= last_time:
+            counter += 1
+    return counter
+
