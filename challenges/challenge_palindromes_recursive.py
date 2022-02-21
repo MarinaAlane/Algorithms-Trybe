c2ba829bcd91fccf39e8d246080d8918f6bb6edf
Diego Ribeiro <dihrm2001@gmail.com>
2022-02-21 18:27:25 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word.strip() == "":
+        return False
+
+    return check_palindrome(word, low_index, high_index)
+
+
+def check_palindrome(word, low_index, high_index):
+    if low_index >= high_index:
+        return True
+
+    equal_letters = word[low_index] == word[high_index]
+
+    return equal_letters and check_palindrome(
+        word, low_index + 1, high_index - 1
+    )
