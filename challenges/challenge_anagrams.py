44471ece675ff3e6f80df57267f027c22f01054c
Renan <renan.vamo@gmail.com>
2022-02-21 19:26:55 -0300

    refatora o ex 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,10 @@
 def is_anagram(first_string, second_string):
-    second_string_array = list(second_string)
+    array = list(second_string)
     try:
         for i in range(len(first_string)):
-            second_string_array.remove(first_string[i])
-        if len(second_string_array) == 0:
+            array.remove(first_string[i])
+        if not array:
             return True
         return False
     except ValueError:
         return False
-
-
-is_anagram('amor', 'roma')
