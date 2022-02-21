bc588ad71b543192c359b1b139aefa153c156909
Antonio Schappo <antonioschappo@gmail.com>
2022-02-21 16:35:55 -0300

    study_schedule functionn works, req 1. done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for tuple in permanence_period:
+            if tuple[0] <= target_time <= tuple[1]:
+                counter += 1
+    except TypeError:
+        return None
+    return counter
