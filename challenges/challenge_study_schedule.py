0337c31b2f16acb2b588b54c4dc15fbc98f0c6f0
Renan <renan.vamo@gmail.com>
2022-02-21 15:21:21 -0300

    add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+from multiprocessing.sharedctypes import Value
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for entrada, saida in permanence_period:
+            if entrada <= target_time <= saida:
+                counter += 1
+    except TypeError:
+        return None
+
+    return counter
