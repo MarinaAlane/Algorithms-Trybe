3198bcbefa3ad60cc4873ba5a370447f5b105beb
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-21 16:13:51 -0300

    perf: use an if case instead of for loop

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,15 +7,17 @@ def study_schedule(permanence_period, target_time):
         return
 
 
+# def count_study_time(permanence_period, target_time):
+#     count = 0
+#     for entrance, exit in permanence_period:
+#         for item in range(entrance, exit + 1):
+#             if target_time == item:
+#                 count += 1
+#     return count
+
 def count_study_time(permanence_period, target_time):
     count = 0
     for entrance, exit in permanence_period:
-        for item in range(entrance, exit + 1):
-            if target_time == item:
-                count += 1
+        if entrance <= target_time <= exit:
+            count += 1
     return count
-
-# def count_study_time(permanence_period, target_time):
-#     return len([1 for entrance, exit in permanence_period
-#                 for item in range(entrance, exit + 1)
-#                 if target_time == item])
