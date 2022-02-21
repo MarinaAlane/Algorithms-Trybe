09225b023db1f5786e62ca0254e46cf1dd2fb54b
Matheus Alexandre <alexandremhm@gmail.com>
2022-02-20 21:39:16 -0300

    fix requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    return [
-        number
-        for number in permanence_period
-        if target_time in range(number[0], number[1]) or target_time in number
-    ]
+    result = 0
+
+    if not bool(target_time):
+        return None
+
+    for number in permanence_period:
+        if not isinstance(number[0], int) or not isinstance(number[1], int):
+            return None
+        if number[0] <= target_time <= number[1]:
+            result += 1
+    return result
 
 
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+permanence_period = [(2, 2), (1, 2), ("a", 3), (1, 5), (4, 5), (4, 5)]
 
 
-print(study_schedule(permanence_period, 5))
+print(study_schedule(permanence_period, None))
--- challenges/tempCodeRunnerFile.py
+++ challenges/tempCodeRunnerFile.py
@@ -1,12 +1,21 @@
 def study_schedule(permanence_period, target_time):
-    return [
-        number
-        for number in permanence_period
-        if target_time in range(number[0], number[1]) or target_time in number
-    ]
+    result = 0
+
+    if not bool(target_time):
+        return None
+
+    for number in permanence_period:
+        if type(number[0]) != int or type(number[0]) != int:
+            return None
+        elif (
+            number[0] <= target_time <= number[1]
+            # target_time in range(number[0], number[1]) or target_time in number
+        ):
+            result += 1
+    return result
 
 
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+permanence_period = [(2, 2), (1, 2), ('a', 3), (1, 5), (4, 5), (4, 5)]
 
 
-print(study_schedule(permanence_period, 5))
+print(study_schedule(permanence_period, None))
