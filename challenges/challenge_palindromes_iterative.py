8f6c1ced31525e2b534f07b9e21e90b0d0292e1c
Pedro Amaral <amaralph1995@gmail.com>
2022-02-21 18:17:32 -0300

    fix: Corrige bug na função palindromo iterativo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -4,7 +4,7 @@ def is_palindrome_iterative(word):
     first_index = 0
     last_index = -1
     mid = len(word) // 2
-    while first_index < mid:
+    while first_index < mid and last_index > -1 * (mid + 1):
         if word[first_index] != word[last_index]:
             return False
         first_index += 1
