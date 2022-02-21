e041ef186ad944172e3c070a7cf818887e5fda9a
Jessica Arruda <sicarruda@gmail.com>
2022-02-21 19:22:21 -0300

    alteraçao do codigo e remoçao dos ifs

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    if type(target_time) != int:
+    #https://github.com/tryber/sd-011-project-algorithms/pull/14/files
+    # Celso Oliva
+    try:
+        count = 0
+        for tupla in permanence_period:
+            count = count+1 if tupla[0] <= target_time and tupla[1] >= target_time else count
+        return count
+    except TypeError:
         return None
-    count = 0
-    for tupla in permanence_period:
-        if type(tupla[0]) != int or type(tupla[1]) != int:
-            return None
-        elif tupla[0] <= target_time and tupla[1] >= target_time:
-            count = count + 1
-    return count
+
