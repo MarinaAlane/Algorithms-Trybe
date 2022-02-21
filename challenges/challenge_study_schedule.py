8338befc4e3c29dac633b4ad43163615f936bc9d
Thiago Leite <leite.tjs@gmail.com>
2022-02-21 19:24:47 -0300

    feat: req 1 passing

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
     total = 0
-    if not target_time and target_time != 0:
+
+    if type(target_time) is not int:
         return None
-    try:
-        for student in permanence_period:
-            for i in range(student[0], student[1] + 1):
-                if i == target_time:
-                    total += 1
-                    break
-    except TypeError:
-        return None
+
+    for valor1, valor2 in permanence_period:
+        if type(valor1) is not int or type(valor2) is not int:
+            return None
+        if valor1 <= target_time <= valor2:
+            total += 1
 
     return total
