dba1e7b6411cc27f1d64c61c045adf82d480a3a2
Renan <renan.vamo@gmail.com>
2022-02-21 19:25:01 -0300

    add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    second_string_array = list(second_string)
+    try:
+        for i in range(len(first_string)):
+            second_string_array.remove(first_string[i])
+        if len(second_string_array) == 0:
+            return True
+        return False
+    except ValueError:
+        return False
+
+
+is_anagram('amor', 'roma')
