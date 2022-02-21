5868327150774e4eea646408e586c779dad391a9
Pedro Amaral <amaralph1995@gmail.com>
2022-02-21 17:26:59 -0300

    feat: Implementa função de teste de anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for letter in first_string:
+        if letter not in second_string:
+            return False
+        second_string = second_string.replace(letter, "", 1)
+    return True
