bbd4359e9be550460660a941adfb4a98f7891ccb
Daniel <danielfdoficial@gmail.com>
2022-02-22 10:10:59 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui """
+    if type(target_time) is not int:
+        return None
+    students_online = 0
+    for beggining, ending in permanence_period:
+        if (
+            type(beggining) is not int
+            or type(ending) is not int
+        ):
+            students_online = None
+            break
+        if beggining <= target_time <= ending:
+            students_online += 1
+    return students_online
