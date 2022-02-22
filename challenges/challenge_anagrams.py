0d60e95b957612ee366905b4597a70080b4bdea2
Filizzola <juliofilizzola@hotmail.com>
2022-02-22 15:24:00 -0300

    🔨: req = 3 ref, delete import

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,3 @@
-from operator import length_hint
-
-
 def is_anagram(first_string, second_string):
     string_validy = (first_string == "" or second_string == "")
     length_validy= len(first_string) != len(second_string)
