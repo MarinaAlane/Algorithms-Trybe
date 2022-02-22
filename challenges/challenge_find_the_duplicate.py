66021c193da083e1ec35d30f8653db3327e4f7d7
Leandro Lopes <leandro_carlos@uol.com.br>
2022-02-22 17:37:54 -0300

    req 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
+def get_list(list):
+    create_list = {}
+    for i in list:
+        if i in create_list: 
+            return i
+        if type(i) is not int or i < 0:
+            return False
+        else:
+            create_list[i] = 1
+    return False
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    create_list = get_list(nums)
+
+    if create_list is None:
+        return False
+    else:
+        return create_list
+
