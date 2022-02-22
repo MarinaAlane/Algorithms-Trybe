683dc36bdad78592b88351fc1de4d8128b6263b9
Italo <italomatos764@gmail.com>
2022-02-22 17:37:21 -0400

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if len(first_string) == len(second_string) == 0 or len(first_string) != len(second_string):
+        return False
+    first_string = list(first_string)
+    second_string = list(second_string)
+    copy = ''
+    for item in first_string:
+        for index in range(0, len(second_string)):
+            if second_string[index] == item:
+                copy = copy + second_string[index]
+                second_string.pop(index)
+                break
+    if len(second_string) == 0:
+        return True
+    return False
+
+# print(is_anagram('pedrra', 'pedraa'))
+# print(is_anagram('pedraa', 'pedrra'))
