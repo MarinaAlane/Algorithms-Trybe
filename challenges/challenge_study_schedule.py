5e9ac6979ad149addbc55f563c1de59e38e7ebcb
Italo <italomatos764@gmail.com>
2022-02-22 00:19:24 -0400

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not target_time:
+        return None
+    result = 0
+    for enter, exit in permanence_period:
+        try:
+            if target_time >= enter and target_time <= exit:
+                result = result + 1
+        except TypeError:
+            return None
+    return result
+# teste = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(teste, 4))
