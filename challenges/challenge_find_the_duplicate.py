72b1c87645fa3dba2b1656e846c2ebb6689a7ca7
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-22 18:37:00 -0300

    challenge 4 completo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,21 @@
+def check_param(param):
+    if not param or len(param) <= 1:
+        return False
+    else:
+        for number in param:
+            if not isinstance(number, int) or number < 0:
+                return False
+            else:
+                return True
+
+
 def find_duplicate(nums):
     """Faça o código aqui."""
+    if not check_param(nums):
+        return False
+    for number in nums:
+        if nums.count(number) > 1:
+            return number
+        else:
+            continue
+    return False
