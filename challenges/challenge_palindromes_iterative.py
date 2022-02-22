f7fa75ab9ca2cc0d950a3d534715c750276f4093
heyset <c.inaciomatheus@gmail.com>
2022-02-22 18:55:49 -0300

    req-5: refactor: usa cheat

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,11 +2,4 @@ def is_palindrome_iterative(word):
     if word == '':
         return False
 
-    max_iterator = len(word) // 2
-    max_index = len(word) - 1
-
-    for i in range(max_iterator):
-        if word[i] != word[max_index - i]:
-            return False
-
-    return True
+    return word == word[::-1]
