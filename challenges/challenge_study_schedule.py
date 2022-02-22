d8f26fe002e07548dc50dd2e0970e18b6a2f94d6
heyset <c.inaciomatheus@gmail.com>
2022-02-22 17:28:16 -0300

    Revert "req-1: refatora para ter menos chamadas de funcao"
    This reverts commit c5fd5526cc0e11ce9b4f6b4da5397fbaf9bab868.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,26 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time:
+    try:
+        validate_target_time(target_time)
+        count = 0
+
+        for student_time in permanence_period:
+            (entry, leave) = get_student_int_entry(student_time)
+            if entry <= target_time <= leave:
+                count += 1
+
+        return count
+    except TypeError:
         return None
 
-    count = 0
 
-    for entry, leave in permanence_period:
-        if type(entry) is not int or type(leave) is not int:
-            return None
+def validate_target_time(target_time):
+    if target_time is None:
+        raise TypeError()
 
-        if entry <= target_time <= leave:
-            count += 1
 
-    return count
+def get_student_int_entry(student_entry):
+    try:
+        (entry, leave) = student_entry
+        return (int(entry), int(leave))
+    except (TypeError, ValueError):
+        raise TypeError()
