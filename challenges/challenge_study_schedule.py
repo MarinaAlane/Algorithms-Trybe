4a3becf9d0a300dbc8712857e8d1d5edb83a628f
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2022-02-22 18:16:15 -0300

    refatora desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time:
-        return None
-
     counter = 0
 
-    for number in permanence_period:
-        if type(number[0]) != int or type(number[1]) != int:
-            return None
-        if number[0] <= target_time and number[1] >= target_time:
-            counter += 1
-
-    return counter
+    try:
+        for number in permanence_period:
+            if number[0] <= target_time <= number[1]:
+                counter += 1
+    except TypeError:
+        pass
+    else:
+        return counter
