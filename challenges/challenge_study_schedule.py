cef8cc06ae6615620f7643b75ea3345814e5d227
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-02-21 21:50:25 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    curr_students = 0
+    for i in range(0, len(permanence_period)):
+        try:
+            if (
+                permanence_period[i][1]
+                >= target_time >= permanence_period[i][0]
+            ):
+                curr_students += 1
+        except TypeError:
+            return None
+    return curr_students
