806226f117981b7bf79e68ad50281e0e4636df72
Italo <italomatos764@gmail.com>
2022-02-22 18:37:33 -0400

    Lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,4 @@
-def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-    if len(first_string) == len(second_string) == 0 or len(first_string) != len(second_string):
-        return False
+def aux(first_string, second_string):
     first_string = list(first_string)
     second_string = list(second_string)
     copy = ''
@@ -11,7 +8,25 @@ def is_anagram(first_string, second_string):
                 copy = copy + second_string[index]
                 second_string.pop(index)
                 break
-    if len(second_string) == 0:
+    return second_string
+
+def is_anagram(first_string, second_string):
+    """ Faça o código aqui. """
+    if len(first_string) == len(second_string) == 0:
+        return False    
+    if len(first_string) != len(second_string):
+        return False
+    # first_string = list(first_string)
+    # second_string = list(second_string)
+    # copy = ''
+    # for item in first_string:
+    #     for index in range(0, len(second_string)):
+    #         if second_string[index] == item:
+    #             copy = copy + second_string[index]
+    #             second_string.pop(index)
+    #             break
+
+    if len(aux(first_string, second_string)) == 0:
         return True
     return False
 
