f7097c9a5456354cbe55bc6f35378ab22b544020
Leandro Lopes <leandro_carlos@uol.com.br>
2022-02-22 16:19:15 -0300

    req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
+def character_count(string):
+    string_dict = {}
+    for char in string:
+        if char in string_dict:
+            string_dict[char] += 1
+        else:
+            string_dict[char] = 0
+    return string_dict
+    
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_dict = character_count(first_string)
+    second_string_dict = character_count(second_string)
+    if len(first_string_dict) != len(second_string_dict):
+        return False
+    for key in first_string_dict:
+        if key not in second_string_dict:
+            return False
+        if first_string_dict[key] != second_string_dict[key]:
+            return False
+    return True
+  
