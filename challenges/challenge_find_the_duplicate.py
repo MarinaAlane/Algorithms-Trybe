6fd30c52760b3f5a0fc9d2e2408b7a6f6807c5cc
Pedro Piquini <piquini.pedro@gmail.com>
2022-02-22 16:07:08 -0300

    build: criado funcao que encontra numero duplicado em array

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    nums.sort()
+    duplicate = ''
+    if len(nums) == 1:
+        return False
+    for index, number in enumerate(nums):
+        if (type(number) is not int) or (number < 0):
+            return False
+        elif index < len(nums) - 1:
+            if nums[index] == nums[index + 1]:
+                duplicate = number
+                break
+            else:
+                duplicate = False
+    return duplicate
