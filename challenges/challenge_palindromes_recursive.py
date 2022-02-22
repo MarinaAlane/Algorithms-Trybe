b0e5bf6a5a57520f235455c7a0d1a63864fb14d6
Daniel <danielfdoficial@gmail.com>
2022-02-22 11:55:23 -0300

    fix lint error req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,10 @@
-from pickle import FALSE, TRUE
-
-
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
     if word[low_index] != word[high_index]:
         return False
-    return True and is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    if low_index >= high_index:
+        return True
+    next = low_index + 1
+    before = high_index - 1
+    return True and is_palindrome_recursive(word, next, before)
