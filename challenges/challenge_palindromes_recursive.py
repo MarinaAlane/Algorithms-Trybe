9387dc88e5f215ed0c051850f3bfef5584645b3f
Thiago Leite <leite.tjs@gmail.com>
2022-02-22 15:43:33 -0300

    feat: req 2 passing

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
-def is_palindrome_recursive(word, low_index, high_index):
+def is_palindrome_recursive(word, low_index=0, high_index=-1):
     """ Faça o código aqui. """
+    if type(word) is not str or not word:
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    low_index += 1
+    high_index += -1
+
+    if low_index == len(word):
+        return True
+    else:
+        return is_palindrome_recursive(word, low_index, high_index)
