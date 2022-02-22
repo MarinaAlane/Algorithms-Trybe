c8fb8394a3cb92370b55b3515aac5b3ab6f26b48
IsleneGomes <islenegomes@gmail.com>
2022-02-22 12:38:28 -0300

    feat: Requisito 3 em desenvolvimento

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,30 @@
+# https://panda.ime.usp.br/panda/static/pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
+def solucaoAnagrama(s1,s2):
+    c1 = [0]*26
+    c2 = [0]*26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i])-ord('a')
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i])-ord('a')
+        c2[pos] = c2[pos] + 1
+
+    j = 0
+    aindaOK = True
+    while j<26 and aindaOK:
+        if c1[j]==c2[j]:
+            j = j + 1
+        else:
+            aindaOK = False
+
+    return aindaOK
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(solucaoAnagrama(first_string, second_string)):
+        return True
+    else:
+        return False
+
+
