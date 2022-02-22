7067ccf9d5d01bd5dc25490a8a324100ad335d08
gab-boff <gmeninboff@gmail.com>
2022-02-22 19:38:40 -0300

    fix: flake error

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,8 +6,9 @@ def char_dict(string):
             string_dict[letter] += 1
         except KeyError:
             string_dict[letter] = 1
-    
+
     return string_dict
+
 
 def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
