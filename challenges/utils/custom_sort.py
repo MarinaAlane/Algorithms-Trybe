07235f3c8d301d981ccb47628d394c01dba09ebf
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-22 08:53:20 -0300

    refactor: list comprehension

--- challenges/utils/custom_sort.py
+++ challenges/utils/custom_sort.py
@@ -2,14 +2,9 @@ def custom_sort(string):
     if len(string) <= 1:
         return string
     base_letter = string[len(string) // 2]
-    middle = []
-    left = []
-    right = []
-    for letter in string:
-        if letter > base_letter:
-            right.append(letter)
-        elif letter < base_letter:
-            left.append(letter)
-        else:
-            middle.append(letter)
+
+    middle = [letter for letter in string if letter == base_letter]
+    left = [letter for letter in string if letter < base_letter]
+    right = [letter for letter in string if letter > base_letter]
+
     return custom_sort(left) + middle + custom_sort(right)
