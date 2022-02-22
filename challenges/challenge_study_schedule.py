13217414bcb5c070bbdecc3c58b71aa8554d5a3b
Filizzola <juliofilizzola@hotmail.com>
2022-02-22 13:39:58 -0300

    🔨: req = 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count_study = 0
+    if not target_time:
+        return None
 
-    ## Comentario de init project
+    for period_study in permanence_period:
+        permanence_zero = isinstance(period_study[0], int)
+        permanence_one = isinstance(period_study[1], int)
+        if not permanence_zero or not permanence_one:
+            return None
+
+        if permanence_zero <= target_time <= permanence_one:
+            count_study += 1
+    
+    return count_study
