c3c26b7aa607449fa7954064d5bacabdff1f9888
Marco Galindo <marco1408@gmail.com>
2022-02-22 18:22:26 -0300

    resolvendo o lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -5,13 +5,16 @@ def selection_sort(array):
         for j in range(i + 1, len(array)):
             if array[j] < array[minimum]:
                 minimum = j
-        
+
         array[minimum], array[i] = array[i], array[minimum]
 
     return array
 
+
 def is_anagram(first_string, second_string):
     try:
-        return selection_sort(list(first_string)) == selection_sort(list(second_string))
+        return selection_sort(
+            list(first_string)
+        ) == selection_sort(list(second_string))
     except ValueError:
         return False
