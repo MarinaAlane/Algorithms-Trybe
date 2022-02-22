505f91e89f972d66cbf7d7143946cceab5037326
holistic <paulofreire.si@outlook.com>
2022-02-22 16:20:16 -0300

    feat: req01 - search for the best time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,27 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """
+    Caso o target_time passado seja nulo, o valor
+    retornado pela função deve ser None
+    """
+    if not target_time:
+        return None
+    
+    return toocomplex(permanence_period, target_time)
+
+# https://www.flake8rules.com/rules/C901.html
+def toocomplex(permanence_period, target_time):
+    i = 0
+    try:
+        for values in permanence_period:
+            """
+            Dica: Quando vou saber qual o melhor horário?
+            Quando o contador retornado pela função for o maior.
+            """
+            if values[0] <= target_time <= values[1]:
+                i += 1
+    except TypeError:
+        """
+        Retorne None se em permanence_period houver alguma entrada inválida
+        """
+        return None
+    return i
