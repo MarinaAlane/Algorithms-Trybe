10ae10f0b68fde5d32370fbdbbdfc7220ddb9024
IsleneGomes <islenegomes@gmail.com>
2022-02-22 11:16:40 -0300

    feat:Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    elif low_index == high_index:
+        return True
+    elif low_index >= high_index:
+        return True
+    else:
+        return word[low_index] == word[high_index] and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
