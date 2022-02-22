bb8431b68a225dae67c2b983e62bfe9411ca3b16
Renan <renan.vamo@gmail.com>
2022-02-22 16:37:56 -0300

    add req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,25 @@
+def is_valid_list(list):
+    if not list or len(list) == 1 or len(set(list)) == len(list):
+        return False
+    return True
+
+
+def is_valid_number(number):
+    if type(number) == 'string' or number < 0:
+        return False
+    return True
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    if not is_valid_list(nums):
+        return False
+
+    for i, num in enumerate(nums):
+        if not is_valid_number(num):
+            return False
+
+        if num == nums[i + 1] or i == len(nums):
+            return num
+
+    return False
