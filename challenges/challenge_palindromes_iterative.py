ffba505c7324cca835117c318eb589034eef7a42
Pedro Piquini <piquini.pedro@gmail.com>
2022-02-22 17:24:38 -0300

    build: criado funcao iterativa que verifica se palavra e um palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    checker = ''
+    if len(word) < 1:
+        return False
+    elif len(word) == 1:
+        return True
+    for i in range(len(word)//2):
+        if word[i] == word[-i - 1]:
+            checker = True
+        else:
+            checker = False
+    return checker
