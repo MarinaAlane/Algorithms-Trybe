9e0fee9f04ac80b9408444d7354a93fabbe614fe
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-02-21 23:13:27 -0300

    refatora anagrams.py

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,16 @@
-def is_anagram(first_string, second_string):
+def create_dict(first_string):
     dict = {}
     for letter in first_string:
         if letter in dict:
             dict[letter] += 1
         else:
             dict[letter] = 1
+    return dict
+
+
+def is_anagram(first_string, second_string):
+    dict = {}
+    dict = create_dict(first_string)
     for letter in second_string:
         if letter in dict:
             dict[letter] -= 1
