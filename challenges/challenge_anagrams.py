226868761423d2af1053d1d13e5127fe0d2117d5
diogotrescastro <diogotrescastro@gmail.com>
2022-02-22 20:09:05 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,29 @@
+def quicksort(array, low, high):
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition_index = partition(array, low, high)
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+
+
+def partition(array, low, high):
+    i = low - 1
+    pivot = array[high]
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_arr = list(first_string)
+    second_arr = list(second_string)
+    quicksort(first_arr, 0, len(first_arr) - 1)
+    quicksort(second_arr, 0, len(second_arr) - 1)
+    return first_arr == second_arr
