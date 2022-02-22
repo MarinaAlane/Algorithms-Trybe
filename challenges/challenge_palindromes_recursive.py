fad80404279a74c52a491f2af39056e794733755
Filizzola <juliofilizzola@hotmail.com>
2022-02-22 15:11:28 -0300

    🔨: req = 2 ref comments 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,4 +5,4 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
-    #Miguel Campos me ajudou nesse requisito
+    """ Miguel Campos me ajudou nesse requisito """
