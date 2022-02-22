8ecfe038c815ee64227bbaf39bd6b88a15b402f0
benlopes <rubenssouss@hotmail.com>
2022-02-22 20:07:09 -0300

    feat: add anagram word checker function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. 1"""
+    return word_dict(first_string) == word_dict(second_string)
+
+
+def word_dict(word):
+    word_dict = dict()
+
+    for character in word:
+        if word_dict.get(character) is None:
+            word_dict[character] = 1
+        else:
+            word_dict[character] += 1
+
+    return word_dict
