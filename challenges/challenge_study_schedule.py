73c060c98071ad5162b4d7ceb6cec7cd880f7246
diogotrescastro <diogotrescastro@gmail.com>
2022-02-22 19:12:03 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui..."""
+    if type(target_time) is not int:
+        return None
+
+    contador = 0
+
+    for entrada, saida in permanence_period:
+        if type(entrada) is not int or type(saida) is not int:
+            return None
+        if entrada <= target_time <= saida:
+            contador += 1
+    return contador
