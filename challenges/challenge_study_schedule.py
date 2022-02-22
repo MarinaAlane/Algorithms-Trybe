177b80e4fd2188f5351f9bd960007120acd854a3
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-02-21 23:22:11 -0300

    refatora study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,12 +2,9 @@
 def study_schedule(permanence_period, target_time):
     count = 0
     for time in permanence_period:
-        if (
-            type(time[0]) != int
-            or type(time[1]) != int
-            or type(target_time) != int
-        ):
+        try:
+            if time[0] <= target_time and target_time <= time[1]:
+                count += 1
+        except TypeError:
             return None
-        if time[0] <= target_time and target_time <= time[1]:
-            count += 1
     return count
