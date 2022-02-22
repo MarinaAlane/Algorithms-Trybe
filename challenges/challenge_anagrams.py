61dea5b043d833ec5c9518e9b58bf3e05ab0e4e8
heyset <c.inaciomatheus@gmail.com>
2022-02-22 18:39:02 -0300

    req-3: refactor: muda merge do merge-sort para ser iterativo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,14 +2,10 @@ def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
         return False
 
-    first_sorted = merge_sort(get_chars(first_string))
-    second_sorted = merge_sort(get_chars(second_string))
+    first_sorted = merge_sort([char for char in first_string])
+    second_sorted = merge_sort([char for char in second_string])
 
     return first_sorted == second_sorted
-
-
-def get_chars(string):
-    return [char for char in string]
 
 
 def merge_sort(arr):
@@ -17,33 +13,29 @@ def merge_sort(arr):
         return arr
 
     if (len(arr) == 2):
-        return sort_list_with_two_elements(arr)
+        return arr if arr[0] <= arr[1] else [arr[1], arr[0]]
 
     middle_index = len(arr) // 2
     left = arr[:middle_index]
     right = arr[middle_index:]
-    return merge(merge_sort(left), merge_sort(right), [])
+    return merge(merge_sort(left), merge_sort(right))
 
 
-def sort_list_with_two_elements(arr):
-    return arr if arr[0] <= arr[1] else [arr[1], arr[0]]
+def merge(arr1, arr2):
+    merged = []
 
+    while arr1 and arr2:
+        if not arr1:
+            merged.extend(arr2)
+            break
 
-def merge(arr1, arr2, merged):
-    if len(arr1) == 0 and len(arr2) == 0:
-        return merged
+        if not arr2:
+            merged.extend(arr1)
+            break
 
-    if len(arr1) == 0:
-        merged.extend(arr2)
-        return merged
+        if (arr2[0] < arr1[0]):
+            merged.append(arr2.pop(0))
+        else:
+            merged.append(arr1.pop(0))
 
-    if len(arr2) == 0:
-        merged.extend(arr1)
-        return merged
-
-    if (arr1[0] < arr2[0]):
-        merged.append(arr1.pop(0))
-    else:
-        merged.append(arr2.pop(0))
-
-    return merge(arr1, arr2, merged)
+    return merged
