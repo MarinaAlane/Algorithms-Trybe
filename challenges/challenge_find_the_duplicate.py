b5b68cdd303f5891bcc3ab5a398ec646e4280d16
Paulo Henrique Assis <pieit@live.com>
2022-02-22 17:00:50 -0300

    Req4 ready

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
+nums = [-1, -1]
+from collections import defaultdict
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    dictionary = defaultdict(int)
+    for i in nums:
+        try:
+            if i < 0:
+                return False
+            else:
+                dictionary[i] += 1
+                if dictionary[i] > 1:
+                    return i
+        except Exception as e:
+            print(e)
+            return False
+    return False
