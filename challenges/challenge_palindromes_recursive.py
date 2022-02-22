dfb381210b7b254205c548f85f7fdffc1853e915
Jessica Arruda <sicarruda@gmail.com>
2022-02-22 17:35:09 -0300

    req 2 - diminuindo complexidade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,14 @@
+def comparation(word, low_index, high_index):
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index+1, high_index-1)
+    else:
+        return False
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     try:
         if (low_index - high_index) < 2: 
-            if word[low_index] == word[high_index]:
-                return is_palindrome_recursive(word, low_index+1, high_index-1)
-            else:
-                return False
-        return True
-    except TypeError:
-        return False
+            return comparation(word, low_index, high_index)
+        return True      
     except IndexError:
         return False
