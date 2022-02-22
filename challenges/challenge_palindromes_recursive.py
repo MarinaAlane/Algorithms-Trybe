a7f33909a879ecf6e6f56806408407f130acd737
Daniel <danielfdoficial@gmail.com>
2022-02-22 11:51:28 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
+from pickle import FALSE, TRUE
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    return True and is_palindrome_recursive(word, low_index + 1, high_index - 1)
