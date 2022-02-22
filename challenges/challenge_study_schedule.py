cda8df5952076c717f164623e7bc90c283ca28ec
Marco Galindo <marco1408@gmail.com>
2022-02-22 17:45:13 -0300

    requisito 1 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    student_count = 0
+    try:
+        for schedule in permanence_period:
+            if schedule[0] <= target_time <= schedule[1]:
+                student_count += 1
+        return student_count
+    except TypeError:
+        return None
+
+# print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
