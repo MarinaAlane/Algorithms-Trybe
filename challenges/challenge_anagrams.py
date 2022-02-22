f65725eb6e297503dbf7b81012fe4ec431a4b617
gab-boff <gmeninboff@gmail.com>
2022-02-22 19:37:28 -0300

    feat: anagrams function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def char_dict(string):
+    string_dict = dict()
+
+    for letter in string:
+        try:
+            string_dict[letter] += 1
+        except KeyError:
+            string_dict[letter] = 1
+    
+    return string_dict
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    if len(first_string) != len(second_string):
+        return False
+
+    return char_dict(first_string) == char_dict(second_string)
