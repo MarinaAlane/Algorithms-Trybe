ece66c7c41a5282503772ead00e7595acd77f2a2
Thyago Pessoa <flash_thyago@hotmail.com>
2022-02-22 18:01:49 -0300

    Req 1 - fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,8 @@ def study_schedule(permanence_period, target_time):
     try:
         counter = 0
         for period_one, period_two in permanence_period:
-            if period_one <= target_time <= period_two: counter += 1
+            if period_one <= target_time <= period_two:
+                counter += 1
         return counter
     except TypeError:
         return None
