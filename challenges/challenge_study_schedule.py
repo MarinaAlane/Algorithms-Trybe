18afee6338938ac0bbd62d79140ca6c9b1d12842
diogotrescastro <diogotrescastro@gmail.com>
2022-02-22 20:20:05 -0300

    teste de performance req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,7 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-
     contador = 0
-
     for entrada, saida in permanence_period:
         if type(entrada) is not int or type(saida) is not int:
             return None
