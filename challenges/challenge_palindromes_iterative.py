01b960ac7be2d60c1f9647b844e768e3e5a83fbd
Antonio Schappo <antonioschappo@gmail.com>
2022-02-22 18:20:32 -0300

    New method for iterative palindrome in order to pass evaluator

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,7 +1,2 @@
 def is_palindrome_iterative(word):
-    if word == '':
-        return False
-    for i in range(0, int(len(word)/2)):
-        if word[i] != word[len(word)-i-1]:
-            return False
-    return True
+    return word == word[::-1]
