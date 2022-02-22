e2dcb4e432c9e47399c32c78b7f2d24c4e196be6
Matheus <matheusaraujo.dev@gmail.com>
2022-02-22 19:30:02 -0300

    Add: Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # Palavra vazia retorna False
+    if word == '':
+        return False
+
+    # Se as letras forem diferentes não é palíndromo.
+    if word[low_index] != word[high_index]:
+        return False
+
+    # Se a palavra for um palíndromo par...
+    # ... o low_index será maior que o high em algum momento.
+    # Se for um palíndromo ímpar os índices serão iguais na letra do meio.
+    if low_index >= high_index:
+        return True
+
+    # Para verificar as proximas letras, deve-se...
+    # Adicionar +1 ao low_index e subtrair -1 do high_index.
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
