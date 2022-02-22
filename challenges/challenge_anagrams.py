f93d278e8335711836469b2447952f092b99a936
IsleneGomes <islenegomes@gmail.com>
2022-02-22 13:11:23 -0300

    Problemas no range

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,25 +1,34 @@
 # https://panda.ime.usp.br/panda/static/pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
 def solucaoAnagrama(s1, s2):
-    c1 = [0]*26
-    c2 = [0]*26
-
+    pos = 0
+    c1 = [pos]*26
+    c2 = [pos]*26
     for i in range(len(s1)):
+    # contar o número de vezes que cada caractere ocorre
+        # ord recebe uma letra e retorna o código ASCII da mesma
         pos = ord(s1[i])-ord('a')
+        # Cada vez que encontramos um caractere em particular,
+        # incrementamos o seu contador naquela posição
         c1[pos] = c1[pos] + 1
-
+        
     for i in range(len(s2)):
+        # ord recebe uma letra e retorna o código ASCII da mesma
         pos = ord(s2[i])-ord('a')
+        # Cada vez que encontramos um caractere em particular,
+        # incrementamos o seu contador naquela posição
         c2[pos] = c2[pos] + 1
 
     j = 0
-    aindaOK = True
-    while j < 26 and aindaOK:
+    anagram = True
+    while j < 26 and anagram:
+        # se as duas listas de contadores são idênticas, as strings são anagramas
         if c1[j] == c2[j]:
-            j = j + 1
+            j += 1
+            return anagram
         else:
-            aindaOK = False
+            anagram = False
 
-    return aindaOK
+    return anagram
 
 
 def is_anagram(first_string, second_string):
