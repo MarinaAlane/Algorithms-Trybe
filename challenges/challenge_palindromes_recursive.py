1e05398bf356d89f2e89a4d845d7994cf239fc0e
Jessica Arruda <sicarruda@gmail.com>
2022-02-22 17:25:55 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if (low_index - high_index) < 2: 
+            if word[low_index] == word[high_index]:
+                return is_palindrome_recursive(word, low_index+1, high_index-1)
+            else:
+                return False
+        return True
+    except TypeError:
+        return False
+    except IndexError:
+        return False
