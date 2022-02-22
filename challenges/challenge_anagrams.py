c4d5e73203ea2834fc531bb16b657fcf0962206c
Marco Galindo <marco1408@gmail.com>
2022-02-22 18:19:43 -0300

    req 3 finalizado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        
+        array[minimum], array[i] = array[i], array[minimum]
+
+    return array
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    try:
+        return selection_sort(list(first_string)) == selection_sort(list(second_string))
+    except ValueError:
+        return False
