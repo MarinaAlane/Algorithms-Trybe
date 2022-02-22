5554694119cf6d6481f34790fb3a0862be327297
Filizzola <juliofilizzola@hotmail.com>
2022-02-22 13:51:23 -0300

    🔨: req = 4 retorn none ref 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -9,7 +9,7 @@ def study_schedule(permanence_period, target_time):
         if not permanence_zero or not permanence_one:
             return None
 
-        if permanence_zero <= target_time <= permanence_one:
+        if period_study[0] <= target_time <= period_study[1]:
             count_study += 1
 
     return count_study
