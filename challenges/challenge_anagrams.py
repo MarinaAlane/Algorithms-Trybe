6fbdf1d7dfaa9d938507b6639f8d3464a33feac1
Paulo Henrique Assis <pieit@live.com>
2022-02-22 16:36:50 -0300

    Added req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+from collections import defaultdict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    anagram = defaultdict(int)
+    if len(first_string) != len(second_string):
+        return False
+    for char in first_string:
+        anagram[char] += 1
+    for char in second_string:
+        anagram[char] -= 1
+        if anagram[char] < 0:
+            return False
+    return True
