0fa20d27625ef52400fed5996dc4b5fbb23dba68
Ivaldo Souza <souza1976ivaldo@gmail.com>
2022-02-23 18:42:06 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if not word:
+            return False
+        elif low_index >= high_index:
+            return word[low_index] == word[high_index]
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    except TypeError:
+        return False
