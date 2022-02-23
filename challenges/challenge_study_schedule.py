27712e6ff253549f4591703b041d43aa3339302d
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-02-23 17:56:43 -0300

    primeiro requisito feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    contador = 0
+    if (target_time is None):
+        return None
+    for i in permanence_period:
+        if(type(i[0]) is not int or type(i[1]) is not int):
+            return None
+        if(i[0] <= target_time and target_time <= i[1]):
+            contador += 1
+    return contador
