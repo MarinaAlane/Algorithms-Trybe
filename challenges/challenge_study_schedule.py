d2322f54434e139a4cad1196a79f31ef119e2fba
Ivaldo Souza <souza1976ivaldo@gmail.com>
2022-02-23 08:54:40 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count_time = 0
+    try:
+        for study_time in permanence_period:
+            check_in_time = study_time[0]
+            check_out_time = study_time[1]
+            if check_in_time <= target_time <= check_out_time:
+                count_time += 1
+        return count_time
+
+    except TypeError:
+        return None
