86531c3210e2efb107b19342c97aeb4ba5144817
benlopes <rubenssouss@hotmail.com>
2022-02-23 17:16:23 -0300

    feat: add study schedule checker function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_online = 0
+
+    for log in permanence_period:
+        try:
+            if log[0] <= target_time <= log[1]:
+                students_online += 1
+        except TypeError:
+            return None
+    return students_online
