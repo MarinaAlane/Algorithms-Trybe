d6ef9c3a4a949925e3f056bd5ba30b469a116118
Filizzola <juliofilizzola@hotmail.com>
2022-02-23 09:14:33 -0300

    ✨: req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,32 @@
 def is_anagram(first_string, second_string):
-    string_validy = (first_string == "" or second_string == "")
-    length_validy = len(first_string) != len(second_string)
-    if string_validy or length_validy:
+    if valid_anagram(split(first_string)) == valid_anagram(split(second_string)):
+        return True
+    else:
         return False
+
+"""Miguel campos me ajudou na complexidade dessa questão"""
+
+def split(word):
+    return [char for char in word]
+
+
+def valid_anagram(sequence):
+    if len(sequence) <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+
+    return valid_anagram(items_lower) + [pivot] + valid_anagram(items_greater)
+
+
+def split(word):
+    return [char for char in word]
