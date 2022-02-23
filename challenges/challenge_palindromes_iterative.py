73f6bd626adb53a044ba57d242025d4ffd7ef3b2
holistic <paulofreire.si@outlook.com>
2022-02-22 21:20:32 -0300

    feat: is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    check_palindrome = "".join(char.lower() for char in word if char.isalnum())
+    return check_palindrome == check_palindrome[::-1]
+# Code based in this article:
+# https://www.pythonlikeyoumeanit.com/Module2_EssentialsOfPython/Problems/Palindrome.html
