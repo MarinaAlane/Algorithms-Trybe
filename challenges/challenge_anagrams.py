72b9046fa9770e202ab4182ad96778c60f133632
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-02-23 22:20:45 -0300

    FEAT: Requirement 3 - is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
+def character_counter(string):
+    character_dict = dict()
+
+    for char in string:
+        if (char in character_dict):
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+
+    return character_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (len(first_string) != len(second_string)):
+        return False
+
+    character_dict = character_counter(first_string + second_string)
+
+    for value in character_dict.values():
+        if (value % 2 != 0):
+            return False
+
+    return True
