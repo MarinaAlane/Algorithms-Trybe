f940da76d379f29785747e8887a0a949226431fa
¨Hugosomers¨ <hugosomersla@gmail.com>
2022-02-24 17:39:02 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,44 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    
+    first = sort(list(first_string))
+    second = sort(list(second_string))
+
+    if first == second:
+        return True
+    return False
+
+def sort(word):
+    if len(word) <= 1:
+        return word
+
+    mid = len(word) // 2
+
+    left = sort(word[:mid])
+    right = sort(word[mid:])
+
+    return merge(left, right, word.copy())
+
+def merge(left, right, merged):
+
+    left_pst = 0
+    right_pst = 0
+
+    while left_pst < len(left) and right_pst < len(right):
+        if left[left_pst] <= right[right_pst]:
+            merged[left_pst + right_pst] = left[left_pst]
+            left_pst += 1
+        else:
+            merged[left_pst + right_pst] = right[right_pst]
+            right_pst += 1
+
+    for left_pst in range(left_pst, len(left)):
+        merged[left_pst + right_pst] = left[left_pst]
+
+    for right_pst in range(right_pst, len(right)):
+        merged[left_pst + right_pst] = right[right_pst]
+
+    return merged
+
+# Peguei uma ajuda do course rsrs
