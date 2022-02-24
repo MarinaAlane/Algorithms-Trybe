3612ea25aab772df4588177fff7b74bb8301322b
felipebuso <felipe_buso@hotmail.com>
2022-02-24 08:43:37 -0300

    fix: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,10 +3,10 @@ def study_schedule(permanence_period, target_time):
         return None
     count = 0
     for period in permanence_period:
-        if type(period[0]) != int or type(period[1]) != int:
-            return None
-        else:
+        try:
             if period[0] <= target_time and period[1] >= target_time:
                 count += 1
+        except TypeError:
+            return None
 
     return count
