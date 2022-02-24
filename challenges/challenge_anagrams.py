a6f12938965b5a19f253dba2f51207e764f7d01c
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2022-02-24 15:41:19 -0300

    Feat: Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    firstWorldDictionary = dict()
+    secondWorldDictionary = dict()
+
+    for character in first_string:
+        firstWorldDictionary[character] = firstWorldDictionary.get(character, 0) + 1
+    for character in second_string:
+        secondWorldDictionary[character] = secondWorldDictionary.get(character, 0) + 1
+
+    return firstWorldDictionary == secondWorldDictionary
