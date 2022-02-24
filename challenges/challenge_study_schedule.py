e32e68f5dfbc896e86c51ca193741f95d9b5269d
felipebuso <felipe_buso@hotmail.com>
2022-02-24 08:30:35 -0300

    refactor: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,8 +5,8 @@ def study_schedule(permanence_period, target_time):
     for period in permanence_period:
         if type(period[0]) != int or type(period[1]) != int:
             return None
-    for period in permanence_period:
-        if period[0] <= target_time and period[1] >= target_time:
-            count += 1
+        else:
+            if period[0] <= target_time and period[1] >= target_time:
+                count += 1
 
     return count
