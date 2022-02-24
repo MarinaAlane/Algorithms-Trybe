01c6f8182249f179c61cfe4e34f2dc1cc7300fa8
Luciano <luciano@desrotulando.com>
2022-02-24 20:12:54 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,9 @@ def study_schedule(permanence_period, target_time):
         return None
     amount = 0
     for first, last in permanence_period:
-        if (type(first) is not int or type(last) is not int):
-            return None
+        if type(first) is not int or type(last) is not int:
+            amount = None
+            break
         if first <= target_time <= last:
             amount += 1
     return amount
