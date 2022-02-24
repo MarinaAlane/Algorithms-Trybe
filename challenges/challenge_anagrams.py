9bd2dab5dec4324e9dd9c29e9fde93c4b5c92be7
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2022-02-24 15:45:59 -0300

    Fix: Lint, tamanho das linhas, mudando nome das variaveis.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,11 @@
 def is_anagram(first_string, second_string):
     # https://www.javatpoint.com/python-dict-function
-    firstWorldDictionary = dict()
-    secondWorldDictionary = dict()
+    firstWord = dict()
+    secondWord = dict()
 
-    for character in first_string:
-        firstWorldDictionary[character] = firstWorldDictionary.get(character, 0) + 1
-    for character in second_string:
-        secondWorldDictionary[character] = secondWorldDictionary.get(character, 0) + 1
+    for letter in first_string:
+        firstWord[letter] = firstWord.get(letter, 0) + 1
+    for letter in second_string:
+        secondWord[letter] = secondWord.get(letter, 0) + 1
 
-    return firstWorldDictionary == secondWorldDictionary
+    return firstWord == secondWord
