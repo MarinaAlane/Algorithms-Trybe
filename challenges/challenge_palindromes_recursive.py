9a3d3ef21d2ca35452de0b9f1c7a67f7a6c91392
¨Hugosomers¨ <hugosomersla@gmail.com>
2022-02-24 16:46:08 -0300

    Fix lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,7 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == '' or (word[low_index] != word[high_index]):
         return False
 
-    if low_index >= (len(word) -1)/2:
+    if low_index >= (len(word) -1) / 2:
         return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
