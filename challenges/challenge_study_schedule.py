620f0884e0dc5785ca197346d4613eee229ba077
Victor Faria <vitilevi@hotmail.com>
2022-02-24 08:15:42 -0300

    Adiciona requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    ...
+    number_of_students = 0
+    
+    for clockIn, clockOut in permanence_period:
+        if (type(clockIn) != int or type(clockOut) != int) or not target_time:
+            return None
+        
+        if clockIn <= target_time <= clockOut:
+            number_of_students += 1
+    
+    return number_of_students
