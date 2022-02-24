1d8590d74b7a375ea8aa7dc04fe1fb8d0e858dbf
¨Hugosomers¨ <hugosomersla@gmail.com>
2022-02-24 16:00:35 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    
+    counter = 0
+
+    for singleTime in permanence_period:
+        if(type(singleTime[0]) is not int or type(singleTime[1]) is not int):
+            return None
+        if singleTime[0] <= target_time <= singleTime[1]:
+            counter += 1
+
+    return counter
