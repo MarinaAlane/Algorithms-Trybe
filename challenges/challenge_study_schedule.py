111787cca775d1e4720ff0de35c09b0183f529d3
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2022-02-24 14:40:44 -0300

    Feat: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        numCounter = 0
+        for firstPeriod, secondPeriod in permanence_period:
+            if firstPeriod <= target_time <= secondPeriod:
+                numCounter += 1
+        return numCounter
+    except TypeError:
+        return None
