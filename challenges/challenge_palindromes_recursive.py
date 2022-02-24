daaaf9eeac21a2da6b3e9d735197a3b0ad811ac2
jorgemeyrelles <jotaengpuc@gmail.com>
2022-02-24 15:45:44 -0300

    feat: primeiro commit

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
+from audioop import reverse
+
+
 def is_palindrome_recursive(word, low_index, high_index):
+    try:
+        if (word[low_index] != word[high_index] or len(word) == 0):
+            return False
+        if (low_index >= high_index or len(word) < 0):
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except (TypeError, IndexError):
+        return False
     """ Faça o código aqui. """
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
+    try:
+        rep = 0
+        for start, end in permanence_period:
+            if (start <= target_time <= end):
+                rep += 1
+    except TypeError:
+        return None
+    return rep
     """ Faça o código aqui. """
