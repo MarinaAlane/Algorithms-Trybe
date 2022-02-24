8dc8a4d3e4e09946e44bd0d9f7e252d8099753fe
Luciano <luciano@desrotulando.com>
2022-02-24 20:05:54 -0300

    Req 1 - Evaluator error

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    # 1.3
     if not target_time:
         return None
-
     amount = 0
     for first, last in permanence_period:
-        # 1.2
         if (type(first) is not int or type(last) is not int):
             return None
-
-        # 1.1
         if first <= target_time <= last:
             amount += 1
-
     return amount
