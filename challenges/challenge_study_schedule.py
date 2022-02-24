c9fc0cca97d20256ed39cd16f1a9ad9aec6bef06
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-02-23 21:04:35 -0300

    FEAT: Requirement 1 - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    # Como validar um tipo no Python:
+    # https://www.delftstack.com/pt/howto/python/python-check-variable-type/
+
+    for entranceTime, exitTime in permanence_period:
+        if not isinstance(entranceTime, int) or not isinstance(exitTime, int):
+            return None
+        if (entranceTime <= target_time <= exitTime):
+            counter += 1
+
+    return counter
