aad497be01d8a9b388fbc724552912b81ea20916
josanjohnata <josan.pontes@meliuz.com.br>
2022-02-25 15:21:02 -0300

    Refatora requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,9 @@
 # https://www.youtube.com/watch?v=92UyahoAne4
 def is_palindrome_recursive(word, low_index, high_index):
     # base case
-    if not word:
+    if not word or word[low_index] != word[high_index]:
         return False
-    if (word[low_index] != word[high_index]):
-        return False
-    if len(word) == 1:
-        return True
-    if low_index >= high_index:
+    if len(word) == 1 or low_index >= high_index:
         return True
     else:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
