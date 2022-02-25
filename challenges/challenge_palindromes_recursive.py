3310395a819364cb114ea591018aba6330497ab3
CIGR2021 <appencigr@gmail.com>
2022-02-25 11:48:23 -0300

    Fix: Linter, identação 'linha 6'

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,8 +2,10 @@ def is_palindrome_recursive(word, low_index, high_index):
     try:
         if low_index >= (len(word) // 2) - 1:
             return word[low_index] == word[high_index]
-        return (word[low_index] == word[high_index]
-            and is_palindrome_recursive(word, low_index + 1, high_index - 1))
+        return (
+            word[low_index] == word[high_index]
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        )
     except ValueError:
         return False
     except IndexError:
