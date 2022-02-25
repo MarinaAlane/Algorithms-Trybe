ed64a5abc5430ea2399a27e6d52839c75f48339c
CamilaCode7 <camilafabiabh9@gmail.com>
2022-02-25 11:16:25 -0300

    adicionando try e except passou todos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    counter = 0
-    if not target_time:
-        return None
-    for item in permanence_period:
-        if item[0] <= target_time and target_time <= item[1]:
-            counter += 1
-    return counter
-
-
-print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 3))
+    try:
+        counter = 0
+        if not target_time:
+            return None
+        for item in permanence_period:
+            if item[0] <= target_time and target_time <= item[1]:
+                counter += 1
+        return counter
+    except Exception:
+        None
