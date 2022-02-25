0b3215cbbc3d74309f6bc1aa31274f20a1b36a5d
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 14:08:22 -0300

    reenviando req. 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,7 @@ def study_schedule(permanence_period, target_time):
         for start_time, end_time in permanence_period:
             if not target_time:
                 return None
-            if start_time <= target_time and target_time <= end_time:
+            if start_time <= target_time <= end_time:
                 count += 1
         return count
     except TypeError:
