f843b63b92c2e4fad14299a9c6164ba6381d5574
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-25 01:45:01 -0300

    corrige erros de identacao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,8 +4,7 @@ def study_schedule(permanence_period, target_time):
 
     count = 0
     for i in permanence_period:
-    
         if i[0] and i[1] != int:
-         return None
+            return None
 
     return count
