db99a65c288f3a1d7b0ba38c2879d10c04c2c961
Erick-Marinho <erickgaves@gmail.com>
2022-02-25 15:23:59 -0300

    :sparkles: Imlementando funçao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        result = [
+                (i,j) for i, j in permanence_period if i <= target_time and target_time <= j 
+            ]
+        return len(result)
+    except TypeError:
+        return None
