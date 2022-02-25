8b83fdc74d27ee2b77a3b2581950937901beaba2
CamilaCode7 <camilafabiabh9@gmail.com>
2022-02-25 15:06:04 -0300

    adicionando logica do req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    for first_num in range(0, len(nums)):
+        for second_num in range(1, len(nums)):
+            if nums[first_num] == nums[second_num] and first_num != second_num:
+                print(f"sozinho {first_num}")
+                print(f"sozinho {second_num}")
+                return nums[first_num]
+
+
+print(find_duplicate([1, 2, 3, 4, 2]))
