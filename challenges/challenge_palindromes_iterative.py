1dbdce85547ac4ac452a6b8d707fc528461df626
felipebuso <felipe_buso@hotmail.com>
2022-02-25 10:24:04 -0300

    feat: requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,15 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    l_index = 0
+    r_index = len(word) - 1
+
+    while l_index < r_index:
+        if word[l_index] != word[r_index]:
+            return False
+        else:
+            l_index += 1
+            r_index -= 1
+    return True
