e74e09cbc05101925b012c346d40a68477851d05
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-02-25 20:58:10 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,32 +1,13 @@
-from operator import truediv
+def build_counts(string):
+    words = dict()
+    for l in string:
+        words[l] = words.setdefault(l, 0) + 1
+
+    return words
 
 
-def check_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-    anagram = dict()
-    if len(first_string) != len(second_string):
-        return False
+def is_anagram(string1, string2):
+    word1 = build_counts(string1)
+    word2 = build_counts(string2)
 
-    for i in first_string:
-        if i not in anagram:
-            anagram[i] = 0
-            anagram[i] += 1
-
-    for i in second_string:
-        if i not in anagram:
-            return False
-        else:
-            anagram[i] -= 1
-
-    for i in anagram:
-        if anagram[i] > 0 or anagram[i] < 0:
-            return False
-
-    return True
-
-
-def is_anagram(first_string, second_string):
-    if check_anagram(first_string, second_string):
-        return True
-    else:
-        return False
+    return word1 == word2
