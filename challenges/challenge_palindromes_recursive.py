62b3ba161fd6fe48966bdebd8fa7e1bc0efa8c80
Erick-Marinho <erickgaves@gmail.com>
2022-02-25 16:24:12 -0300

    :rotating_light:

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,6 +4,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if not isinstance(word, str):
         return False
-    if low_index < high_index: 
+    if low_index < high_index:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
