152409dc136b1a93575aba5356221f66db1a14f8
Luciano <luciano@desrotulando.com>
2022-02-24 21:14:06 -0300

    Req 3 - nova tentativa

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,18 @@
-from helpers.sort import sort
+def sort_string(word):
+    array = list(word)
+    array_sorted = []
+
+    while array:
+        order = min(array)
+        array_sorted.append(order)
+        array.pop(array.index(order))
+
+    return "".join(array_sorted)
 
 
 def is_anagram(first_string, second_string):
 
-    if sort(first_string) == sort(second_string):
+    if sort_string(first_string) == sort_string(second_string):
         return True
 
     if not first_string or not second_string:
