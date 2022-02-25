08519d8221bc14fbd8326534ebd8f85ba069a662
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 14:18:46 -0300

    reenviando req. 1, agora vai dar certo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
+    if not target_time:
+        return None
     count = 0
     try:
         for start_time, end_time in permanence_period:
-            if not target_time:
-                return None
-            if start_time <= target_time <= end_time:
+            if start_time <= target_time and target_time <= end_time:
                 count += 1
         return count
     except TypeError:
