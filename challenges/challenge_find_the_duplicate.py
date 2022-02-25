233ddf4a6b9b19399240fdb9cd3ddb2bcffe2b07
felipebuso <felipe_buso@hotmail.com>
2022-02-25 09:34:39 -0300

    feat: requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,23 @@
+def check_is_valid_array(nums):
+    try:
+        for num in nums:
+            if num < 0:
+                return False
+    except TypeError:
+        return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+
+    if len(nums) <= 1:
+        return False
+
+    if check_is_valid_array(nums) is False:
+        return False
+
+    sorted_nums = sorted(nums)
+
+    if sorted_nums[0] == sorted_nums[1]:
+        return sorted_nums[0]
+    else:
+        return find_duplicate(sorted_nums[1:])
