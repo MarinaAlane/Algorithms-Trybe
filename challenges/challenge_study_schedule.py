957995c6625ccd36d6b914241d01eb8a5020cfd3
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2022-02-25 14:40:01 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for x1, x2 in permanence_period:
+            if x1 <= target_time <= x2:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
+
+if __name__ == '__main__':
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
