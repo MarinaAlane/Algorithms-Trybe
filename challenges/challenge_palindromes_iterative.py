f738ce7b194cda816d50eb990818717095bf66cb
felipebuso <felipe_buso@hotmail.com>
2022-02-25 15:49:38 -0300

    refactor: requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,15 +1,15 @@
 def is_palindrome_iterative(word):
     if len(word) == 0:
         return False
-    if len(word) == 1:
-        return True
-    l_index = 0
-    r_index = len(word) - 1
+    try:
+        l_index, r_index = 0, len(word) - 1
 
-    while l_index < r_index:
-        if word[l_index] != word[r_index]:
-            return False
-        else:
-            l_index += 1
-            r_index -= 1
-    return True
+        while l_index < r_index:
+            if word[l_index] != word[r_index]:
+                return False
+            else:
+                l_index += 1
+                r_index -= 1
+        return True
+    except ValueError:
+        return False
