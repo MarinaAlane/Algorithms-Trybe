302119367ed8ff899e863e8ac8e5165f231b3d1f
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-02-25 11:05:52 -0400

    Corrigi erro de lint.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,6 +5,6 @@ def study_schedule(permanence_period, target_time):
     for login, logout in permanence_period:
         if type(login) is not int or type(logout) is not int:
             return None
-        if (login <= target_time <= logout):
+        if login <= target_time <= logout:
             counter += 1
     return counter
