b0702e42a5a19427d8941a04c6300c68ded70804
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-25 02:12:21 -0300

    finaliza req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,3 +6,8 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     if word[low_index] != word[high_index]:
         return False
+    if high_index <= (len(word) - 1) / 2:
+        return True
+
+    return (True
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1))
