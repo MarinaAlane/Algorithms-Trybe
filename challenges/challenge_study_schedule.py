e18dcc835af8219d64dfd95c928bb9610899239c
tonpasso <wellpasso@gmail.com>
2022-02-25 20:58:30 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,8 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    pass
+    try:
+        while(permanence_period):
+            contador = [item for item in permanence_period if item[0] <= target_time and item[1] >= target_time]
+            return len(contador)
+    except TypeError:
+        return None
