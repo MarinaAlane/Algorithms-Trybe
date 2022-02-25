e2b07c0e8177d5fd0945330947e3486b843455c0
gabiazevedo <gabiazevedoms@gmail.com>
2022-02-25 19:55:16 -0300

    feat: Corrige requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,17 +1,14 @@
-def validate_nums(nums):
-    if (type(nums) != int) or (nums == '') or (len(nums) == 1) or (nums < 0):
-        return False
-
-
 def find_duplicate(nums):
-    if not validate_nums(nums):
-        return False
+    for index in nums:
+        if (type(index) != int) or (len(nums) == 1) or (index < 0):
+            return False
 
     nums.sort()
 
-    if len(set(nums)) != len(nums):
-        for num in range(len(nums)):
-            if nums[num] == nums[num + 1]:
-                return nums[num]
-
+    for num in range(len(nums) - 1):
+        if nums[num] == nums[num + 1]:
+            return nums[num]
     return False
+
+
+print(find_duplicate([1, 3, 4, 2, 2]))
