63c735ffa20716048117d901f6a3f1386ed38e3c
CIGR2021 <appencigr@gmail.com>
2022-02-25 11:00:10 -0300

    Implementação do método study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        numberOfStudentsPresent = 0
+        for period in permanence_period:
+            if period[0] <= target_time and period[1] >= target_time:
+                numberOfStudentsPresent += 1
+        return numberOfStudentsPresent
+    except:
+        return None
