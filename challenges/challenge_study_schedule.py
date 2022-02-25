77d22d1219c73e6c99e10a8f1409d54bf3a7ee72
Marconi <marconieng@gmail.com>
2022-02-25 20:50:32 -0300

    Adiciona requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    if target_time is None: return None
+    contador = 0
+    for p in permanence_period:
+        if (type(p[0]) != int or p[0] is None or type(p[0]) != int or p[1] is None): return None
+        if target_time >= p[0] and target_time <= p[1]:
+            contador += 1
+    return contador
