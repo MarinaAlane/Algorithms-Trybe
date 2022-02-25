5d7df60c325f7fa0a4a534ff482bbd9430c7ec2e
Jessica Arruda <sicarruda@gmail.com>
2022-02-25 19:04:32 -0300

    req 3 - alteraçao de ordenaçao e comparaçao de array para comparaçao de objetos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,41 +1,31 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    array_first_string = selection_sort(first_string)
-    array_second_string = selection_sort(second_string)
-    for i in range(len(array_first_string)):
-        if array_first_string[i] == array_second_string[i]:
-            return True
-        else:
+    obj_1 = pop_obj(first_string)
+    obj_2 = pop_obj(second_string)
+    array_keys = list(obj_1.keys())
+    return compare_strings(array_keys, obj_1, obj_2)
+
+
+def compare_strings(array_keys, obj_1, obj_2):
+    for i in array_keys:
+        try:
+            if obj_1[i] != obj_2[i]:
+                return False
+        except KeyError:
             return False
+    return True
 
 
-# https://app.betrybe.com/course/computer-science/algoritmos/
-#   algoritmos-de-ordenacao-e-busca/
-#   29521083-44ea-488d-a74d-216b1ac79b04/conteudos/
-#   60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/
-#   fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
-#   codigo de ordenação do curso da Trybe com a adição do
-#   append para separa as letras
-def selection_sort(word):
-    # como um algoritmo de força bruta
-    # percorre a estrutura exaustivamente
-    word_array = []
+def pop_obj(word):
+    obj = {}
+    reference = 0
     for letter in word:
-        word_array.append(letter)
-    for i in range(len(word_array)):
-        minimum = i
-        # itera sobre os elementos não ordenados
-        for j in range(i + 1, len(word_array)):
-            # seleciona o menor valor
-            if word_array[j] < word_array[minimum]:
-                minimum = j
-        # após encontrar o menor valor
-        # ao invés de criar um novo array
-        # (o que aumenta complexidade de espaço)
-        # realizamos a substituição entre o menor elemento
-        # e a posição i que corresponde ao primeiro elemento não ordenado
-        # que consequentemente passará a ser o último ordenado
-        word_array[minimum], word_array[i] = word_array[i], word_array[minimum]
-
-    return word_array
+        obj[letter] = 0
+    array_keys = list(obj.keys())
+    while reference < len(array_keys):
+        for i in range(len(word)):
+            if array_keys[reference] == word[i]:
+                obj[word[i]] = obj[word[i]] + 1
+        reference = reference + 1
+    return obj
