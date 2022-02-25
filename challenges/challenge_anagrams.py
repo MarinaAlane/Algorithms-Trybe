854e97a86f6f02b177c6a5afff2d17fd348c5b73
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 19:28:51 -0300

    reenviando req. 3, com função anterior, avaliador tava bugado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,28 +1,35 @@
-def check_anagram(first_string, second_string):
-    words = dict()
-    if len(first_string) != len(second_string):
-        return False
+# ref:
+#  https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
 
-    for i in first_string:
-        if i not in words:
-            words[i] = 0
-            words[i] += 1
+# Ordenação por Seleção (Selection Sort)
+def selection_sort(strings):
+    # como um algoritmo de força bruta
+    # percorre a estrutura exaustivamente
+    array = list(strings)
+    for i in range(len(array)):
+        minimum = i
 
-    for i in second_string:
-        if i not in words:
-            return False
-        else:
-            words[i] -= 1
+        # itera sobre os elementos não ordenados
+        for j in range(i + 1, len(array)):
+            # seleciona o menor valor
+            if array[j] < array[minimum]:
+                minimum = j
 
-    for i in words:
-        if words[i] > 0 or words[i] < 0:
-            return False
+        # após encontrar o menor valor
+        # ao invés de criar um novo array (o que aumenta complexidade de espaço)
+        # realizamos a substituição entre o menor elemento
+        # e a posição i que corresponde ao primeiro elemento não ordenado
+        # que consequentemente passará a ser o último ordenado
+        array[minimum], array[i] = array[i], array[minimum]
 
-    return True
+    return array
+
+
+print(selection_sort('carro'))
 
 
 def is_anagram(first_string, second_string):
-    if check_anagram(first_string, second_string):
+    if selection_sort(first_string) == selection_sort(second_string):
         return True
     else:
         return False
