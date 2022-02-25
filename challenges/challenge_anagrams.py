4503d87d1a43c22acaa4a9583c05b37d33d15eb0
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 19:04:04 -0300

    reenviando req. 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,5 @@
-# ref: https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+# ref:
+# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
 
 # Ordenação por Seleção (Selection Sort)
 def selection_sort(string):
@@ -15,7 +16,8 @@ def selection_sort(string):
                 minimum = j
 
         # após encontrar o menor valor
-        # ao invés de criar um novo array (o que aumenta complexidade de espaço)
+        # ao invés de criar um novo array
+        # (o que aumenta complexidade de espaço)
         # realizamos a substituição entre o menor elemento
         # e a posição i que corresponde ao primeiro elemento não ordenado
         # que consequentemente passará a ser o último ordenado
