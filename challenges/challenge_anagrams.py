e54edb8e2d1450b686a16406d9d25da529cd815c
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 19:57:05 -0300

    retirei comentários pra corrigir o linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,23 +2,14 @@
 
 # Ordenação por Seleção (Selection Sort)
 def selection_sort(strings):
-    # como um algoritmo de força bruta
     array = list(strings)
-    # percorre a estrutura do array da lista exaustivamente
     for i in range(len(array)):
         minimum = i
-    # itera sobre os elementos não ordenados
+
     for j in range(i + 1, len(array)):
-        # seleciona o menor valor
         if array[j] < array[minimum]:
             minimum = j
 
-     # após encontrar o menor valor
-     # ao invés de criar um novo array
-     # (o que aumenta complexidade de espaço)
-     # realizamos a substituição entre o menor elemento
-     # e a posição i que corresponde ao primeiro elemento não ordenado
-     # que consequentemente passará a ser o último ordenado
     array[minimum], array[i] = array[i], array[minimum]
 
     return array
