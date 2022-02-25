890604225ab85dc58328be734255725b42013062
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-25 01:39:31 -0300

    corrige erros de identacao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
-            return None
+        return None
 
     count = 0
     for i in permanence_period:
         
-     if i [0] and i [1]!= int:
-               return None
+     if i[0] and i[1] != int:
+        return None
 
-     return count
+    return count
