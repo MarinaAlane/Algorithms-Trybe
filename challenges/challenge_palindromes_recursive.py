4b176fb32ac1bec6f521c0a25141cf64569ec745
Jhonata Braga <jhonatavbrg@gmail.com>
2022-02-25 07:42:36 -0300

    add req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if len(word) < 2 and len(word) > 0: return True
+        if word[low_index] != word[high_index]: return False
+        if low_index >= high_index: return word[low_index] == word[high_index]
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except ValueError:
+        return False
+    except IndexError:
+        return False
