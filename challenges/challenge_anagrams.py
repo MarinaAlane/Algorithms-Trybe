3b9b9c7a799d5786d8e7a192971e8346e9c55a24
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-02-25 19:17:07 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    anagram = dict()
+    if len(first_string) != len(second_string):
+        return False
+
+    for i in first_string:
+        if i not in anagram:
+            anagram[i] = 0
+            anagram[i] += 1
+
+    for i in second_string:
+        if i not in anagram:
+            return False
+        else:
+            anagram[i] -= 1
+
+    for i in anagram:
+        if anagram[i] > 0 or anagram[i] < 0:
+            return False
+
+    return True
