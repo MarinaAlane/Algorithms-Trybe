e598c4daadc9833d9eb14405b616011ac65bb4f9
Rodrigo <rodrigopython16@gmail.com>
2022-02-25 18:27:32 -0300

    added challenge anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_dict = dict()
+    sec_dict = dict()
+
+    for i in range(len(first_string)):
+        if  first_string[i] in first_dict:
+            first_dict[first_string[i]] += 1
+        else:
+            first_dict[first_string[i]] = 1
+
+        if second_string[i] in sec_dict:
+            sec_dict[second_string[i]] += 1
+        else:
+            sec_dict[second_string[i]] = 1
+
+    for i in range(len(first_string)):
+        if first_string[i] not in sec_dict:
+            return False
+        if(first_dict[first_string[i]] != sec_dict[first_string[i]]):
+            return False
+
+    return True
