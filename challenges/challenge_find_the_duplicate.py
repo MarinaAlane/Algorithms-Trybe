c0d2215465dc1b1da2f9d93e52220916abd0039b
gabiazevedo <gabiazevedoms@gmail.com>
2022-02-25 15:01:45 -0300

    feat: Sobe parte do requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
+def validate_nums(nums):
+    if (type(nums) != int) or (nums == '') or (len(nums) == 1) or (nums < 0):
+        return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not validate_nums(nums):
+        return False
+
+    nums.sort()
+
+    if len(set(nums)) != len(nums):
+        for num in range(len(nums)):
+            if nums[num] == nums[num + 1]:
+                return nums[num]
+
+    return False
