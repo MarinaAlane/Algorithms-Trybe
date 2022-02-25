e2725a17265bc2be08a8b9385ef628ed831d82de
Daniel Leon <daniel.radialista@gmail.com>
2022-02-25 18:44:53 -0300

    requisito 3, testando

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+from itertools import permutations
+
+
+def selection_sort(word):
+    return list(permutations(word))
+
+
+print(selection_sort('carro'))
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    string1 = selection_sort(first_string)
+    string2 = selection_sort(second_string)
+
+    if string1 == string2:
+        return True
+    else:
+        return False
