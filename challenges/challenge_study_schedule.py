4d8abf7e6753ff3dcb1b43e8e99eaef906ae024a
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-02-25 15:05:12 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    students = 0
+    try:
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                students += 1
+        return students
+    except TypeError:
+        return None
