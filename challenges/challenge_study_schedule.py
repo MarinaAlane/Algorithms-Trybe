694369dfdc2ea4815e2acf2a50f6a8a637b94ae3
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-02-25 10:38:23 -0400

    Implementa validar_target_time_com_vazio, req 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,6 @@
 def study_schedule(permanence_period, target_time):
+    if type(target_time) is not int:
+        return None
     counter = 0
     for login, logout in permanence_period:
         if (login <= target_time <= logout):
