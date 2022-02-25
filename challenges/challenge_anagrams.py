ed2197b18fd7d75187a105d3f1f1d279ee6ddfbb
josanjohnata <josan.pontes@meliuz.com.br>
2022-02-25 19:55:44 -0300

    Add req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
+# https://app.betrybe.com/course/computer-science/
+# algoritmos/algoritmos-de-ordenacao-e-busca/29521083-
+# 44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-
+# 40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/
+# fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+def string_to_list(string):
+    new_list = list(string)
+    for i in range(len(new_list)):
+        min = i
+        for j in range(i + 1, len(new_list)):
+            if new_list[j] < new_list[min]:
+                min = j
+        new_list[min], new_list[i] = new_list[i], new_list[min]
+    return new_list
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if string_to_list(first_string) == string_to_list(second_string):
+        return True
+    else:
+        return False
