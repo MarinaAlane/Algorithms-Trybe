6761778ff0e67fbb46e6026727a375fb3ccfe219
CamilaCode7 <camilafabiabh9@gmail.com>
2022-02-25 11:18:14 -0300

    corrigindo lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,6 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     try:
         counter = 0
-        if not target_time:
-            return None
         for item in permanence_period:
             if item[0] <= target_time and target_time <= item[1]:
                 counter += 1
