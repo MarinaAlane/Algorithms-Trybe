b1dbcb8e10c9d2585a4a8f4298cda68561256efc
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-02-25 11:02:01 -0400

    Implementa validar_permanence_periods_com_entradas_invalidas, req 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,6 +3,8 @@ def study_schedule(permanence_period, target_time):
         return None
     counter = 0
     for login, logout in permanence_period:
+        if type(login) is not int or type(logout) is not int:
+            return None
         if (login <= target_time <= logout):
             counter += 1
     return counter
