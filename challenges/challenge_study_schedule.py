6b8ff1fb01a85e30de724c7c1d789327905e8ab1
Erick-Marinho <erickgaves@gmail.com>
2022-02-25 15:41:28 -0300

    :rotating_light: Correção linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     try:
         result = [
-            (i,j)
+            (i, j)
             for i, j in permanence_period
             if i <= target_time and target_time <= j
             ]
