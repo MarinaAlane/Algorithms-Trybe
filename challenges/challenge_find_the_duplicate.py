f1ab7845e371f92c1d05634137d8d9c5be659f09
Breno Rodrigues <brenorm@gmail.com>
2022-02-24 22:20:20 -0300

    Requisito 4: Implementação da função challenge_find_the_duplicate(nums)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,22 @@
 def find_duplicate(nums):
+    differente_number = []
     if len(nums) < 2:
         return False
+
     for item in nums:
         if isinstance(item, str) or item < 0:
             return False
+
+    nums.sort()
+    converted_list_to_set = set(nums)
+    converted_set_to_list = list(converted_list_to_set)  # nums = [1, 2, 3, 4]
+
+    if not len(converted_set_to_list) == len(nums):
+        for numb1 in converted_set_to_list:
+            count = 0
+            for numb2 in nums:
+                if numb1 == numb2:
+                    count += 1
+                    if count > 1:
+                        return numb1
+    return False
