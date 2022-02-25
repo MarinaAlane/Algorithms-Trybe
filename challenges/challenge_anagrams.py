f19bfb03224293486c925e621e203088a82ce3f1
Breno Rodrigues <brenorm@gmail.com>
2022-02-25 00:43:21 -0300

    [ Feat] Requisito 3: Implementação da função is_anagram(first_string, second_string)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,28 @@
+# Source: https://stackoverflow.com/questions/52717742/how-to-sort-a-string-\
+# into-alphabetical-order-without-inbuilt-functions
+def sort(string):
+    if not string:
+        return []
+    return (
+        sort([letter for letter in string[1:] if letter < string[0]])
+        + [string[0]] +
+        sort([letter for letter in string[1:] if letter >= string[0]])
+    )
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    if not len(first_string) == len(second_string):
+        return False
+
+    first_string = ''.join(sort(first_string))
+    second_string = ''.join(sort(second_string))
+
+    if first_string == second_string:
+        return True
+
+    return False
+
+
+print(sort("pedra"))
