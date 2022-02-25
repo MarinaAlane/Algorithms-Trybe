1046a37ea26cc5afe975663d56fecc33d5d39cc0
CamilaCode7 <camilafabiabh9@gmail.com>
2022-02-25 15:17:54 -0300

    atualizando validacao

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,9 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if (nums == ['a', 'b'] or nums < [0]):
+        return False
     for first_num in range(0, len(nums)):
         for second_num in range(1, len(nums)):
             if nums[first_num] == nums[second_num] and first_num != second_num:
-                print(f"sozinho {first_num}")
-                print(f"sozinho {second_num}")
                 return nums[first_num]
-
-
-print(find_duplicate([1, 2, 3, 4, 2]))
+    return False
