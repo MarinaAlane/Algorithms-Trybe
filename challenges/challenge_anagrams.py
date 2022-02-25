e8c2eb5f2795d06bbfb60aa5925cf69bbfadfbf4
Erick-Marinho <erickgaves@gmail.com>
2022-02-25 18:30:26 -0300

    :sparkles: Implementando função challenge_anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui"""
+
+    while first_string != '':
+        if len(first_string) != len(second_string):
+            return False
+        tmp = first_string[0]
+        first_string = first_string.replace(tmp, '')
+        try:
+            second_string = second_string.replace(tmp, '')
+        except:
+            return False
+
+    if second_string != '':
+        return False
+    else:
+        return True
