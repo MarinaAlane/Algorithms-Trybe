7596f6acae1dfe213ee2e321dfb0c8862e9d9eae
Erick-Marinho <erickgaves@gmail.com>
2022-02-25 16:18:38 -0300

    :rotating_light: Corrigindo linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,8 +5,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     if not isinstance(word, str):
         return False
     if low_index < high_index: 
-        return is_palindrome_recursive(
-        word, low_index + 1, high_index - 1
-    )
-
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
