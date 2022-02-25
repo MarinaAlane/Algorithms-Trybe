e60ac0a7f35c555ffee71662faad656c3844d292
benlopes <rubenssouss@hotmail.com>
2022-02-25 16:22:56 -0300

    feat: add find duplicates function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,21 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) <= 1:
+        return False
+    if validations(nums):
+        return False
+
+    nums.sort()
+    for index in range(len(nums)):
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+
+def validations(list):
+    if len(list) <= 2 and list[0] != list[1]:
+        return True
+
+    for index in list:
+        if type(index) == str:
+            return True
+        if index < 1:
+            return True
