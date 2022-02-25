907dddd363459f95008db367475e1315f25e7e9c
Rodrigo <rodrigopython16@gmail.com>
2022-02-25 18:54:55 -0300

    added find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    low = 0
+    high = len(nums) - 1
+
+    dic = dict()
+
+    while low < high:
+        if nums[low] in dic:
+            return nums[low]
+        elif nums[high] in dic:
+            return nums[high]
+        else:
+            nums[high] = 1
+            nums[low] = 1
+        high -= 1
+        low += 1
+
+    return False
