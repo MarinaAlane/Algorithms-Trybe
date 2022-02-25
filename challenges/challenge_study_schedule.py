16f32967bc32a29f8e36fe8608230f42258fab8a
Caio <caioqs.info@gmail.com>
2022-02-25 19:24:05 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    ## Iniciando projeto
+    """Faça o código aqui."""
+    if not target_time:
+        return None
+
+    count = 0
+
+    for tuple in permanence_period:
+        # https://pt.stackoverflow.com/questions/341085/como-validar-se-um-valor-%C3%A9-uma-tupla-possuindo-uma-string-e-um-inteiro
+        if not isinstance(tuple[0], int) or not isinstance(tuple[1], int):
+            return None
+        if tuple[0] <= target_time <= tuple[1]:
+            count += 1
+
+    return count
