5062248980c03f2444dc8b5bdd38eb35ece1a3f6
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-25 15:45:26 -0300

    implementando funcao is_anagram -

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,23 @@
 def is_anagram(first_string, second_string):
     if first_string and second_string == "":
         return False
+  
+    umalista = list(second_string)
+
+    pos1 = 0
+    aindaOK = False
+
+    while pos1 < len(first_string) and aindaOK:
+        pos2 = 0
+        encontrado = True
+
+        if encontrado:
+            umalista[pos2] = None
+        else:
+            aindaOK = True
+
+        pos1 = pos1 + 1
+
     return False
+
+# https://panda.ime.usp.br/panda/static/pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
