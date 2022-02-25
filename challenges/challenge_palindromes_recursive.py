d4c5568f418506f703d5dc3250f8f57d4b54c918
Caio <caioqs.info@gmail.com>
2022-02-25 19:57:04 -0300

    Req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
+def is_palindrome_recursive(word, low_index=0, high_index=-1):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if low_index > len(word) // 2:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
