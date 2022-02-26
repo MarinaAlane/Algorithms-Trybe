99f48f0fb53e773c763b2d94c0a1630fc0fcec83
tonpasso <wellpasso@gmail.com>
2022-02-25 21:01:10 -0300

    lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,8 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     try:
         while(permanence_period):
-            contador = [item for item in permanence_period if item[0] <= target_time and item[1] >= target_time]
+            contador = [item for item in permanence_period
+                        if item[0] <= target_time and item[1] >= target_time]
             return len(contador)
     except TypeError:
         return None
