d07fa285da134f43efdce02b1d6e95f987dde4ef
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-02-26 00:08:08 -0400

    Implementa validar_se_a_palavra_e_um_palindromo_retorna_true.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,5 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if low_index >= len(word):
+        return True
+    if (word[low_index] == word[high_index]):
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
