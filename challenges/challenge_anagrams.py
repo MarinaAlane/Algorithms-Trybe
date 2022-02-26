2e328e5b81eafb2af71dbd1cbb0e7fb9766d40b5
marlon307 <marlon-ramosb@hotmail.com>
2022-02-26 08:09:20 -0300

    Rq 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_string_list = list(first_string)
+    second_string_list = list(second_string)
+
+    for i in range(len(first_string)):
+        if first_string_list[i] in second_string_list:
+            second_string_list.remove(first_string_list[i])
+        else:
+            return False
+
+    return True
