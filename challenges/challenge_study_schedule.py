ebfe1042ccd0884ae52a9c88df53b351540d1fe7
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-02-26 19:08:22 -0300

    Req01 realizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    periods = []
+    if type(target_time) is not int:
+        return None
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                periods.append(period)
+        except TypeError:
+            return None
+    return len(periods)
+        
+
