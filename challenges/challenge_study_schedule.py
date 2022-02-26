c661c85270913f47ddf6e73c45d4a7ff77f66388
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-26 00:14:11 -0300

    corrige identacao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
     
     try:
-       count = 0  
-       for i in permanence_period:        
-           if i[0] <= target_time <= i[1]:
-              count += 1
+        count = 0  
+        for i in permanence_period:        
+            if i[0] <= target_time <= i[1]:
+                count += 1
     except TypeError:
-      return None
+        return None
     else:
-     return count
+        return count
