5fbc2e766b8834757955de31be37dc1f678a5dc6
Daniel <danielfdoficial@gmail.com>
2022-02-25 23:19:28 -0300

    fix complexity req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,4 @@
-def is_anagram(first_string, second_string):
-    if len(first_string) != len(second_string):
-        return False
-
-    if not first_string or not second_string:
-        return False
-
+def check_if_is_anagram(first_string, second_string):
     all_letters = list(first_string.lower() + second_string.lower())
     letters = []
     repeated_letters = []
@@ -14,12 +8,24 @@ def is_anagram(first_string, second_string):
         low_letter = min(all_letters)
         current_index = all_letters.index(low_letter)
         while turn % 2 == 0:
-          letters.append(low_letter)
-          turn += 1
+            letters.append(low_letter)
+            turn += 1
         while turn % 2 != 0:
-          repeated_letters.append(low_letter)
-          turn += 1
+            repeated_letters.append(low_letter)
+            turn += 1
         all_letters.pop(current_index)
-
     if letters == repeated_letters:
         return True
+    return False
+
+
+def is_anagram(first_string, second_string):
+    if len(first_string) != len(second_string):
+        return False
+
+    if not first_string or not second_string:
+        return False
+
+    if check_if_is_anagram(first_string, second_string):
+        return True
+    return False
