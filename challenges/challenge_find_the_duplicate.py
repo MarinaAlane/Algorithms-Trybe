78e3d9c333e4541a9b90bc83a15f692d0fcc45b0
marlon307 <marlon-ramosb@hotmail.com>
2022-02-26 08:58:37 -0300

    Rq 4 updat finish

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+    if len(nums) == 1:
+        return False
+    for i in range(len(nums)):
+        if type(nums[i]) is str:
+            return False
+        if nums[i] < 0:
+            return False
+        if nums[i] in nums[i + 1 :]:
+            return nums[i]
+    return False
+
+
+nums = [1, 3, 4, 2, 2]
+
+print(find_duplicate(nums))
