5bd75a54d57b63e8b985eaa80c53dd0360c83b20
Bruno Duarte <brunoarduarte@gmail.com>
2022-02-25 22:32:54 -0300

    [Req. 3] Impements is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def string_to_sorted_list(str):
+    list_base = list(str)
+    list_to_return = list()
+
+    for item in range(len(list_base)):
+        min_str = min(list_base)
+        list_to_return.append(min_str)
+        list_base.remove(min_str)
+
+    return list_to_return
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    result = string_to_sorted_list(first_string) == string_to_sorted_list(
+        second_string
+    )
+    return result
