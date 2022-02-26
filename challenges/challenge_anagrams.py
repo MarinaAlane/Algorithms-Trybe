a98a4c6cb46246642bacf2cb3b847abaec9498fd
Daniel <danielfdoficial@gmail.com>
2022-02-25 23:03:47 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,27 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+      return False
+
+    if not first_string or not second_string:
+      return False
+
+    all_letters = list(first_string.lower() + second_string.lower())
+    letters = []
+    repeated_letters = []
+    turn = 0
+
+    while all_letters:
+      low_letter = min(all_letters)
+      current_index = all_letters.index(low_letter)
+      while turn % 2 == 0:
+        letters.append(low_letter)
+        turn += 1
+      while turn % 2 != 0:
+        repeated_letters.append(low_letter)
+        turn += 1
+      all_letters.pop(current_index)
+    
+    print(letters)
+    print(repeated_letters)
+    if letters == repeated_letters:
+      return True
