44fa884838936d4e2c68e8b7c0715148aa9e47d5
hheloisa <hhackenhaar@gmail.com>
2022-02-26 13:58:41 -0300

    Avançando na resoluçao do requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
+
+    firstStringList = list(first_string)
+    secondStringList = list(second_string)
+    for letra in firstStringList:
+        if secondStringList.__contains__(letra):
+            secondStringList.remove(letra)
+            if len(secondStringList) == 0:
+                return True
+        else: False
+
+
+
+
+def sort_string(string):
+    if len(string) <= 1:
+        return False
