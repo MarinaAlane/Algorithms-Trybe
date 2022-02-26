116613d961ed939968360f2dec9b2d01df408084
Bruno <brunocmoraes97@gmail.com>
2022-02-26 12:43:15 -0300

    Iniciando projeto Algorithms

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
+
+    try:
+        for time in permanence_period:
+            if time[0] <= target_time <= time[1]:
+                count += 1
+
+        return count
+    except TypeError:
+        return None
