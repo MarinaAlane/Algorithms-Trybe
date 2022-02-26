08641028c2e2578f5cb3ccabb02f8fe13cb75cc6
marlon307 <marlon-ramosb@hotmail.com>
2022-02-26 07:50:05 -0300

    Rq 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    count = 0
+
+    for begin, end in permanence_period:
+        if type(begin) is not int or type(end) is not int:
+            return None
+
+        if begin <= target_time <= end:
+            count += 1
+
+    return count
+
