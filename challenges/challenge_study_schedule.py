abcd783124f8a6b9482d6a282b02aa0287c1202b
Marconi <marconieng@gmail.com>
2022-02-25 21:01:45 -0300

    Corrigindo identaçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,10 @@ def study_schedule(permanence_period, target_time):
         return None
     contador = 0
     for p in permanence_period:
-        if (type(p[0]) != int or p[0] is None or type(p[0]) != int or p[1] is None):
+        if (
+            type(p[0]) != int or p[0] is None or type(
+                p[0]) != int or p[1] is None
+        ):
             return None
         if target_time >= p[0] and target_time <= p[1]:
             contador += 1
