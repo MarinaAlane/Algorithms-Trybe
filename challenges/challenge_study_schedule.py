5f7f8b95ac1dbcc1863e5620c2fbb8b1269c4c98
leofuna <leo.funa@gmail.com>
2022-02-26 08:26:40 -0300

    feat: challenge 1 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == 0 or target_time is None:
+        return None
+
+    students_online = 0
+    for student in permanence_period:
+        if isinstance(student[0], int) and isinstance(student[1], int):
+            if student[1] >= target_time >= student[0]:
+                students_online += 1
+        else:
+            return None
+
+    return students_online
