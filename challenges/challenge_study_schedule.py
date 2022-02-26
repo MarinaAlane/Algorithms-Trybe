b1704529d63787740a40f07dab3e1e07904ba1ad
Maria Lisboa <marialisboa1515@gmail.com>
2022-02-26 00:10:46 -0300

    conclui req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-
-    count = 0
-    for i in permanence_period:
-        if i[0] and i[1] != int:
-            return None
-
-    return count
+    
+    try:
+       count = 0  
+       for i in permanence_period:        
+           if i[0] <= target_time <= i[1]:
+              count += 1
+    except TypeError:
+      return None
+    else:
+     return count
