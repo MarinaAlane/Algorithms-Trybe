1cadebfbec48b6d3d74a1b3413e4a33f4c2a3575
Daniel Leon <daniel.radialista@gmail.com>
2022-02-26 14:53:56 -0300

    refatorando req. 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,36 +1,29 @@
-# ref:
-# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+# Função auxiliar construída baseada no link:
+# https://www.programiz.com/python-programming/methods/dictionary/setdefault
 
-# Ordenação por Seleção (Selection Sort)
-def selection_sort(string):
-    # como um algoritmo de força bruta
-    # percorre a estrutura exaustivamente
-    array = list(string)
-    for i in range(len(array)):
-        minimum = i
-
-        # itera sobre os elementos não ordenados
-        for j in range(i + 1, len(array)):
-            # seleciona o menor valor
-            if array[j] < array[minimum]:
-                minimum = j
-
-        # após encontrar o menor valor
-        # ao invés de criar um novo array
-        #  (o que aumenta complexidade de espaço)
-        # realizamos a substituição entre o menor elemento
-        # e a posição i que corresponde ao primeiro elemento não ordenado
-        # que consequentemente passará a ser o último ordenado
-        array[minimum], array[i] = array[i], array[minimum]
-
-    return array
+def check_string(string):
+    word = {}
+    for letter in string:
+        word[letter] = word.setdefault(letter, 0) + 1
+        # percorre todo o laço e conta o total de letras da palavra
+        # setdefault é um método do dicionário Python, com chave e valor
+    # print('por aqui passa o laço for', word)
+    return word
 
 
-print(selection_sort('carro'))
+# percorreu todo o laço for e conta o número de letras
+# print(check_string('Telefone'))
+# {'T': 1, 'e': 3, 'l': 1, 'f': 1, 'o': 1, 'n': 1}
 
 
 def is_anagram(first_string, second_string):
-    if selection_sort(first_string) == selection_sort(second_string):
+    string1 = check_string(first_string)
+    string2 = check_string(second_string)
+    if string1 == string2:
         return True
     else:
         return False
+
+
+print(is_anagram('amor', 'roma'))  # True
+print(is_anagram('coxinha', 'empada'))  # False
