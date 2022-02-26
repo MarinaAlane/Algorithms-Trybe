bce7de08985f641f3588fa7ba969463699fb0778
leofuna <leo.funa@gmail.com>
2022-02-26 09:41:55 -0300

    feat: challenge 3 done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
+def create_dict_from_string(string):
+    string_dict = {}
+    for letter in string:
+        if letter in string_dict:
+            string_dict[letter] += 1
+        else:
+            string_dict[letter] = 1
+
+    return string_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (
+        first_string == ""
+        or second_string == ""
+        or len(first_string) != len(second_string)
+    ):
+        return False
+
+    first_dict = create_dict_from_string(first_string)
+    second_dict = create_dict_from_string(second_string)
+
+    return first_dict == second_dict
