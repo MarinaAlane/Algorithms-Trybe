4717315a294a762437524a1e709adb0558a5a7a9
Daniel <danielfdoficial@gmail.com>
2022-02-26 00:04:15 -0300

    fix while logic req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,17 +3,21 @@ def check_if_is_anagram(first_string, second_string):
     letters = []
     repeated_letters = []
     turn = 0
+    added = True
 
     while all_letters:
+        added = False
         low_letter = min(all_letters)
         current_index = all_letters.index(low_letter)
         while turn % 2 == 0:
             letters.append(low_letter)
+            added = True
             turn += 1
-        while turn % 2 != 0:
+        while turn % 2 != 0 and not added:
             repeated_letters.append(low_letter)
             turn += 1
         all_letters.pop(current_index)
+
     if letters == repeated_letters:
         return True
     return False
