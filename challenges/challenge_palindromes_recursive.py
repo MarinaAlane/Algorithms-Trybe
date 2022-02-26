05e890f4b7690b076754a30e5e1f0889ed061c74
hheloisa <hhackenhaar@gmail.com>
2022-02-26 13:41:32 -0300

    Resoluçao do requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if (word == ""):
+    if word == "":
         return False
-    """ Faça o código aqui. """
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    if low_index >= high_index and len(word) > 0:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+# Recebi ajuda do Luciano Pimenta para a resolução deste requisito.
