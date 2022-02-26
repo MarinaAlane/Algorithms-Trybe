a156a345ff7d58c6b0f1e08eaac79275c71283c6
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2022-02-26 15:44:16 -0300

    function anagram, study

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
-def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+def counts_words(string):
+    list = dict()
+    for item in string:
+        list[item] = list.setdefault(item, 0) + 1
+        # https://www.w3schools.com/python/ref_dictionary_setdefault.asp
+
+    return list
+
+
+def is_anagram(string1, string2):
+    str1 = counts_words(string1)
+    str2 = counts_words(string2)
+
+    if (str1 == str2):
+        return True
+    else:
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count_student = 0
+
+    for item in permanence_period:
+        try:
+            if item[0] <= target_time <= item[1]:
+                count_student += 1
+        except TypeError:
+            return None
+    return count_student
