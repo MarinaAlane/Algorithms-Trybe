68c1698a90ae5b82b2b9d845f3c80b5ad39e2647
lima08 <joaopaulo.gomeslima8@gmail.com>
2022-02-27 10:21:54 -0300

    add requirement 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == 0 or target_time is None:
+        return None
+
+    online_studants = 0
+    for student_time in permanence_period:
+        if not isinstance(student_time[0], int) or not isinstance(
+            student_time[1], int
+        ):
+            return None
+        if student_time[0] <= target_time and student_time[1] >= target_time:
+            online_studants = online_studants + 1
+
+    return online_studants
