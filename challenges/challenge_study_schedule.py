7666201ac7657fe43edfcc1a133723cf0a10f00d
anaclarabck <ackyotoku@gmail.com>
2022-02-27 13:15:29 -0300

    feat: Add req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    total_students = 0
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                total_students += 1
+        except TypeError:
+            return None
+    return total_students
