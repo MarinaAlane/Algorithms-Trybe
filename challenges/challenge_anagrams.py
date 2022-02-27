e59aa3bc7b179535a1dfec96d43f890c402c1cf4
Sergio Martins <sergiomos2010@gmail.com>
2022-02-27 12:57:53 -0300

    feat: req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    anagram = second_string
+    for leather in first_string:
+        if leather in anagram:
+            anagram = anagram.replace(leather, "", 1)
+
+    if len(anagram) == 0:
+        return True
+
+    return False
