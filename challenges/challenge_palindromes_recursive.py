5287fdbe61b4f8ec0b03379b7dc58803fedf497a
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-02-27 13:15:15 -0300

    add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if len(word) == 0:
+            return False
+        if low_index >= len(word):
+            return True
+        return (
+            word[low_index] == word[high_index]
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1))
+
+    except ValueError:
+        return False
