400d503065eb6f7891def626c44a9e4ebdb8b348
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-02-26 21:25:45 -0300

    req04 realizado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
+def create_dict(word):
+    """ Faça o código aqui. """
+    dict_ = {}
+    for letter in word:
+        if letter in dict_:
+            dict_[letter] += 1
+        else:
+            dict_[letter] = 1
+    return dict_
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    dict1 = create_dict(first_string)
+    dict2 = create_dict(second_string)
+    if len(dict1) != len(dict2):
+        return False
+    for key in dict1:
+        if key in dict2:
+            if dict1[key] != dict2[key]:
+                return False
+        else:
+            return False
+    return True
