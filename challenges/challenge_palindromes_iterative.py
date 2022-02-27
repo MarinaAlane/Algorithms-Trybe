a3023d6827df9553a8a43d3932f9b81621ab2c75
anaclarabck <ackyotoku@gmail.com>
2022-02-27 15:24:01 -0300

    feat: Add req5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
+import math
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    half = math.floor(len(word) / 2)
+    counter = 0
+    while counter <= half:
+        try:
+            if word[counter] != word[len(word) - 1 - counter]:
+                return False
+            counter += 1
+        except IndexError:
+            return False
+    return True
