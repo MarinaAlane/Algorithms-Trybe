f92d5e2606492d9b84f5be303dc866cf6e680a29
talescoelho <tales-coelho@live.com>
2022-02-27 11:58:33 -0300

    resolvendo problemas de lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,6 @@
 def separeChar(fs, ss):
     fObj = dict()
     sObj = dict()
-    
     for index in range(len(fs)):
         if fs[index] in fObj:
             fObj[fs[index]] += 1
@@ -16,18 +15,16 @@ def separeChar(fs, ss):
     all['sObj'] = sObj
     return all
 
+
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    
     res = separeChar(first_string, second_string)
     firstObj = res['fObj']
     secondObj = res['sObj']
-
     for index in range(len(first_string)):
         if first_string[index] not in secondObj:
             return False
         if firstObj[first_string[index]] != secondObj[first_string[index]]:
             return False
-
     return True
