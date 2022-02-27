1fba706e81ec9aaa40a9cfcf9efa25bd8402f4ba
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-02-27 12:35:30 -0300

    add req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+    counter = 0
+    for login, logout in permanence_period:
+        if type(login) is not int or type(logout) is not int:
+            return None
+        if (login <= target_time <= logout):
+            counter += 1
+    return counter
