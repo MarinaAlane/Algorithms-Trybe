ea62928d0cfa74503e6a9010f413f235eeb454b5
Dario <dario.junior.rs@gmail.com>
2022-02-27 11:18:38 -0300

    Req 1: Done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    ocurrences = 0
+    for per in permanence_period:
+        if target_time >= per[0] and target_time <= per[1]:
+            ocurrences += 1
+
+    if ocurrences > 0:
+        return ocurrences
+
+    return None
+
+
+# teste = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1)
+# print(teste)
