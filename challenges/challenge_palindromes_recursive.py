82a930627791ce9132b4a8c0fcecfd519675c07e
Marcelo <marsleite@gmail.com>
2022-02-27 16:44:21 -0300

    [feat] se a palavra é um palidome retorna true - req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,7 +3,9 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if low_index == high_index:
         return True
-    elif word[low_index] != word[high_index]:
-        return False
-    else:
+    if word[low_index] == word[high_index]:
+        if high_index == 1:
+            return True
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
+
