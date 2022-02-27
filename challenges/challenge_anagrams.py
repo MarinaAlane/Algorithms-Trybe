5de4b36b9cb5c6cea451e3f869671c71a97236fb
anaclarabck <ackyotoku@gmail.com>
2022-02-27 15:10:14 -0300

    feat: Add req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for char in first_string:
+        second_string = second_string.replace(char, '', 1)
+    if len(second_string) == 0:
+        return True
+    return False
