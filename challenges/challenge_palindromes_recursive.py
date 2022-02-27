2e64d7d734c0b6681a5e2f3a54653c218af8ef45
anaclarabck <ackyotoku@gmail.com>
2022-02-27 15:46:49 -0300

    refactor: Improve req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,8 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     try:
         if word[low_index] != word[high_index]:
             return False
-        if ((low_index == high_index or low_index + 1 == high_index)
-                and word[low_index] == word[high_index]):
+        if (high_index - low_index <= 1):
             return True
         else:
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
