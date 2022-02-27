53c7bb68a35d05abe3da9688ca5a4b9082eb89f9
anaclarabck <ackyotoku@gmail.com>
2022-02-27 15:38:19 -0300

    refactor: Improve req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,6 +3,4 @@ def is_anagram(first_string, second_string):
         return False
     for char in first_string:
         second_string = second_string.replace(char, '', 1)
-    if len(second_string) == 0:
-        return True
-    return False
+    return len(second_string) == 0
