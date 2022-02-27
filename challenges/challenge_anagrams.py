9684150147898ac87f8b9dfcb1480010c0948661
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-02-27 13:55:32 -0300

    testa se o req3 que nao esta deixando passar

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,2 @@
 def is_anagram(first_string, second_string):
-    if len(first_string) or len(second_string) == 0:
-        return False
-    if len(first_string) is not len(second_string):
-        return False
+    ""
