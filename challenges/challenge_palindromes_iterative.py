6e4821e160c9ab5706bbb5ad07ba4612ecbf8714
Marcelo <marsleite@gmail.com>
2022-02-27 16:56:35 -0300

    [feat] palindrome iterative - req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    test_word = word[::-1]
+    if not word:
+        return False
+    if word == test_word:
+        return True
+    return False
