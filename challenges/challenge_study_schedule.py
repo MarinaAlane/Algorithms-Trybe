e897b34cf618b415e449b7c47cf0000732d14dcd
Sergio Martins <sergiomos2010@gmail.com>
2022-02-27 12:07:45 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    online_students = 0
+
+    for student_period in permanence_period:
+        if not isinstance(student_period[0], int) or not isinstance(
+            student_period[1], int
+        ):
+            return None
+
+        elif student_period[0] <= target_time <= student_period[1]:
+            online_students += 1
+
+    return online_students
