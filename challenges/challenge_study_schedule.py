4cf961042d814b77f2d70c50870e75c1dcb775fc
Dario <dario.junior.rs@gmail.com>
2022-02-27 12:04:45 -0300

    Req 1: complete done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
     ocurrences = 0
-    
-    if target_time == None:
-        return None
 
     for per in permanence_period:
         if type(per[0]) != int or type(per[1]) != int:
             return None
 
-        if target_time >= per[0] and target_time <= per[1]:
+        if target_time is not None \
+            and target_time >= \
+                per[0] and target_time <= per[1]:
             ocurrences += 1
 
     if ocurrences > 0:
@@ -17,5 +16,5 @@ def study_schedule(permanence_period, target_time):
     return None
 
 
-# teste = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1)
+# teste = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
 # print(teste)
