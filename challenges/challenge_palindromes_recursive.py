e42df409ae100ea24170c0167213f3c40dd3cb44
Marcelo <marsleite@gmail.com>
2022-02-27 16:38:35 -0300

    [feat] se não é um palindromo retorna false quando não tem word

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Dado uma string, determine se ela é um palíndromo ou não. Escreva uma função que irá determinar se uma string é um palíndromo ou não. Um palíndromo é uma string, uma palavra, em que não faz diferença se ela é lida da esquerda para a direita ou vice-versa, pois ela mantêm o mesmo sentido. Por exemplo, "ABCBA". """
+    if not word:
+        return False
     if low_index == high_index:
         return True
     elif word[low_index] != word[high_index]:
