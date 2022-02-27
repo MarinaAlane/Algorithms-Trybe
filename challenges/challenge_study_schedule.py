813c63e3ab4802e03813cdfe4ca4bc91ae6aa3c6
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2022-02-27 17:57:34 -0300

    :sparkles: feat - Requisito 2 :sparkles:

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,25 +3,11 @@ def study_schedule(permanence_period, target_time):
     try:
         for time in permanence_period:
             if target_time >= time[0] and target_time <= time[1]:
-                print(time[0], time[1])
                 contador += 1
-                print(contador)
     except TypeError:
         return None
 
     return contador
 
-
-# permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-# target_time = 4
-
-permanence_periods = [(4, "e"), ("0", 4)]
-target_time = 4
-
-print(
-    study_schedule(
-        permanence_period=permanence_periods, target_time=target_time
-    )
-)
 
 # https://stackoverflow.com/questions/48343387/valueerror-and-typeerror-in-python
