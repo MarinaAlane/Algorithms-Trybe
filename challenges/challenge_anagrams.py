77fdbb20e6b0b01691ed2e84731429ffceaffecd
Marcelo <marsleite@gmail.com>
2022-02-27 17:14:39 -0300

    [feat] verificando se a primeira e segunda string são anagramas - req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    first_string_list = list(first_string)
+    second_string_list = list(second_string)
+
+    for letter in first_string_list:
+        if letter in second_string_list:
+            second_string_list.remove(letter)
+        else:
+            return False
+
+    return True
