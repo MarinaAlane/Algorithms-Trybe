c7bab9fd37437afb9ef4c922f2db0ed8510fb7b4
tonpasso <wellpasso@gmail.com>
2022-02-27 20:04:58 -0300

    requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == '':
+        return False
+    if low_index == high_index or low_index == len(word):
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
