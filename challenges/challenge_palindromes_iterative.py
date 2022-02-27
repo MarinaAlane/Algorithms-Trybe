94d0961c83b1b99de08b545a2808c162ca1e6158
anaclarabck <ackyotoku@gmail.com>
2022-02-27 16:02:11 -0300

    refactor: Improve req5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,14 +1,15 @@
-import math
-
-
 def is_palindrome_iterative(word):
-    half = math.floor(len(word) / 2)
-    counter = 0
-    while counter <= half:
-        try:
-            if word[counter] != word[len(word) - 1 - counter]:
-                return False
-            counter += 1
-        except IndexError:
-            return False
-    return True
+    # half = math.floor(len(word) / 2)
+    # counter = 0
+    # while counter <= half:
+    #     try:
+    #         if word[counter] != word[len(word) - 1 - counter]:
+    #             return False
+    #         counter += 1
+    #     except IndexError:
+    #         return False
+    # return True
+    # if len(word) == 0:
+    #     return False
+    # https://www.w3schools.com/python/python_howto_reverse_string.asp
+    return len(word) > 0 and word == word[::-1]
