b8f58ae59ad668cad325f668dc1286b2dfae2bfe
Marcelo <marsleite@gmail.com>
2022-02-27 16:20:32 -0300

    [feat] retornar a quantidade de estudantes online em 0.02s - req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. Vamos que vamos """
+    if not target_time:
+        return None
+    counter = 0
+    for i in range(len(permanence_period)):
+        if not isinstance(permanence_period[i][0], int) or not isinstance(
+            permanence_period[i][1], int
+        ):
+            return None
+        if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+            counter += 1
+    return counter
