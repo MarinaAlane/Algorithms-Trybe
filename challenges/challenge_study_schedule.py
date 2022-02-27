d32c4cc4a060dbd8360455364a5ba31e0fe11eca
Reginara Gomes <reginara.gomes@datasprints.com>
2022-02-27 16:54:11 -0300

    Adiciona requisito 1 função challenge_study_schedule. Usando try except

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+# Usei o try except para que ele tentasse pegar uma informação, ou retornar
+# um erro caso essa informação não exista.
+# no if, verifiquei se o target_time está entre a primeira e a segunda posição informadas
+# em cada tupla do permanence_period. Se estiver, conta 1 e passa pra proxima tupla, se não,
+# ele vai pro erro.
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if not target_time:
+        return None
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
