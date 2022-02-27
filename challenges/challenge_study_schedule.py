6885bbf34179324718e7ff677b91e49cbeb53e02
Amandha W. Barok <amandha.barok@gmail.com>
2022-02-26 16:26:20 -0800

    Finishing first requiriment

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+    if not permanence_period:
+        return None
+    if not target_time:
+        return None
+    for item in permanence_period:
+        (entry_time, exit_time) = item
+        if type(entry_time) != int or type(exit_time) != int:
+            return None
+        if target_time >= entry_time and target_time <= exit_time:
+            result += 1
+    return result
