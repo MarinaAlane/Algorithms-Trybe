86b1c3fe303fc95908640ae4f4a71c836a19b3ce
Rodolfo <rodolforrcont@gmail.com>
2022-02-27 12:00:34 -0300

    Requisito 4

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if word == "":
+        return False
+    lower_index = 0
+    high_index = len(word) - 1
+
+    while lower_index < high_index:
+        if word[lower_index] != word[high_index]:
+            return False
+        lower_index += 1
+        high_index -= 1
+    return True
