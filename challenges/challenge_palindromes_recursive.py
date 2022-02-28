9a6a3a2742f5909db16441e0533f4dda76ef9049
Marconi <marconieng@gmail.com>
2022-02-28 15:17:36 -0300

    Corrigindo linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,11 @@
 # Requisito resolvido com base neste site: https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
 def is_palindrome_recursive(word, low_index, high_index):
-    if word == "": return False
-    if low_index == high_index: return True
-    if word[low_index] != word[high_index]: return False
+    if word == "":
+        return False
+    if low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
 
     if low_index < high_index + 1:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
