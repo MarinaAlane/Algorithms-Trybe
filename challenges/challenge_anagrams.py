ff60a7c5422b4c5dff86c8854f76f07a9e407b37
Wilquer82 <wilquer.fig@gmail.com>
2022-02-28 14:49:37 -0300

    3 anagrama 3/4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def ordenate(string):
+    s_list = list(string)
+    for i in range(len(s_list)):
+        minimum = i
+        for j in range(i + 1, len(s_list)):
+            if s_list[j] < s_list[minimum]:
+                minimum = j
+        s_list[minimum], s_list[i] = s_list[i], s_list[minimum]
+    return s_list
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui.   """
+
+    if ordenate(first_string) == ordenate(second_string):
+        return True
+    else:
+        return False
