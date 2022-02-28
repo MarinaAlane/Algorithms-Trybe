d9ee74263fbce563888aedceee0ba1b2065758e7
Laura Gusmao <laurargus@gmail.com>
2022-02-28 13:38:31 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    
+    counter = 0
+
+    for entry_time, exit_time in permanence_period:
+        if type(entry_time) is not int or type(exit_time) is not int:
+            return None
+        if entry_time <= target_time <= exit_time:
+            counter += 1
+    
+    return counter
