4acfaf755207c9329586313faf66aade6f1eb979
DiegoCS777 <diegomethal@gmail.com>
2022-02-28 20:30:51 -0300

    req 3 corrigido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,7 @@
 def count_letter_string(str):
+    """
+    retorna um dicionário onde cada index é a contagem da letra em uma string
+    """
     string = {}
     for letter in str:
         if letter in string:
@@ -17,6 +20,8 @@ def is_anagram(first_string, second_string):
     if not string1 and string2 or string1 != string2:
         return False
     else:
+        #verifica se cada index em string1 tem um correspondente
+        # em string2 e vice e versa
         for index in string1:
             if not index in string2:
                 return False
