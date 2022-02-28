8a5d23909fc7d73ff506668d73a8cd7396e0ee98
Gustavo Lemes <gustavolemes997@gmail.com>
2022-02-28 20:10:55 -0300

    Implementaçao do palindromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) <= 1:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    new_word = word[1:high_index]
+    new_word_length = len(new_word)
+    result = (is_palindrome_recursive(new_word, 0, new_word_length - 1)
+              if new_word_length != 0 else True)
+    return result
