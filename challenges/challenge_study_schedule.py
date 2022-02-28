8dec88c3fba976154690b9a8acb1b6aa82288f17
Wilquer82 <wilquer.fig@gmail.com>
2022-02-28 11:42:58 -0300

    1try

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,10 @@
 def study_schedule(permanence_period, target_time):
     contador = 0
-    for entry_exit in permanence_period:
-        if entry_exit[0] <= target_time <= entry_exit[1]:
-            contador += 1
-        else:
-            return contador
+    try:
+        for entry_exit in permanence_period:
+            if entry_exit[0] <= target_time <= entry_exit[1]:
+                contador += 1
+    except TypeError:
+        return None
+    else:
+        return contador
