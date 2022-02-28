ebde71e92dfc2e3ab9f8173630035009c31ea96a
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-28 16:38:25 -0300

    feat: 1. número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    studants_count = 0
+
+    if type(target_time) != int:
+        return None
+
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        
+        if target_time >= period[0] and target_time <= period[1]:
+            studants_count += 1
+
+    return studants_count
