3e5d2fd687032604d15203aea898be550b31b1a4
IsaacMagno <isaacmagno98@gmail.com>
2022-02-28 14:45:18 -0300

    r1c

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
     c = 0
     for e, s in permanence_period:
-      try:
-          if e <= target_time <= s:
-              c += 1
-          return c
-      except TypeError:
-          return None
+        try:
+            if e <= target_time <= s:
+                c += 1
+            return c
+        except TypeError:
+            return None
