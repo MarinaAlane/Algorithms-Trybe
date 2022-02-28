c8300eb2d539b212153647070eec9e97880c20b4
Gustavo Lemes <gustavolemes997@gmail.com>
2022-02-28 20:26:47 -0300

    Implementaçao da funçao que verifica anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    aux_string = second_string
+    for char in first_string:
+        if char in aux_string:
+            aux_string = aux_string.replace(char, '', 1)
+    return True if len(aux_string) == 0 else False
