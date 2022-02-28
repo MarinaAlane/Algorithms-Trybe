ddfcec7a028af9de0aa57bfcf4ce019a673941e6
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-28 16:25:07 -0300

    Falta corriri o is_anagram, ele está considera certo algumas palavras como grr é anagrama de  ggr

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,8 @@
 def is_anagram(first_string, second_string):
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
     for letter in second_string:
         if letter not in first_string:
             return False
