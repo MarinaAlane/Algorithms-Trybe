70b996a8dcd0daacdc81f5a6d15de7e16b789c19
Victor Faria <vitilevi@hotmail.com>
2022-02-28 14:48:28 -0300

    Adiciona requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or type(nums) is str:
+        return False
+    
+    for index, num in enumerate(nums):
+        if type(num) is str or num < 0:
+            return False
+        
+        if num in nums[index + 1:]:
+            return num
+        
+    return False
