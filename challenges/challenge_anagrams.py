3e0a6828d27d26451838c568d535c18f26a84b8b
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-02-28 01:13:25 -0300

    feat: req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def count_string(word):
+    string = dict()
+
+    for letter in word:
+        try:
+            string[letter] += 1
+        except KeyError:
+            string[letter] = 1
+    return string
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui!"""
+    if count_string(first_string) == count_string(second_string):
+        return True
+    else:
+        return False
