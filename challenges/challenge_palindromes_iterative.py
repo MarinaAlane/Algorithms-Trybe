6a82be1315a38f9aa7573a7ae15cee679bc0e906
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-28 15:36:41 -0300

    Requisito 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,31 @@
-def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+# jeito fácil
+# def is_palindrome_iterative(word):
+#     if word == word[::-1]:
+#         return word[::-1]
+#     else:
+#         return 'false'
+
+
+def is_palindrome_iterative(word, result='', originalWord=''):
+    if not word:
+        return False
+
+    if len(originalWord) < len(word):
+        originalWord = word
+
+    if(len(word) > 1):
+        return is_palindrome_iterative(
+            word[0:len(word) - 1],
+            result + word[-1],
+            originalWord
+            )
+
+    if result + word == originalWord:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    permanence_period_data = 'REVIVER'
+    result = is_palindrome_iterative(permanence_period_data)
+    print(result)
