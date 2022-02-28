52f016e81617ad4649b3c2a189465d9aee692a1e
Victor Faria <vitilevi@hotmail.com>
2022-02-28 15:19:05 -0300

    fix performance

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,9 +2,4 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 
-    reversed = ''
-
-    for index in range(len(word) - 1, -1, -1):
-        reversed += word[index]
-
-    return reversed == word
+    return word == word[::-1]
