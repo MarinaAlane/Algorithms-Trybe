86b03af7796616766b5be152b8044dc6a3d1cf43
Gabriela Feijo <feijo.gabriela@gmail.com>
2022-02-28 00:14:48 -0300

    add requirement 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
+def counter(string):
+    return {i: string.count(i) for i in set(string)}
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if counter(first_string) == counter(second_string):
+        return True
+    return False
+
+# https://stackoverflow.com/questions/48217471/is-it-possible-to-check-for-anagram-without-using-sorted-or-dictionary-that-pe
