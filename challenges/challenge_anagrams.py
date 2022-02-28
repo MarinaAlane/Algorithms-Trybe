eb9d65a7288675fa329eec8ee3ef3b74e00a94e8
DiegoCS777 <diegomethal@gmail.com>
2022-02-28 20:24:43 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,34 @@
+def count_letter_string(str):
+    string = {}
+    for letter in str:
+        if letter in string:
+            string[letter] += 1
+        else:
+            string[letter] = 1
+    return string
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    string1 = count_letter_string(first_string)
+    string2 = count_letter_string(second_string)
+    
+    
+    if not string1 and string2 or string1 != string2:
+        return False
+    else:
+        for index in string1:
+            if not index in string2:
+                return False
+            if not string1[index] == string2[index]:
+                return False
+
+        for index in string2:
+            if not index in string1:
+                return False
+            if not string1[index] == string2[index]:
+                return False
+
+        return True
+        
+           
