ac259c6f8d678a3d38354af9026f6fa1dc43cb6c
IsaacMagno <isaacmagno98@gmail.com>
2022-02-28 15:40:41 -0300

    r3c1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,10 +4,13 @@ def is_anagram(first_string, second_string):
 
     second_string = list(second_string)
 
-    for l in range(len(first_string)):
-        second_string.remove(first_string[l])
+    try:
+        for l in range(len(first_string)):
+            second_string.remove(first_string[l])
 
-    if not second_string:
-        return True
+        if not second_string:
+            return True
 
-    return False
+        return False
+    except ValueError:
+        return False
