845da4cbda12ec9ce23e67c2500ad295739bcb87
Gabriela Feijo <feijo.gabriela@gmail.com>
2022-02-28 00:46:21 -0300

    add requirement 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 0:
+        return False
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
+
+# https://thispointer.com/python-3-ways-to-check-if-there-are-duplicates-in-a-list/
