baf6c9f7d2d2f496d62effefa90db5cbfe2edb00
Wilquer82 <wilquer.fig@gmail.com>
2022-02-28 12:15:11 -0300

    2 palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[low_index] != word[high_index]:
+        return False
+# Teste 1 sem palavra ou final diferente do começo
+    if len(word) == 1 or low_index >= high_index:
+        return True
+# Teste 2 palavra com 1 letra ou maior que 1
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,8 @@ def study_schedule(permanence_period, target_time):
     try:
         for entry_exit in permanence_period:
             if entry_exit[0] <= target_time <= entry_exit[1]:
-                contador += 1
+                contador = contador + 1
+# Para capturar os erros
     except TypeError:
         return None
     else:
