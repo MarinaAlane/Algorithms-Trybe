48cc23b7a8f1e7c4bf65e59a958d7bd2d9a28654
Victor Faria <vitilevi@hotmail.com>
2022-02-28 11:32:36 -0300

    Fix espaços em branco

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,12 +1,12 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    
+
     matchedChars = 0
-    
+
     for char in first_string:
         if char in second_string:
             matchedChars += 1
             second_string = second_string.replace(char, '', 1)
-    
+
     return matchedChars == len(first_string)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,14 @@
 def study_schedule(permanence_period, target_time):
     number_of_students = 0
-    
+
     if not target_time:
         return None
-    
+
     for clockIn, clockOut in permanence_period:
         if type(clockIn) is not int or type(clockOut) is not int:
             return None
-        
+
         if clockIn <= target_time <= clockOut:
             number_of_students += 1
-    
+
     return number_of_students
