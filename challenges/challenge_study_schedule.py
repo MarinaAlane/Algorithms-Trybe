68f232423c078f51ad5cbb84aa050275be9e308a
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-28 17:55:20 -0300

    study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if not target_time:
+        return None
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+        if target_time >= period[0] and target_time <= period[1]:
+            counter += 1
+    return counter
