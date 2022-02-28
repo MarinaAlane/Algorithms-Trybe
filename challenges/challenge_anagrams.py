92935404e392b3cea75da03c749ad5115d4bc80a
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-02-28 17:59:35 -0300

    feat: 3. anagramas (algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+def quicksort(string):
+    if not string: return string
+
+    pivot = string[0]
+
+    head = quicksort([x for x in string[1:] if x < pivot])
+    tail = quicksort([x for x in string[1:] if x > pivot])
+
+    return head + [x for x in string if x == pivot] + tail
+
+# E K S A
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return quicksort(first_string) == quicksort(second_string)
