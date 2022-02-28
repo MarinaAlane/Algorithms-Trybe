059c4db8aaeaff36baf9b01ffafb75a6fbb382e4
Victor Faria <vitilevi@hotmail.com>
2022-02-28 14:49:13 -0300

    remove espaços em branco

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,12 +1,12 @@
 def find_duplicate(nums):
     if not nums or type(nums) is str:
         return False
-    
+
     for index, num in enumerate(nums):
         if type(num) is str or num < 0:
             return False
-        
+
         if num in nums[index + 1:]:
             return num
-        
+
     return False
