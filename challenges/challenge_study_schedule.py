47efdb5f0a1b55b6684e1b7d4d416242d8e6c85b
DiegoCS777 <diegomethal@gmail.com>
2022-02-28 18:05:06 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-# commit
+    student = 0
+    try:
+        for student_entry in permanence_period:
+            if student_entry[0] <= target_time <= student_entry[1]:
+                student += 1
+    except:
+        return None
+    return student
