63ef4b300020c18023dcf9222f92b356f271b205
Amandha W. Barok <amandha.barok@gmail.com>
2022-02-28 21:29:34 -0800

    Finishing 3th req

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def my_sort(string):
+    array = list(string)
+    n = len(array)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+    return "".join(array)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_sorted = my_sort(first_string)
+    second_sorted = my_sort(second_string)
+    return first_sorted == second_sorted
