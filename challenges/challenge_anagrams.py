f6ca72e88ec78b740e0688c762c59cc3aa6a25e2
Rubia <rubiaemilly29@gmail.com>
2022-03-01 01:59:46 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
+# def setDic(word)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    if first_string != second_string:
+        return False
+    return True
