48bfbb7b7d267dcbc9903a4e70523e95ed5356a8
MarianaSavoldi <marianasavodi123@gmail.com>
2022-02-28 22:39:14 -0300

    Requirement 01 done (study_schedule function)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ First commit"""
+    students_counter = 0
+    try:
+        # p aqui é uma tupla de horários, sendo (entrada, saída)
+        for p in permanence_period:
+            if p[0] <= target_time <= p[1]:
+                students_counter += 1
+    # aqui o TypeError é pra caso o algum valor seja nulo
+    # https://www.pylenin.com/blogs/type-error-exception/
+    except TypeError:
+        return None
+    else:
+        return students_counter
