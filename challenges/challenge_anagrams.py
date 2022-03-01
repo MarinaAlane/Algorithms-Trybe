bdce5e359d1b30047348002b4dbfcfa6a1441b54
lucas-chave <lcchavesr150@gmail.com>
2022-03-01 12:34:53 -0300

    feat: app

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
+# https://github.com/tryber/sd-011-project-algorithms/pull/91
+# https://github.com/tryber/sd-011-project-algorithms/pull/97
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    for le in first_string:
+        second_string = second_string.replace(le, '', 1)
+
+    return second_string == ''
