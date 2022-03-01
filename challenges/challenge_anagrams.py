3c16b8c546ed56f6fdd240104b2bf54cd3a9faf0
Jean Esteves <jean.esteves2001@gmail.com>
2022-02-28 21:29:55 -0300

    [TASK3] - done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for each_letter in first_string:
+        if each_letter in second_string:
+            second_string = second_string.replace(each_letter, '', 1)
+    if len(second_string) == 0:
+        return True
+
+    return False
