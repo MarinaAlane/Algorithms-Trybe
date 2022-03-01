d82f5f4626fc01685f277853c69813cb3a1e8124
Laura Gusmao <laurargus@gmail.com>
2022-03-01 00:21:39 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    for letter in first_string:
+        if letter in second_string:
+            second_string = second_string.replace(letter, '', 1)
+
+    if len(second_string) == 0:
+        return True
+    else:
+        return False
+
+# Ref:
+# https://appdividend.com/2020/01/21/python-list-contains-how-to-check-if-item-exists-in-list/
