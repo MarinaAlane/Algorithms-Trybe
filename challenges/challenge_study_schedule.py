637c991f09d77a243790dbc82a345b764a771ece
giovani-diccini <giovani.diccini@gmail.com>
2022-03-01 12:08:39 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+
+    counter = 0
+
+    for start, end in permanence_period:
+        if type(start) is not int or type(end) is not int:
+            return None
+
+        if start <= target_time <= end:
+            counter += 1
+
+    return counter
