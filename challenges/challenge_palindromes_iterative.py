890b9b863546eff44e47b60ffdc27f312b151fa3
GabriellCastro <kastro.gabriel17@gmail.com>
2022-03-01 13:11:35 -0400

    add: finish project

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    high = len(word) - 1
+    low = 0
+
+    while low < high:
+        if word[low] != word[high]:
+            return False
+        low += 1
+        high -= 1
+
+    return True
