150aedaa78f7b6fc0879e8c1314749c5ec195a3e
tonpasso <wellpasso@gmail.com>
2022-03-01 08:32:54 -0300

    requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,34 @@
+def quicksort(lista, esquerda=0, direita=None):
+    if direita is None:
+        direita = len(lista)-1
+    if esquerda < direita:
+        pivot = partition(lista, esquerda, direita)
+        quicksort(lista, esquerda, pivot - 1)
+        quicksort(lista, pivot + 1, direita)
+
+    return lista
+
+
+def partition(lista, esquerda, direita):
+    pivot = lista[direita]
+    i = esquerda
+    for j in range(esquerda, direita):
+        if lista[j] <= pivot:
+            lista[j], lista[i] = lista[i], lista[j]
+            i = i + 1
+    lista[i], lista[direita] = lista[direita], lista[i]
+    return i
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if (len(first_string) != len(second_string)
+            or first_string == '' or second_string == ''):
+        return False
+
+    primeira = quicksort(list(first_string))
+    segunda = quicksort(list(second_string))
+
+    if primeira == segunda:
+        return True
+    return False
