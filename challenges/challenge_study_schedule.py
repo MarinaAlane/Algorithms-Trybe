44a5011c69cfff5b1402e174109003eee7292993
Marina Alane <marina.alane@outlook.com>
2022-03-01 15:48:57 -0300

    chore: add challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    if target_time == 0 or target_time is None:
+        return None
+
+    online_studants = 0
+    for student_time in permanence_period:
+        if not isinstance(student_time[0], int) or not isinstance(
+            student_time[1], int
+        ):
+            return None
+        if student_time[0] <= target_time and student_time[1] >= target_time:
+            online_studants = online_studants + 1
+
+    return online_studants
+
+# ref : https://www.w3schools.com/python/ref_func_isinstance.asp
