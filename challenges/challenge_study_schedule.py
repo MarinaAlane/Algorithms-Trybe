95529731c92924f282cd3a95eb21830952995d2c
OldRoge <rogeriomrjr@icloud.com>
2022-03-02 19:00:06 -0300

    Try fix again

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -12,8 +12,3 @@ def study_schedule(permanence_period, target_time):
             time += 1
 
     return time
-
-
-permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
-print(study_schedule(permanence_periods, 5))
