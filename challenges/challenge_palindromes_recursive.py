7365f5bb98644c9d354d3fd26b7a8787a623a7fe
Igor Farias <igor.farias@liferay.com>
2022-03-02 13:48:35 -0300

    Ajusta requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,10 +2,11 @@ def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
 
-    if word[low_index] != word[high_index]:
-        return False
-
-    if low_index - high_index < 1:
+    if high_index - low_index < 1:
         return True
-    next_low_index, next_high_index = low_index + 1, high_index - 1
-    return is_palindrome_recursive(word, next_low_index, next_high_index)
+
+    if word[low_index] == word[high_index]:
+        next_low_index, next_high_index = low_index + 1, high_index - 1
+        return is_palindrome_recursive(word, next_low_index, next_high_index)
+
+    return False
