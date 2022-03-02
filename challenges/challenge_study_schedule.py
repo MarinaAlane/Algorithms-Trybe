dae26497f179c1554075c7b9351ff9aa8811e370
OldRoge <rogeriomrjr@icloud.com>
2022-03-02 19:20:22 -0300

    Add refference

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,11 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-
     time = 0
     for num, value in permanence_period:
-
+        # https://www.guru99.com/type-isinstance-python.html#7
         if not isinstance(num, int) or not isinstance(value, int):
             return None
-
         if num <= target_time <= value:
             time += 1
-
     return time
