d4add83232f2da535204097c71d3ed8dbc687828
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 17:22:01 -0300

    Requisito 05 - Implementado

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word is None or word == '':
+        return False
+
+    is_palindrome = True
+    for index, letter in enumerate(word):
+        if letter != word[-(index+1)]:
+            return False
+
+    return is_palindrome
