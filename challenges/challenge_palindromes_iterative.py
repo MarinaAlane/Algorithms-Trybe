0cbe14b64e2d13642de559a8f58223e0ebdc88b3
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 17:39:35 -0300

    Requisito 05 - Fix - Melhoria performance

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,10 +1,9 @@
 def is_palindrome_iterative(word):
-    if word is None or word == '':
+    if word == '':
         return False
 
-    is_palindrome = True
     for index, letter in enumerate(word):
         if letter != word[-(index+1)]:
             return False
 
-    return is_palindrome
+    return True
