7d79ae756fbcf6e6897187cbe1d8531622ebe221
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 15:29:01 -0300

    Requisito 02 Implementado

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    if len(word) == 1:
+        return True
+
+    if len(word) == 2:
+        return word[low_index] == word[high_index]
+
+    remaining_word = word[low_index+1:high_index]
+    check = word[low_index] == word[high_index]
+
+    return check and is_palindrome_recursive(
+        remaining_word, 0, len(remaining_word) - 1
+    )
