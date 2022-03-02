a9709d6e5d28855b860570316e6ca4bb5a967ffa
Igor Farias <igor.farias@liferay.com>
2022-03-02 12:38:17 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    low_index, high_index = 0, len(word) - 1
+
+    for char_comparison in word[:len(word) // 2]:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
