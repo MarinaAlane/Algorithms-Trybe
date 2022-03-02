e21d9e35f5ba7997cea689a5675852e1eecb5447
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-01 21:43:15 -0300

    Requisito 01 - otimizada a execução trocando range por chained comparison

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,8 +8,7 @@ def study_schedule(permanence_period, target_time):
         if not isinstance(start, int) or not isinstance(finnish, int):
             return None
 
-        permanece = range(start, finnish + 1)
-        if target_time in permanece:
+        if start <= target_time <= finnish:
             count += 1
 
     return count
