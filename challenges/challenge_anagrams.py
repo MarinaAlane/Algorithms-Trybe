86957c67760e3e37e1bafc87f372399fb4fe0a4b
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 17:00:15 -0300

    Requisito 03 - fix lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -11,11 +11,15 @@ def selection_sort(word):
 
     return "".join(letters)
 
+
 # implementação inspirada no selection sort do gabarito abaixo:
 # https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca-gabarito/solutions/b471328b-a7ef-4c62-ac38-743f15b38fad/gabarito-dos-exercicios/b9ef5713-3678-4570-890c-4b4de0b7cffa?use_case=calendar
-# essa implementação funciona para comparação de caracteres pois a linguagem compara os valores da tabela ascii
-# um potencial bug para a aplicação da forma que está implementada é não lidar com a capitalização de palavras
-# como há limite de tempo um bypass para manter a capitalização das letras não foi implementado para preservar a performance.
+# essa implementação funciona para comparação de caracteres pois a linguagem
+# compara os valores da tabela ascii um potencial bug para a aplicação da forma
+# que está implementada é não lidar com a capitalização de palavras como há
+# limite de tempo um bypass para manter a capitalização das letras não foi
+# implementado para preservar a performance.
+
 
 def is_anagram(first_string, second_string):
     if first_string is None or second_string is None:
