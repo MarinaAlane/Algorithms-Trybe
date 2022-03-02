9b44a611edf9d87ba9bb65da8bb0328891208160
Igor Farias <igor.farias@liferay.com>
2022-03-02 11:15:45 -0300

    Corrige requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    try:
-        students_per_period = 0
-        for period in permanence_period:
-            (entry_time, exit_time) = period
-            if entry_time <= target_time <= exit_time:
-                students_per_period += 1
-        return students_per_period
-    except TypeError:
+    if type(target_time) is not int:
         return None
+    students_per_period = 0
+    for entry_time, exit_time in permanence_period:
+        if type(entry_time) is not int or type(exit_time) is not int:
+            return None
+        if entry_time <= target_time <= exit_time:
+            students_per_period += 1
+    return students_per_period
