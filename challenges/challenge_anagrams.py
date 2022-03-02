ee26d977822bdd484a8b6488bc926d35eb20ad5b
Igor Farias <igor.farias@liferay.com>
2022-03-02 18:31:31 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    return bubble_sort(first_string) == bubble_sort(second_string)
+
+
+def bubble_sort(string):
+    str_list = list(string)
+    has_swapped = True
+
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+        for i in range(len(str_list) - num_of_iterations - 1):
+            if str_list[i] > str_list[i + 1]:
+                str_list[i], str_list[i + 1] = str_list[i + 1], str_list[i]
+                has_swapped = True
+        num_of_iterations += 1
+        return "".join(str_list)
