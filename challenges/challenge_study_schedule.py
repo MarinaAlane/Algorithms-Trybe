0055f5936517c91b5de514da1f64308ac3a1d354
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-01 21:33:18 -0300

    Requisito 01 implementado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    count = 0
+
+    for start, finnish in permanence_period:
+        if not isinstance(start, int) or not isinstance(finnish, int):
+            return None
+
+        permanece = range(start, finnish + 1)
+        if target_time in permanece:
+            count += 1
+
+    return count
