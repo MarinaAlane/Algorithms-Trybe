c1cdb1459256e9ed8fec433872476dd511b93e5f
Igor Farias <igor.farias@liferay.com>
2022-03-02 10:50:55 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    students_per_period = 0
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
+            students_per_period += 1
+    return students_per_period
