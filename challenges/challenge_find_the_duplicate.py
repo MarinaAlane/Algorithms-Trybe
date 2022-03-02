813dfb11b569d842b6d4337d29d0201c5def5528
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 18:25:17 -0300

    Requisito 04 - Fix - Redução de complexidade, Linters deserve love <3

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,25 +1,17 @@
 def find_duplicate(nums):
-    if len(nums) == 0 or len(nums) == 1:
-        return False
-
-    if any(isinstance(number, str) for number in nums):
-        return False
-
-    if any(number < 0 for number in nums):
+    if len(nums) <= 1:
         return False
 
     duplicated_number = False
     sorted_nums = nums
     sorted_nums.sort()
     for i, j in enumerate(sorted_nums):
+        if isinstance(j, str) or j < 0:
+            return False
+
         curr = j
         prev = sorted_nums[i - 1]
-        if (
-            duplicated_number is not False
-            and curr == prev
-            and duplicated_number != curr
-        ):
-            return False
+
         if curr == prev:
             duplicated_number = curr
 
