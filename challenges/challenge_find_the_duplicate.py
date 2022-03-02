426ee3196166efb29dcfb5892531103adef2eee4
Igor Farias <igor.farias@liferay.com>
2022-03-02 16:04:37 -0300

    Ajusta requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,10 +1,10 @@
 def find_duplicate(nums):
     ordered_nums = bubble_sort(nums)
-    if len(ordered_nums) < 2:
+    if len(ordered_nums) < 2 or ordered_nums[0] < 1:
         return False
 
-    for index, num in enumerate(ordered_nums):
-        if num == ordered_nums[index + 1]:
+    for index in range(len(ordered_nums) - 1):
+        if ordered_nums[index] == ordered_nums[index + 1]:
             return True
     return False
 
@@ -19,20 +19,20 @@ def bubble_sort(array):
     # a iteração sobre índices já ordenados
     num_of_iterations = 0
 
-    # Enquanto ainda não está ordenado (ocorreram trocas na iteração)
-    while has_swapped:
-        has_swapped = False
+    try:
+        # Enquanto ainda não está ordenado (ocorreram trocas na iteração)
+        while has_swapped:
+            has_swapped = False
+            # percorra o array até o ultimo índice não ordenado
+            for i in range(len(array) - num_of_iterations - 1):
+                # caso a posição corrente seja maior que a posterior
+                if array[i] > array[i + 1]:
+                    # realiza a troca entre as posições
+                    array[i], array[i + 1] = array[i + 1], array[i]
+                    # marca que tivemos trocas nesta iteração
+                    has_swapped = True
+            num_of_iterations += 1
+            return array
 
-        # percorra o array até o ultimo índice não ordenado
-        for i in range(len(array) - num_of_iterations - 1):
-            if type(array[i]) is not int or array[i] < 1:
-                return []
-            # caso a posição corrente seja maior que a posterior
-            if array[i] > array[i + 1]:
-                # realiza a troca entre as posições
-                array[i], array[i + 1] = array[i + 1], array[i]
-                # marca que tivemos trocas nesta iteração
-                has_swapped = True
-        num_of_iterations += 1
-
-    return array
+    except TypeError:
+        return []
