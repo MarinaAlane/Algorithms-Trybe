d9089f428065a30a35bf4483d621feb441cdf1d9
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-03-02 17:51:28 -0300

    Requisito 05 - Fix - Hack ¬¬'

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,9 +1,15 @@
+# def is_palindrome_iterative(word):
+#     if word == '':
+#         return False
+
+#     for index, letter in enumerate(word):
+#         if letter != word[-(index+1)]:
+#             return False
+
+#     return True
+
 def is_palindrome_iterative(word):
     if word == '':
         return False
 
-    for index, letter in enumerate(word):
-        if letter != word[-(index+1)]:
-            return False
-
-    return True
+    return word == word[::-1]
