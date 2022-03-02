374e1c6fe6a24de8f06538ecf3a628f2e7c5034e
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-03-02 14:28:33 -0300

    req. 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        if target_time is None:
+            return None
+        return studyTime(permanence_period, target_time)
+    except TypeError:
+        return None
+
+
+def studyTime(permanence_period, target_time):
+    count = 0
+    for entrance, exit in permanence_period:
+        if entrance <= target_time <= exit:
+            count += 1
+    return count
