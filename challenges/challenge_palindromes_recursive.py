c04d3630bc05ca811cc5f37b7ba3180484ad35d6
OldRoge <rogeriomrjr@icloud.com>
2022-03-03 15:36:30 -0300

    Fix identation errors

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
-    
+
     elif low_index > len(word) - 1:
         return True
-    
+
     if (word[low_index] == word[high_index]):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     return False
-
