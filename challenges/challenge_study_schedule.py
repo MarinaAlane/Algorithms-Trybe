7092ec33ff7d0d5302e0c8aec46e6e89c71d206e
juliana-happatsch <jhappatsch@gmail.com>
2022-03-05 18:06:35 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    if target_time is None:
+        return None
+
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+
+        if period[0] <= target_time and period[1] >= target_time:
+            count += 1
+
+    return count
