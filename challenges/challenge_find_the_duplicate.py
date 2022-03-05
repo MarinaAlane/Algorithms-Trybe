42ccc0bc90efaf198ff42e5a22aad8714aa8c548
juliana-happatsch <jhappatsch@gmail.com>
2022-03-05 19:17:22 -0300

    acho q o 4?

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    for number in nums:
+        if isinstance(number, str) or number < 0:
+            return False
+        
+        if nums.count(number) > 1:
+            return number
+        
+    return False
