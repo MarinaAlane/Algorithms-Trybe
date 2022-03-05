703b30314501bde7814af84e9bf2d646a0aa43fd
Ederson S <edersonrodrigo31@gmail.com>
2022-03-05 15:39:20 -0300

    feat: requisito 4 metade

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
 def find_duplicate(nums):
-    return
+    if not (nums):
+        return False
+    if (len(nums) < 2):
+        return False
+    for number in nums:
+        if (type(number) == str or number < 0):
+            return False
