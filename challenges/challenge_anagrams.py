206c504c360d0db9b9453a73323e59f175cd205f
Ederson S <edersonrodrigo31@gmail.com>
2022-03-05 15:52:38 -0300

    fix: requisito 3 exceptions

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,4 @@
 def is_anagram(first_string, second_string):
-    if first_string != second_string:
+    if not first_string or not second_string or first_string != second_string:
         return False
+    return True
