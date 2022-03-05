710ed8555daeb1b07f7fc74e1d6a0a235f543d67
Ederson S <edersonrodrigo31@gmail.com>
2022-03-05 15:45:20 -0300

    fix: requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,16 +1,3 @@
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string:
+    if first_string != second_string:
         return False
-    
-    found=0
-
-    for i in range(len(first_string)):
-        found = 0
-        for j in range(len(first_string)):
-            if first_string[i] == second_string[j]:
-                found = 1
-                break
-        if found==0:
-            return False
-    else:
-        return True
