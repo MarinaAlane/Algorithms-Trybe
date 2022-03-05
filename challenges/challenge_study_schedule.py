d54f0497573f7b4667f22910dab2f9e67c27936c
juliana-happatsch <jhappatsch@gmail.com>
2022-03-05 18:42:48 -0300

    tenta consertar req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,7 +8,7 @@ def study_schedule(permanence_period, target_time):
         if not isinstance(period[0], int) or not isinstance(period[1], int):
             return None
 
-        if period[0] <= target_time and period[1] >= target_time:
+        if period[0] <= target_time <= period[1]:
             count += 1
 
     return count
