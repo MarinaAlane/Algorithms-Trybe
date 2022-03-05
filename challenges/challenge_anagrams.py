d3201ec1bae3dcdee15f41f9cd460f5c9347003d
juliana-happatsch <jhappatsch@gmail.com>
2022-03-05 18:38:14 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
+def order_function(string):
+    string_arr = list(string)
+    new_list = list()
+
+    while string_arr:
+        min = string_arr[0]
+
+        for char in string_arr:
+            if char <= min:
+                min = char
+        
+        string_arr.remove(min)
+        new_list.append(min)
+
+    return new_list
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = order_function(first_string)
+    second = order_function(second_string)
+
+    return first == second
