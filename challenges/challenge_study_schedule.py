cfcc6a71c8a46688a0c6c8faa5282461a94adfb2
juliana-happatsch <jhappatsch@gmail.com>
2022-03-05 18:54:40 -0300

    scrr n ta passando

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,21 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
+    # if target_time is None:
+    #     return None
 
     count = 0
 
-    for period in permanence_period:
-        if not isinstance(period[0], int) or not isinstance(period[1], int):
-            return None
+    # for period in permanence_period:
+    #     if not isinstance(period1, int) or not isinstance(period2, int):
+    #         return None
 
-        if period[0] <= target_time <= period[1]:
-            count = count + 1
+    #     if period1 <= target_time <= period2:
+    #         count = count + 1
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count = count + 1
+    except TypeError:
+        return None
 
     return count
