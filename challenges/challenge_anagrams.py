2d238998b85fff3a69ed495ca38272c3c76c1c12
Ederson S <edersonrodrigo31@gmail.com>
2022-03-05 15:39:04 -0300

    feat: requisito 3 metade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
-    return
+    if not first_string or not second_string:
+        return False
+    
+    found=0
+    notFound=0
+
+    for i in range(len(first_string)):
+        found = 0
+        for j in range(len(first_string)):
+            if first_string[i] == second_string[j]:
+                found = 1
+                break
+        if found==0:
+            notFound = 1
+            break
+    if notFound==1:
+        return False
+    else:
+        return True
