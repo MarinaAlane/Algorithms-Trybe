14053ebf1850d9c36daf574dd4ecbbcb026c10e9
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-03-06 00:30:40 -0300

    FEAT: Requirement 5 - is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if (len(word) == 0):
+        return False
+
+    for index in range(len(word) // 2):
+        first_current_word = word[index]
+        last_current_word = word[(len(word) - 1) - index]
+
+        if(first_current_word != last_current_word):
+            return False
+
+    return True
