c4722cb4d3fbc2e3ebf7006252fb92ab4828229f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-03-06 15:23:44 -0300

    fix. req.5 iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,4 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    if not word:
+def is_palindrome_iterative(word):
+    if not word or word != word[::-1]:
         return False
-
-    if low_index >= high_index:
-        return True
-
-    if word[low_index] == word[high_index]:
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    else:
-        return False
+    return True
