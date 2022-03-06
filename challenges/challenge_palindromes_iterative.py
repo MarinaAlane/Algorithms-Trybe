8b481681da21a2bef5b12186f406d7963388198b
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-03-06 01:14:35 -0300

    Finished! :airplane:

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,11 +2,18 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 
-    for index in range(len(word) // 2):
-        first_current_word = word[index]
-        last_current_word = word[(len(word) - 1) - index]
+    counter = 0
 
-        if(first_current_word != last_current_word):
+    while counter < len(word) // 2:
+        first_current_word = word[counter]
+        last_current_word = word[(len(word) - 1) - counter]
+
+        if (first_current_word != last_current_word):
             return False
 
+        counter += 1
+
     return True
+
+# O range(len(word) // 2) ou o laço for é menos performático?
+# Ao substituir por um laço while a performance melhorou de 0.0022 para 0.0018
--- tests/test_palindromes_iterative.py
+++ tests/test_palindromes_iterative.py
@@ -40,7 +40,6 @@ def test_validar_tempo_iterative():
             number=10000,
         )
     correct_time = time <= 0.005
-    print(f"O meu tempo foi de: {time}")
     assert (
         algorithms_correct and correct_time
     ), f"Falhou, o tempo foi: {time}, algoritmo correto? {algorithms_correct}"
