00c439f4e47a0ed6ff21f5b9bf897b0fad8fe9b2
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-03-06 00:21:21 -0300

    FEAT: Requirement 4 - find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) < 2:
+        return False
+
+    for index, num in enumerate(nums):
+        if (not isinstance(num, int) or num < 0):
+            return False
+
+        if (nums[index] in nums[index + 1:]):
+            return nums[index]
+
+    return False
