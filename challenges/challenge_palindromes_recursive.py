d67bc927088bdfa1938808d129092e529acfeae8
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-03-06 13:43:13 -0300

    req. 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) < 1:
+        return False
+    palindrome = reverse(word, low_index, high_index)
+    return palindrome == word
+
+
+def reverse(word, low_index, high_index):
+    if low_index == high_index:
+        return word
+    return (
+        reverse(word[1:], low_index, len(word[1:]) - 1)
+        + word[0]
+    )
