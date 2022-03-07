08c94991cc379b29a62510334ab9ca0b8117bd6d
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-03-06 21:32:37 -0300

    Requisito 03-Feature: implementa algoritmo verifica se duas palavras são anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    dict_1 = dict()
+    dict_2 = dict()
+    for letter in first_string:
+        try:
+            dict_1[letter] += 1
+        except KeyError:
+            dict_1[letter] = 1
+
+    for letter in second_string:
+        try:
+            dict_2[letter] += 1
+        except KeyError:
+            dict_2[letter] = 1
+
+    return dict_1 == dict_2
