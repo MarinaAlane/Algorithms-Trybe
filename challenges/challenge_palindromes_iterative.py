a79af9b6eeb0b6c1df9342f32b9f57a32ab0f32f
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-03-06 22:14:08 -0300

    Requisito 05-Feature: Implementa algoritmo que verifica se a palavra é um palindromo(iterativo)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
+""" Meu conterraneo Luiz Wendell me ajudou a encontrar esta solução
+https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/"""
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    return word == word[::-1]
