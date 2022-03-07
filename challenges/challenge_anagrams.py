50f057f005f60fdbc98441b5966df015622a6ec4
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-03-06 21:40:16 -0300

    Requisito 03-Fix: Corrigindo complexidade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,16 +1,16 @@
-def is_anagram(first_string, second_string):
-    dict_1 = dict()
-    dict_2 = dict()
-    for letter in first_string:
+def mount_dict(string):
+    dictio = dict()
+    for letter in string:
         try:
-            dict_1[letter] += 1
+            dictio[letter] += 1
         except KeyError:
-            dict_1[letter] = 1
+            dictio[letter] = 1
 
-    for letter in second_string:
-        try:
-            dict_2[letter] += 1
-        except KeyError:
-            dict_2[letter] = 1
+    return dictio
+
+
+def is_anagram(first_string, second_string):
+    dict_1 = mount_dict(first_string)
+    dict_2 = mount_dict(second_string)
 
     return dict_1 == dict_2
