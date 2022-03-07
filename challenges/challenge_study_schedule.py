1361c4ed381f7f6c9c4736c0deb1638a9566be57
deltadigo <deltadigo@gmail.com>
2022-03-07 01:39:01 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    amount = 0
+    
+    if not target_time:
+        return None
+    for start_time, final_time in permanence_period:
+        if type(start_time) is not int or type(final_time) is not int:
+            return None
+        if start_time <= target_time <= final_time:
+            amount += 1
+
+    return amount
