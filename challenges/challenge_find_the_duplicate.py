013300eb978f2e5bcac5a26950e9d5526641e60f
deltadigo <deltadigo@gmail.com>
2022-03-07 02:46:16 -0300

    feat: Requisito 4 - complexidade 6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 0:
+        return False
+
+    for num in nums:
+        if type(num) != int:
+            return False
+        if num < 0:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
