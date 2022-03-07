bc05449fc701e054974b329817a103c954af507d
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-03-06 21:57:00 -0300

    Requisito 04-Feture: Implementa algoritmo que valida se existem numeros repetidos em uma lista

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,24 @@
+def mount_dict(nums):
+    dictio = dict()
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+        try:
+            dictio[num] += 1
+        except KeyError:
+            dictio[num] = 1
+
+    return dictio
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    dict_nums = mount_dict(nums)
+
+    if not dict_nums:
+        return False
+
+    for key, value in dict_nums.items():
+        if value > 1:
+            return key
+
+    return False
