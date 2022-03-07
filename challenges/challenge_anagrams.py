2356b6f4c63d94cf562a73bd2caa661c9c4d8938
deltadigo <deltadigo@gmail.com>
2022-03-07 02:15:29 -0300

    feat: Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+def char_counter(string):
+    counted_char = {}
+    for char in string:
+        if char in counted_char:
+            counted_char[char] += 1
+        else:
+            counted_char[char] = 1
+    return counted_char
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_string_counted = char_counter(first_string)
+    second_string_counted = char_counter(second_string)
+
+    return first_string_counted == second_string_counted
