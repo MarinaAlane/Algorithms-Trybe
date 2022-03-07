d4263cfa0de79fbc7db6b5f55cb1890d02252576
OldRoge <rogeriomrjr@icloud.com>
2022-03-07 09:10:48 -0300

    Done requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # Ambas palavras precisam ter o mesmo length
+    # sabendo o mesmo length já diminui uma longe de busca
+    # precisa verificar se possuem as mesmas letras
+    if len(first_string) != len(second_string):
+        return False
+    
+    for value in first_string:
+        second_string = second_string.replace(value, '', 1)
+    return len(second_string) == 0
+
+# print(is_anagram("pedra", "perdaaa"))
+# print(is_anagram(
+#         "Lorem ipsum dolor sit amet, consectetur "
+#         "adipiscing elit, do sed eiusmod tempor "
+#         "incididunt ut labore et dolore magna aliqua.",
+        
+#         "Lorem ipsum dolor sit amet, consectetur "
+#         "adipiscing elit, do sed eiusmod tempor "
+#         "incididunt ut labore et dolore magna aliqua."))
