dc920011a90566329d59811eecd5e8e6660789d0
deltadigo <deltadigo@gmail.com>
2022-03-07 03:10:04 -0300

    fix: correção de complexidade 6 no lint

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,8 @@
 def find_duplicate(nums):
     if len(nums) == 0:
         return False
-
     for num in nums:
-        if type(num) != int:
-            return False
-        if num < 0:
+        if type(num) != int or num < 0:
             return False
         if nums.count(num) > 1:
             return num
