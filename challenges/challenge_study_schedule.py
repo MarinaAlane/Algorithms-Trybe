2fbdf0bb56cda6bcaf20c6a1fea2ddff543a7569
guinhoanjos <iago.anjos@hotmail.com>
2022-03-08 18:03:48 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for period1, period2 in permanence_period:
+        if (type(period1) is  int) and (type(period2) is  int) and (type(target_time) is int):
+            if target_time >= period1 and target_time<= period2:
+                count += 1
+        else:
+            return None
+    return count
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)],3))
+
+print()
