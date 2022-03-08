70e17df6bc57a4ecf92a8f01489b0fab37581902
guinhoanjos <iago.anjos@hotmail.com>
2022-03-08 18:22:23 -0300

    lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,10 @@
 def study_schedule(permanence_period, target_time):
     count = 0
+    if type(target_time) is not int:
+        return None
     for period1, period2 in permanence_period:
-        if (type(period1) is  int) and (type(period2) is  int) and (type(target_time) is int):
-            if target_time >= period1 and target_time<= period2:
+        if (type(period1) is int) and (type(period2) is int):
+            if target_time >= period1 and target_time <= period2:
                 count += 1
         else:
             return None
