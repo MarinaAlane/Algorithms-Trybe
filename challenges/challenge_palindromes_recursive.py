fbbcc6bdf4671e0c8da4e9f60f4c92ef638246a0
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-09 04:47:58 -0300

    fix: consertando error de lint 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
-    
+
     if word[low_index] == word[high_index]:
         if low_index < high_index:
             new_low_index = low_index + 1
