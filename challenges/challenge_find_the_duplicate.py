d3e86d2cfad741d0a81d2a04cf2948937274478c
dcmatheus <mathws.costag@gmail.com>
2022-03-09 15:18:18 -0300

    implements find_duplicate funcion

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    try:
+        numbers = set()
+        for num in nums:
+            if num in numbers:
+                return num
+            if num < 0:
+                return False
+            numbers.add(num)
+    except TypeError:
+        return False
+    return False
