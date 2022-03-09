1cf5e033c19309840bbbde7cca7f2977d84407f1
guinhoanjos <iago.anjos@hotmail.com>
2022-03-09 02:09:10 -0300

    requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    result = [0, 0]
+    for number in nums:
+        if  type(number) == str or number < 0:
+            return False
+        if nums.count(number) > result[1]:
+         result[0] = number
+         result[1] = nums.count(number)
+    if result[1] == 1:
+        return False
+    return result[0]
+
+# array = [ 1, 2 ,1]
+# print(array.count(2))
+numero = "ae"
+print(type(numero) is str)
