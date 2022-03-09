90dc37a7f0ae7015069feb73e00eb3a32df34ba9
dcmatheus <mathws.costag@gmail.com>
2022-03-09 12:13:46 -0300

    implements study_schetule funcion

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    try:
+        compatible_hours = 0
+        for entry_time, departure_time in permanence_period:
+            if entry_time <= target_time <= departure_time:
+                compatible_hours += 1
+        return compatible_hours
+    except TypeError:
+        return None
