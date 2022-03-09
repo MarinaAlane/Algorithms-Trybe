e2680e87afb3f6c16a6585310293d776311759e2
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-09 04:31:25 -0300

    fix: consertando error de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    # função type() encontrada no site: https://www.guru99.com/type-isinstance-python.html
+# https://www.guru99.com/type-isinstance-python.html
     result = 0
 
     if not target_time:
         return None
-    
+
     for start_time, exit_time in permanence_period:
         if type(start_time) is not int or type(exit_time) is not int:
             return None
-        
+
         if start_time <= target_time <= exit_time:
             result = result + 1
-    
+
     return result
