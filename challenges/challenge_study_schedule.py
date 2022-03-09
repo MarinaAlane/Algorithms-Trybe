c6c3e3da4c9693b3c1afeed29517a506f5efee57
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-03-08 21:35:07 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_in_period = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                students_in_period += 1
+        return students_in_period
+    except TypeError:
+        return None
