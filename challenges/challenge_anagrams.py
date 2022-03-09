374a6bb204d9db15801d4a072a7ed1cbeb3308cf
guinhoanjos <iago.anjos@hotmail.com>
2022-03-09 01:53:24 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    for letter in first_string:
+        if first_string.count(letter) != second_string.count(letter):
+            return False
+
+    return True
