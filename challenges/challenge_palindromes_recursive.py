29af86d45629faf35256608fa8aa2cacd484434a
Matheus H. Gois <matheus.hengo@gmail.com>
2022-03-09 15:09:18 -0300

    feat: req 2 - palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[low_index] != word[high_index]:
+      return False
+
+    if high_index - low_index <= 0:
+      return True
+
+    next_initial_char = low_index + 1
+    previous_last_char = high_index - 1
+
+    return is_palindrome_recursive(word, next_initial_char, previous_last_char)
+
+
+# REVIVER (impar)
+# low_index   = 0 -> 1 -> 2 -> 3 
+# high_index  = 6 -> 5 -> 4 -> 3 
+
+# GG (par)
+# low_index   = 0 -> 1
+# high_index  = 1 -> 0
