9c48c7da5ab663c6bd20c02d7b424010a833bb29
richardfazeredo91 <richardbrprogrammer@gmail.com>
2022-03-09 06:58:38 -0300

    Implementada função iterativa que dado um target time, verifica em uma lista de tuplas qual dos elementos é o mais presente dentro do range de cada elemento

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_qtd = 0
+
+    try:
+        for time in permanence_period:
+            if time[0] <= target_time <= time[1]:
+                students_qtd += 1
+        return students_qtd
+    except TypeError:
+        return None
