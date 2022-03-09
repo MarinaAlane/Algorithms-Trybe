4769b3ad3c162a2f42318f746bcb43c2af93ea7f
alstent <rafaelalstent@gmail.com>
2022-03-09 16:03:42 +0100

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_count = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                students_count += 1
+        return students_count
+    except TypeError:
+        return None
