668eb45a4aaca5b8edfde849c250a3c81101fbe3
Matheus H. Gois <matheus.hengo@gmail.com>
2022-03-09 17:02:31 -0300

    fix: req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,11 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-	
+
     repetition = 0
+    index = 0
 
     while repetition < len(word) // 2:
-        index = 0
         reversed_index = -1 * (index + 1)
 
         if word[index] != word[reversed_index]:
