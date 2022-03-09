5001908b217c7ad09f3771720b40cea803f28d82
Matheus H. Gois <matheus.hengo@gmail.com>
2022-03-09 14:35:52 -0300

    feat: req 1 - challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not (
+        target_time
+        and isinstance(permanence_period, list)
+      ):
+        return None
+
+    students = 0
+
+    for (start, end) in permanence_period:
+
+        if not (isinstance(start, int) and isinstance(end, int)):
+            return None
+
+        if target_time >= start and target_time <= end:
+            students += 1
+
+    return students
