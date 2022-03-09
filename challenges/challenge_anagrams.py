2faf509fdaddf123b49dffc4de4b79f5222655ea
carlosgabrielcampo <carlosgabrielcampo@gmail.com>
2022-03-09 20:11:48 -0300

    3rd requirement completed

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    for e in first_string:
+        if first_string.count(e) != second_string.count(e):
+            return False
+    return True
