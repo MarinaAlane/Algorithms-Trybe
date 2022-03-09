0c44eb4632155479ac49363684a5a72c2428bf23
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-03-08 22:21:37 -0300

    req 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums_set = set(nums)
+    for num in nums:
+        if type(num) is str:
+            return False
+        if num < 0:
+            return False
+        if num in nums_set:
+            nums_set.remove(num)
+        else:
+            return num
+    return False
