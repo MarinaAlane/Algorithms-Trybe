cb7f28bfcf2f7cbc1fd79a7d9aaf7df3a2e3b466
carlosgabrielcampo <carlosgabrielcampo@gmail.com>
2022-03-09 19:44:19 -0300

    2nd requirement completed

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if low_index == high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        if high_index == 1:
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
