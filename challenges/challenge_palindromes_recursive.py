ca4c06d554e269a899e491464f9348ea44cede75
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-09 04:46:07 -0300

    feat: req 2 passando nos testes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    
+    if word[low_index] == word[high_index]:
+        if low_index < high_index:
+            new_low_index = low_index + 1
+            new_high_index = high_index - 1
+            return is_palindrome_recursive(word, new_low_index, new_high_index)
+        else:
+            return True
+    else:
+        return False
