61ad4a5f07691a51b009b716d54d48fc0a612511
carlosgabrielcampo <carlosgabrielcampo@gmail.com>
2022-03-09 20:41:07 -0300

    4th requirement pre-concluded

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sset = set()
+    if len(nums) < 2:
+        return False
+    for e in nums:
+        if type(e) is not int or e < 0:
+            return False
+        sset.add(e)
+    return False
