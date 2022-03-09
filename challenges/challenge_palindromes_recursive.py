83beb54193fde65cac327c94b026a5a7c6dbd546
dcmatheus <mathws.costag@gmail.com>
2022-03-09 13:43:39 -0300

    implements is_palindrome_recursive funcion

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) == 1:
+        return True
+    if low_index <= high_index:
+        return word[low_index] == word[high_index] and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
+    return word[low_index] == word[high_index]
