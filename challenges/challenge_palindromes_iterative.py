07be97017ff4c37d0ead53eda4a20e49c531939d
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-03-10 17:26:31 -0300

    done five requirement

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
+# code export in the https://stackoverflow.com/questions/61604504/palindrome
+# -iterative-checker-in-python
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    for forward, backward in zip(word, reversed(word)):
+        if forward != backward:
+            return False
+    return True
