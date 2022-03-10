a93065f00f6919919ced41a3174b042454132dc9
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-10 01:51:00 -0300

    feat: req 3 passando nos testes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,46 @@
+# Função Merge Sort encontrada do course da Trybe no endereço:
+# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+def merge_sort(array):
+
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    # Metodo para separar a string en chars encontrado no site:
+    # https://www.geeksforgeeks.org/python-split-string-into-list-of-characters/
+    sorted_first_string = merge_sort(list(first_string))
+    sorted_second_string = merge_sort(list(second_string))
+
+    if sorted_first_string == sorted_second_string:
+        return True
+    else:
+        return False
