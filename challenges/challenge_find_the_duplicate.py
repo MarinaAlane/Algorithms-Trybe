c2485827e49b11068751e3418f496778ba12e668
Henrique Pozzolini <hpmoreira05@gmail.com>
2022-03-10 16:43:24 -0300

    Req 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for index, out_number in enumerate(nums):
+        for in_number in nums[index + 1:]:
+            if type(out_number) is str or out_number < 0:
+                return False
+            if out_number == in_number:
+                return out_number
+    return False
