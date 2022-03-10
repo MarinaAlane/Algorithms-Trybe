ac0554dba3844a78187a95f2c1e8e9ca65c95be3
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-10 17:24:58 -0300

    feat: req 5 passando nos testes

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    right = len(word) - 1
+    left = 0
+
+    while left < right:
+        if word[left] == word[right]:
+            right = right - 1
+            left = left + 1
+        else:
+            return False
+
+    return True
