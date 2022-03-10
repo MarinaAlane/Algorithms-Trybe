0ee261943c6ed93f873e55eaa9b32b0e8b6b368b
Henrique Pozzolini <hpmoreira05@gmail.com>
2022-03-10 15:38:08 -0300

    Req 01 fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,9 +2,9 @@ def study_schedule(permanence_period, target_time):
     total_of_students_at_target_time = 0
     for student_start_time in permanence_period:
         try:
-            is_online = student_start_time[0] <= target_time <= student_start_time[1]
-            if is_online:
+            on = student_start_time[0] <= target_time <= student_start_time[1]
+            if on:
                 total_of_students_at_target_time += 1
         except TypeError:
-                return None
+            return None
     return total_of_students_at_target_time
