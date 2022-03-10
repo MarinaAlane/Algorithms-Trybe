2d77e2e509bd713ce1f14e9f4c4b098efeb8786f
Marina Alane <marina.alane@outlook.com>
2022-03-10 09:55:46 -0300

    chore: add find duplicate and palindrome itearitve functions

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    return word == word[::-1]
+
+# ref: https://github.com/tryber/sd-011-project-algorithms/pull/119/files
