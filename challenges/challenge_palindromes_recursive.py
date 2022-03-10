e0ec1ddb0ab39fb4baf088acc043c8e55e5f17a0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-03-10 16:43:01 -0300

    done second requirement

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
+# reference https://www.w3resource.com/python-exercises
+# /python-functions-exercise-12.php
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if (low_index - high_index) < 2:
+            return is_palindrome(word, low_index, high_index)
+        return True
+    except IndexError:
+        return False
+
+
+def is_palindrome(word, low_index, high_index):
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index+1, high_index-1)
+    else:
+        return False
