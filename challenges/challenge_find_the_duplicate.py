291650b2012083485b7379b69a201466c699640c
Guilherme Nunes <guiicnunes@gmail.com>
2022-03-10 20:43:51 -0300

    Req: 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,25 @@
+from collections import defaultdict
+
+def array_to_dict(array):
+    my_dict = defaultdict(lambda: 0)
+    for num in array:
+        my_dict[num] += 1
+
+    return my_dict
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 0 :
+        return False
+
+    for number in nums:
+        if type(number) is str:
+            return False
+        if number < 0:
+            return False
+    
+    dict = array_to_dict(nums)
+    if max(dict.values()) < 2:
+        return False
+
+    return max(dict, key=dict.get)
