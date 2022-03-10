e4a5e58dcdda0b0af3c235bcb7276872f56512b5
Guilherme Nunes <guiicnunes@gmail.com>
2022-03-10 19:57:36 -0300

    Req: 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
+from collections import defaultdict
+
+def array_to_dict(string):
+    my_dict = defaultdict(lambda: 0)
+    for letter in string:
+        my_dict[letter] += 1
+
+    return my_dict
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return array_to_dict(first_string) == array_to_dict(second_string)
