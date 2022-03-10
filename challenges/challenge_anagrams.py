3c607d068e282ebac0fa7b4ca3bdac353a0162ee
Marina Alane <marina.alane@outlook.com>
2022-03-10 09:59:28 -0300

    fix: delte white spaces and add new blank line at end of file

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,6 +3,6 @@ def is_anagram(first_string, second_string):
         return False
     if first_string != second_string:
         return False
-    return True 
+    return True
 
 # ref https://github.com/tryber/sd-011-project-algorithms/pull/112
