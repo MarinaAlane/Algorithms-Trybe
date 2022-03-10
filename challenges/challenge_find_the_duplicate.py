820119ecb138c13529a80ef5f617d7cbfc6ce242
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-03-10 17:19:13 -0300

    done four requirement

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,23 @@
+# reference https://stackoverflow.com/questions/14681096/
+# typeerror-isinstance-arg-2-must-be-a-type-or-tuple-of-types
+# reference https://realpython.com/python-enumerate/
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    while len(nums) <= 1:
+        return True
+
+    duplicated_number = False
+    sorted_nums = nums
+    sorted_nums.sort()
+    for i, j in enumerate(sorted_nums):
+        if isinstance(j, str) or j < 0:
+            return False
+
+        curr = j
+        prev = sorted_nums[i - 1]
+
+        if curr == prev:
+            duplicated_number = curr
+
+    return duplicated_number
