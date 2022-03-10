02b0e237b48345b5070749731fb9f1f00ced30a8
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2022-03-10 20:02:57 -0300

    fix: req 1 teste

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,11 +3,11 @@ from tokenize import Number
 
 def study_schedule(permanence_period, target_time):
     if(target_time is None):
-        return target_time
+        return None
     sum = 0
-    for period in permanence_period:
-        if(period[0] is not Number or period[1] is not Number):
+    for period1, period2 in permanence_period:
+        if(period1 is not Number or period2 is not Number):
             return None
-        if(period[0] <= target_time <= period[1]):
-            sum = sum + 1
+        if(period1 <= target_time <= period2):
+            sum += 1
     return sum
