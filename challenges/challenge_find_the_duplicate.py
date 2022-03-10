f39ed569407ee46b69b5c1d0045ea2c5b6abed2f
Lucas Duque <lucas3rios2014@gmail.com>
2022-03-10 02:17:31 -0300

    feat: req 4 passando nos testes

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    # Metodo para encontrar duplicatas encontrado no site:
+    # https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
+
+    isDuplicate = set()
+
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+        if num in isDuplicate:
+            return num
+        else:
+            isDuplicate.add(num)
+    return False
