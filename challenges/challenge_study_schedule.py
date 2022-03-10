64c6f23a3dca6a1f0d59a710c6f2a64895e9c740
cristian <cristian.brum2@yahoo.com.br>
2022-03-10 16:03:10 -0300

    feat: :rocket: req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,21 @@
 def study_schedule(permanence_period, target_time):
-    ...
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for index in permanence_period:
+        if type(index[0]) != int or type(index[1]) != int:
+            return None
+
+        if index[0] <= target_time <= index[1]:
+            count = count + 1
+
+    return count
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+target_time = 1
+
+study_schedule(permanence_period, target_time)
