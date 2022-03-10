6803c2117b25d935051213f032f044b73bc2a9b3
IgorMendez <igor.emp@hotmail.com>
2022-03-10 20:13:57 -0300

    Req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    if not target_time:
+        return None
+    for (start, end) in permanence_period:
+        if type(start) is not int or type(end) is not int:
+            return None
+        if start <= target_time <= end:
+            count += 1
+
+    return count
