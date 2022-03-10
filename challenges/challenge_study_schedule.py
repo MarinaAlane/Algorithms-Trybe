59d65e56051b1de809ec68d2806eaeb8e5da6dc4
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-03-10 16:02:26 -0300

    done first requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        count = 0
+        for student in permanence_period:
+            count = count+1 if student[0] <= target_time and student[1] >= target_time else count
+    except TypeError:
+        return count
