44c33c5a99446378a12edc7e323566d1cc8e1f00
Marina Alane <marina.alane@outlook.com>
2022-03-09 21:28:20 -0300

    chore: add anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    if first_string != second_string:
+        return False
+    return True 
+
+# ref https://github.com/tryber/sd-011-project-algorithms/pull/112
