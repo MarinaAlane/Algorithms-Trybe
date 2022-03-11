d591020db3f1d6dd510dec2381e829e180383f57
regoraphael <raphael.mlrego@gmail.com>
2022-03-10 21:08:16 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    frequency = 0
+    try:
+        for item in permanence_period:
+            check_in = item[0]
+            check_out = item[1]
+            if check_in <= target_time and target_time <= check_out:
+                frequency += 1
+        return frequency
+    except TypeError:
+        return None
+
