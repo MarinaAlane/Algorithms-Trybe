a635ee26212bd049d07db2d5012e02eb75207676
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 10:51:17 -0300

    feat: req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
+def order(word):
+    abc = {}
+    for letter in word:
+        if letter in abc:
+            abc[letter] += 1
+        else:
+            abc[letter] = 1
+    return abc
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-# anagram
+    if len(first_string) != len(second_string):
+        return False
+    first_string = order(first_string)
+    second_string = order(second_string)    
+    
+    if first_string == second_string:
+        return True
+    else:
+        return False
