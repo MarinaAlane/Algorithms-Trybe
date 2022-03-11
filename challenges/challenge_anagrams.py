359d36875bcfb727e233e47f76f2bed6a289cbb3
cristian <cristian.brum2@yahoo.com.br>
2022-03-11 11:23:49 -0300

    fix: :rocket: refact req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    a = list(first_string)
+    b = list(second_string)
+    if a == b:
+        return True
+    else:
+        return False
+
+
+print(is_anagram('pedra', 'perdaaa'))
