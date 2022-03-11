90446f5925e1442aff05c91dcc1d5eeacb6510c3
Guilherme Nunes <guiicnunes@gmail.com>
2022-03-10 21:11:21 -0300

    FIX: Lint

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,13 @@
 import math
 
+
 def is_palindrome_iterative(word):
-    
+
     if word == "":
         return False
 
     length = len(word)
-    middle = math.ceil(length /2)
+    middle = math.ceil(length / 2)
 
     for i in range(0, middle):
         if word[i] != word[(length - 1) - i]:
