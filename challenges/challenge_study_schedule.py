7150b3276e9326682c8e6056923f92f1cc985f20
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-10 21:56:30 -0300

    feat: req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    studentsPeriod = 0
+    try:
+        for period in permanence_period:
+            time_entered = period[0]
+            time_leave = period[1]
+            if target_time >= time_entered and target_time <= time_leave:
+                studentsPeriod += 1
+        return studentsPeriod
+    except TypeError:
+        return None
