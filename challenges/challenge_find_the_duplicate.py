ede88bb658a976fbc94274db12affc1bbfacad6b
IgorMendez <igor.emp@hotmail.com>
2022-03-10 22:34:37 -0300

    Req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,23 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if check_nums(nums):
+        return False
+    if not nums:
+        return False
+
+    numb = sorted(nums)
+    for i in range(1, len(numb)):
+        if numb[i - 1] == numb[i]:
+            return numb[i]
+    return False
+
+
+def check_nums(nums):
+
+    if type(nums) is str:
+        return False
+
+    for numbe in nums:
+        if type(numbe) is not int:
+            return False
+        if numbe < 0:
+            return False
