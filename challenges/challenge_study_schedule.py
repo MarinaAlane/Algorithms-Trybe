9a2ec0c147bbd161237ee196e72441975e56c81d
Rafael Javaro <rafaeljavaro750@gmail.com>
2022-03-10 22:48:39 -0300

    Inicia Projeto - Req 1 - def study_schedule(p, t) - Recebe o (p)periodo de permanencia) dos alunos e retorna a quantidade de alunos online de acordo com o (t)periodo de pesquisa) informado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    number_of_students_online = 0
+
+    if not target_time:
+        return None
+
+    for log_in, log_out in permanence_period:
+        if type(log_in) is not int or type(log_out) is not int:
+            return None
+
+        if log_in <= target_time <= log_out:
+            number_of_students_online += 1
+
+    return number_of_students_online
