db55dec12ca575041e9e52fc9174931502f34d4b
cristian <cristian.brum2@yahoo.com.br>
2022-03-11 10:34:17 -0300

    fix: :rocket: refact req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-
     count = 0
-
-    for key, value in permanence_period:
-        if type(key) != int or type(value) != int:
-            return None
-
-        if key <= target_time <= value:
-            count = count + 1
+    try:
+        for key, value in permanence_period:
+            if key <= target_time <= value:
+                count = count + 1
+    except TypeError:
+        return None
 
     return count
