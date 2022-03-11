72464ba465b7f8ecec6800d4fedfde30202dc7a0
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2022-03-10 21:09:26 -0300

    feat: req 3 test

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(first_string == '' or second_string == ''):
+        return False
+    if(len(first_string) != len(second_string)):
+        return False
+    return custom_sort(first_string) == custom_sort(second_string)
+
+
+def custom_sort(string):
+    new_string = ""
+    min = string[0]
+    while(len(string) > 0):
+        for char in string:
+            if(char < min):
+                min = char
+        string.replace(min, None, 1)
+        new_string += min
+    return new_string
