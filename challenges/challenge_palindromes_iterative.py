ece9b32df8016f6b9a995c9444c8d637439aec7b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-03-11 00:19:02 -0300

    without alternative for consecutive time

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,7 +2,11 @@
 # -iterative-checker-in-python
 
 def is_palindrome_iterative(word):
-    for forward, backward in zip(word, reversed(word)):
-        if forward != backward:
-            return False
-    return True
+    # for forward, backward in zip(word, reversed(word)):
+    #     if forward != backward:
+    #         return False
+    # return True
+    if word == '':
+        return False
+
+    return word == word[::-1]
