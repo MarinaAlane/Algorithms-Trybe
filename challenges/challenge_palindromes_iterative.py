1295f0ef5762974d96e3249f80c5971f2c95be07
Guilherme Nunes <guiicnunes@gmail.com>
2022-03-10 21:09:31 -0300

    REQ: 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,15 @@
+import math
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    
+    if word == "":
+        return False
+
+    length = len(word)
+    middle = math.ceil(length /2)
+
+    for i in range(0, middle):
+        if word[i] != word[(length - 1) - i]:
+            return False
+
+    return True
