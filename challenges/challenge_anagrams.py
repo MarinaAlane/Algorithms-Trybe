fb6891f06ac88831a8c2ebf9f5c14768135e3d32
alstent <rafaelalstent@gmail.com>
2022-03-11 14:08:48 +0100

    req 3 e 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,27 @@
+def my_counter(string):
+    first_counter = {}
+    for letra in string:
+        if letra in first_counter:
+            first_counter[letra] += 1
+        else:
+            first_counter[letra] = 1
+    return first_counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return my_counter(first_string) == my_counter(second_string)
+
+
+def is_anagram_v2(first_string, second_string):
+    # shortest case, if the strings dont have the same length
+    # hello != helloo
+    if len(first_string) != len(second_string):
+        return False
+    # common case
+    for letra in first_string:
+        """ if the second string contains the "letra"=lether of the first
+        string it gets removed from the second string """
+        second_string = second_string.replace(letra, '', 1)
+    """ if all the lethers are removed, that means that both words have the
+    same amount of equal lethers, therefore they are anagrams """
+    return len(second_string) == 0
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    list = {}
+    for num in nums:
+        if num in list:
+            list[num] += 1
+        else:
+            list[num] = 1
+        if list[num] > 1:
+            if num < 0 :
+                return False
+            return num
+    return False
