e198d6287e33b7f9328d71ed619e4b440ec8d12c
regoraphael <raphael.mlrego@gmail.com>
2022-03-10 23:52:47 -0300

    flakecorrection

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,8 @@
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string or len(first_string) != len(second_string):
+    if not first_string or not second_string:
+        return False
+
+    if len(first_string) != len(second_string):
         return False
 
     first_array = list(first_string)
