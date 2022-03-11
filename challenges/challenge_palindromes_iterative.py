a1c8ceab02e98bf3535f73157648beb1f69a9cf5
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 12:06:38 -0300

    feat:req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+
+    if not word:
+        return False
+    if str(word) == str(word)[::-1]: # o indice comeca do final e vai ate o inicio decrementando 1
+        return True
+    else:
+        return False
