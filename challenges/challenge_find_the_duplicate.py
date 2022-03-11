ae53a05b048100c3f44f4ba0a34dc94bbc5a2e43
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-11 13:19:50 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
+import string
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if (not nums or type(nums) == string or len(nums) == 1):
+        return False
+
+    return check_nums(nums)
+
+
+def check_nums(nums):
+    numb = sorted(nums)
+    for i in range(1, len(numb)):
+        if type(numb[i]) != int:
+            return False
+        if numb[i - 1] == numb[i]:
+            return numb[i]
+    return False
