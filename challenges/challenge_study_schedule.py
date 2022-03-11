6e5ddf877a60d470c035485eae343707bd65f729
Marcos Albuquerque <mpdsa20@gmail.com>
2022-03-11 00:37:26 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    if not target_time or target_time == 0:
+        return None
+
+    for (item1, item2) in permanence_period:
+        if type(item1) is not int or type(item2) is not int:
+            return None
+        if item1 <= target_time <= item2:
+            count += 1
+
+    return count
