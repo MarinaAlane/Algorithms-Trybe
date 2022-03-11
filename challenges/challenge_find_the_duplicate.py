42b9db93bf26d2a80f28b44e67da272a93acbbf0
cristian <cristian.brum2@yahoo.com.br>
2022-03-11 13:19:15 -0300

    feat: :rocket: req 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
+# https://www.techiedelight.com/find-duplicate-items-python-list/
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for elem in nums:
+        if type(elem) != int or elem < 1:
+            return False
+        if nums.count(elem) > 1:
+            dup = [x for i, x in enumerate(nums) if i != nums.index(x)]
+            return dup[0]
+    return False
+
+
+find_duplicate([1, 2, 3, 4, 5, 5])
