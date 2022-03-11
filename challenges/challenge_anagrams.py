ff2cba6b707002b2cc0d572d447c20bc18f2ed66
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 10:54:31 -0300

    fix: lint flake8 req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,12 +7,13 @@ def order(word):
             abc[letter] = 1
     return abc
 
+
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
     first_string = order(first_string)
-    second_string = order(second_string)    
-    
+    second_string = order(second_string)
+
     if first_string == second_string:
         return True
     else:
