9f7d164d2a1822e161f3268340c0e3ddbf9a62a9
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-10 22:33:40 -0300

    fix: flake8 lint line 6 palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,7 +3,7 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     elif low_index < high_index:
         if word[low_index] == word[high_index]:
-            return is_palindrome_recursive(word, low_index + 1, high_index -1)
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         else:
             return False
     else:
