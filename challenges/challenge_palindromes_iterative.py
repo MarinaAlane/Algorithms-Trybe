3a8c098a03eb633fc5f1b4896fcae6ded1968760
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 12:15:26 -0300

    feat: refatorando palindromo iterativo, tempo de execucacao maior que o permitido

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,7 +3,4 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 # o indice comeca do final e vai ate o inicio decrementando 1
-    if str(word) == str(word)[::-1]:
-        return True
-    else:
-        return False
+    return str(word) == str(word)[::-1]
