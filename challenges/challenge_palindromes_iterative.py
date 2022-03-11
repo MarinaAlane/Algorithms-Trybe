820021c61ebfee508992d11089ca1c89341e8eb7
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 12:08:22 -0300

    fix: lint flake8 palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,7 +2,8 @@ def is_palindrome_iterative(word):
 
     if not word:
         return False
-    if str(word) == str(word)[::-1]: # o indice comeca do final e vai ate o inicio decrementando 1
+# o indice comeca do final e vai ate o inicio decrementando 1
+    if str(word) == str(word)[::-1]:
         return True
     else:
         return False
