94b37d3bde7d2bac2867d0fa2db9f4d401d85697
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-03-11 11:23:56 -0300

    feat: req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    numbers = list()
+
+
+# https://www.w3schools.com/python/ref_func_isinstance.asp
+
+    for number in nums:
+        if isinstance(number, str) or number < 0:
+            return False
+
+        if number in numbers:
+            return number
+            numbers.add(number)
+
+    return False
